3712b34a4753ec76977a90d8aa35ec71dc46feb0
Fomagran <fomagran6@naver.com>
2021-05-03 14:56:20 +0900

    프로그래머스 음양더하기 완료

--- ViewController.swift
+++ ViewController.swift
@@ -9,34 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var 수박 = "수박"
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
+       print(solution([1,3,4], [true,false,true]))
     }
 }
 
+func solution(_ absolutes:[Int], _ signs:[Bool]) -> Int {
+    var answer = 0
 
-func solution(_ n:Int) -> String {
+    for (i,isPositive) in signs.enumerated() {
+        answer += isPositive ? absolutes[i] : -(absolutes[i])
+    }
     
- 
-    var str = String()
-     var su = "수"
-    if n == 1 {
-        return "수"
-    }
-    else if n%2 == 0 {
-        for _ in 1...n/2{
-            str += "수박"
-        }
-        return str
-    }else{
-        for _ in 1...n/2{
-            su += "박수"
-        }
-        return su
-    }
+    return answer
 }
