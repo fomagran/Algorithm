c70c2150a37913a48761997e57fd7340e8dcb15a
Fomagran <fomagran6@naver.com>
2021-05-03 14:56:20 +0900

    프로그래머스 음양더하기 완료

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,45 +1,11 @@
 import Foundation
 
-func solution(_ n:Int, _ times:[Int]) -> Int64 {
-    
-    //최소시간,최대시간
-    var (min,max) = (Int64(1),Int64(times.max()!) * Int64(n))
-    
-    //이분탐색 이용해서 심사를 받는데 최소 시간을 구함.
-    return binarySearch(times: times, n: n, min: &min, max: &max)
-}
+func solution(_ absolutes:[Int], _ signs:[Bool]) -> Int {
+    var answer = 0
 
-func binarySearch(times:[Int],n:Int,min:inout Int64,max: inout Int64) -> Int64 {
-    
-    //입국심사 최소시간을 담는다.
-    var minTime:Int64 = 0
-    
-    while min <= max {
-        //최소와 최대를 합쳐 2로 나눈 중간값
-        let mid = (min + max) / 2
-        
-        //사람수(중간값을 times안에 있는 값으로 나눈 것들의 합)
-        let peopleCount = times.map{mid/Int64($0)}.reduce(0,+)
-        
-        //사람 수가 n보다 크거나 작다면
-        if peopleCount >= n {
-            
-            //최소시간을 중간값으로 바꿈.
-            minTime = mid
-            
-            //최대값을 중간값에 -1로 바꿈
-            max = mid - 1
-            
-        //사람 수가 n보다 작다면
-        } else {
-            
-            //최소값을 중간값에 +1로 바꿈
-            min = mid + 1
-        }
+    for (i,isPositive) in signs.enumerated() {
+        answer += isPositive ? absolutes[i] : -(absolutes[i])
     }
     
-    //입국심사 최소시간을 반환
-    return minTime
+    return answer
 }
-
-solution(6, [1,2,3,7,10])
