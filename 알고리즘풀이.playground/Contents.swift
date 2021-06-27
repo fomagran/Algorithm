cf4879cd64347b5b2399e030fb72555899b96ee1
Fomagran <fomagran6@naver.com>
2021-06-27 21:08:31 +0900

    순열 예제 만들기

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,56 +1,26 @@
-import Foundation
+var cases:[[Int]] = []
 
-
-func solution(_ user_id:[String], _ banned_id:[String]) -> Int {
+func permutaion(indexes:[Int],current:Int,pickCount:Int,visited:[Bool],picked:[Int]) {
     
-    var result:Set<[Int]> = []
-    let banArray = findBanIdEqualUserId(userId: user_id, banId: banned_id)
-    dfs(banArray:banArray,pickedArray:[],result: &result,current: 0)
-
-    return result.count
-}
-
-func findBanIdEqualUserId(userId:[String],banId:[String]) -> [[Int]] {
+    let initialVisited = visited.isEmpty ? Array(repeating: false, count: indexes.count) : visited
     
-    var banArray = Array(repeating: [Int](), count: banId.count)
+    if current == pickCount {
+        cases.append(picked)
+        return
+    }
     
-    for (i,uid) in userId.enumerated() {
-        for (j,bid) in banId.enumerated() {
-            if isEqual(userId: uid, bannedId: bid) {
-                banArray[j].append(i)
+    indexes
+        .enumerated()
+        .forEach{
+            if !initialVisited[$0.offset] {
+                var newVisited = initialVisited
+                newVisited[$0.offset] = true
+                var newPicked = picked
+                newPicked.append($0.element)
+                permutaion(indexes: indexes, current: current+1, pickCount: pickCount,visited: newVisited,picked: newPicked)
             }
         }
-    }
-    return banArray
 }
 
-func isEqual(userId:String,bannedId:String) -> Bool {
-    if userId.count != bannedId.count { return false }
-    
-    let uid = userId.map{String($0)}
-    let bid = bannedId.map{String($0)}
-    
-    for (i,b) in bid.enumerated() {
-        if b != "*" && uid[i] != bid[i]  {
-            return false
-        }
-    }
-    return true
-}
-
-func dfs(banArray:[[Int]],pickedArray:Set<Int>,result:inout Set<[Int]>,current:Int) {
-    for id in banArray[current] {
-        var newPickedArray = pickedArray
-        newPickedArray.insert(id)
-        if current != banArray.count - 1 {
-            dfs(banArray:banArray,pickedArray: newPickedArray,result:&result ,current: current+1)
-        }else if current == banArray.count - 1 && newPickedArray.count == banArray.count {
-            result.insert(newPickedArray.sorted())
-        }
-    }
-}
-
-
-solution(["frodo", "fradi", "crodo", "abc123", "frodoc"],["fr*d*", "*rodo", "******", "******"])
-
-
+permutaion(indexes: [1,2,3], current: 0,pickCount: 3,visited:[],picked: [])
+print(cases)
