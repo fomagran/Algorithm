6bd5315726766f7b191c581c2a98aab24538c36e
Fomagran <fomagran6@naver.com>
2021-06-27 21:28:39 +0900

    조합 예제

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,26 +1,20 @@
 var cases:[[Int]] = []
 
-func permutaion(indexes:[Int],current:Int,pickCount:Int,visited:[Bool],picked:[Int]) {
-    
-    let initialVisited = visited.isEmpty ? Array(repeating: false, count: indexes.count) : visited
+func combination(indexes: [Int], pickCount: Int, current: Int, picked: [Int]) {
     
     if current == pickCount {
         cases.append(picked)
         return
+    }else if current == indexes.count {
+        return
     }
     
-    indexes
-        .enumerated()
-        .forEach{
-            if !initialVisited[$0.offset] {
-                var newVisited = initialVisited
-                newVisited[$0.offset] = true
-                var newPicked = picked
-                newPicked.append($0.element)
-                permutaion(indexes: indexes, current: current+1, pickCount: pickCount,visited: newVisited,picked: newPicked)
-            }
-        }
+    var newPicked = picked
+    newPicked.append(indexes[current])
+    combination(indexes: indexes, pickCount: pickCount-1, current: current+1, picked: newPicked)
+    combination(indexes: indexes, pickCount: pickCount, current: current+1, picked: picked)
+    
 }
 
-permutaion(indexes: [1,2,3], current: 0,pickCount: 3,visited:[],picked: [])
 print(cases)
+
