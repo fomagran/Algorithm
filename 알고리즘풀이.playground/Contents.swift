1ac526c122d0b132aeb9cf494e3a02cf75e6b16c
Fomagran <fomagran6@naver.com>
2021-06-28 10:35:06 +0900

    조합 수정

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,20 +1,25 @@
 var cases:[[Int]] = []
 
-func combination(indexes: [Int], pickCount: Int, current: Int, picked: [Int]) {
+func combination(indexes: [Int], pickCount: Int, current: Int, picked: [Int],visited:[Bool]) {
+    var initialVisited:[Bool] = visited.isEmpty ? Array(repeating: false, count: indexes.count) : visited
     
     if current == pickCount {
         cases.append(picked)
         return
-    }else if current == indexes.count {
-        return
     }
     
-    var newPicked = picked
-    newPicked.append(indexes[current])
-    combination(indexes: indexes, pickCount: pickCount-1, current: current+1, picked: newPicked)
-    combination(indexes: indexes, pickCount: pickCount, current: current+1, picked: picked)
-    
+    indexes
+        .enumerated()
+        .forEach{
+            if !initialVisited[$0.offset] {
+                initialVisited[$0.offset] = true
+                var newPicked = picked
+                newPicked.append($0.element)
+                combination(indexes: indexes, pickCount: pickCount, current: current+1, picked: newPicked,visited: initialVisited)
+            }
+        }
 }
 
+combination(indexes: [1,2,3,4,5], pickCount: 3, current: 0, picked: [], visited: [])
 print(cases)
 
