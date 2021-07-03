c3f9248a4ba7a1eb04f75091269fb69c773c2081
Fomagran <fomagran6@naver.com>
2021-07-03 22:39:53 +0900

    2개 이하로 다른 비트 풀이 완료!

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,58 +1,27 @@
-import Foundation
-
 func solution(_ numbers:[Int64]) -> [Int64] {
-    var answer:[Int64] = []
-    
-    for n in numbers {
-        answer.append(findMinNumber(n: n))
-    }
-    
-    return answer
+    return numbers.map{findMinNumber(n:$0)}
 }
 
 func makeDecimalToBinary(decimal:Int64) -> String {
+    return "0" + String(decimal, radix: 2)
+}
 
-    return String(decimal, radix: 2)
+func makeBinaryToDecimal(binary:String) -> Int64 {
+    return Int64(binary,radix: 2)!
 }
 
 func findMinNumber(n:Int64) -> Int64 {
-    var x:Int64 = n + 1
-    let nBinary = makeDecimalToBinary(decimal: n).map{String($0)}
-    
-
-    let max = Int64(pow(10.0, 15.0))
-    
-    while x <= max {
-        var diffrenceCount:Int = 0
-        let xBinary = makeDecimalToBinary(decimal: x).map{String($0)}
-        
-        for i in 0..<nBinary.count {
-            if xBinary[xBinary.count - (i+1)] != nBinary[nBinary.count - (i+1)] {
-                diffrenceCount += 1
-                if diffrenceCount > 2 {
-                   break
-                }
+    var nBinary = makeDecimalToBinary(decimal: n).map{String($0)}
+    for i in 0..<nBinary.count {
+        if nBinary[nBinary.count - (i+1)] == "0" {
+            nBinary[nBinary.count - (i+1)] = "1"
+            if i != 0 {
+                nBinary[nBinary.count - i] = "0"
             }
+            break
         }
-
-        if nBinary.count < xBinary.count {
-            let d = xBinary.count - nBinary.count
-            for i in 0..<d {
-                if xBinary[i] != "0" {
-                    diffrenceCount += 1
-                    if diffrenceCount > 2 {
-                       break
-                    }
-                }
-            }
-        }
-        
-        if diffrenceCount <= 2 {
-            return x
-        }
-        x += 1
     }
-    return 0
+    return makeBinaryToDecimal(binary: nBinary.joined())
 }
 
 solution([2,7])
