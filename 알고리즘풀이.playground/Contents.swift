595a7f962d4b4abdf43ac2162047d70ca880e934
Fomagran <fomagran6@naver.com>
2021-07-03 20:59:33 +0900

    2개 이하로 다른 비트 풀이 중 10번 11번 시간초과남 계속

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,25 +1,59 @@
-var cases:[[Int]] = []
+import Foundation
 
-func combination(indexes: [Int], pickCount: Int, current: Int, picked: [Int],visited:[Bool]) {
-    var initialVisited:[Bool] = visited.isEmpty ? Array(repeating: false, count: indexes.count) : visited
+func solution(_ numbers:[Int64]) -> [Int64] {
+    var answer:[Int64] = []
     
-    if current == pickCount {
-        cases.append(picked)
-        return
+    for n in numbers {
+        answer.append(findMinNumber(n: n))
     }
     
-    indexes
-        .enumerated()
-        .forEach{
-            if !initialVisited[$0.offset] {
-                initialVisited[$0.offset] = true
-                var newPicked = picked
-                newPicked.append($0.element)
-                combination(indexes: indexes, pickCount: pickCount, current: current+1, picked: newPicked,visited: initialVisited)
-            }
-        }
+    return answer
 }
 
-combination(indexes: [1,2,3,4,5], pickCount: 3, current: 0, picked: [], visited: [])
-print(cases)
+func makeDecimalToBinary(decimal:Int64) -> String {
+
+    return String(decimal, radix: 2)
+}
+
+func findMinNumber(n:Int64) -> Int64 {
+    var x:Int64 = n + 1
+    let nBinary = makeDecimalToBinary(decimal: n).map{String($0)}
+    
+
+    let max = Int64(pow(10.0, 15.0))
+    
+    while x <= max {
+        var diffrenceCount:Int = 0
+        let xBinary = makeDecimalToBinary(decimal: x).map{String($0)}
+        
+        for i in 0..<nBinary.count {
+            if xBinary[xBinary.count - (i+1)] != nBinary[nBinary.count - (i+1)] {
+                diffrenceCount += 1
+                if diffrenceCount > 2 {
+                   break
+                }
+            }
+        }
+
+        if nBinary.count < xBinary.count {
+            let d = xBinary.count - nBinary.count
+            for i in 0..<d {
+                if xBinary[i] != "0" {
+                    diffrenceCount += 1
+                    if diffrenceCount > 2 {
+                       break
+                    }
+                }
+            }
+        }
+        
+        if diffrenceCount <= 2 {
+            return x
+        }
+        x += 1
+    }
+    return 0
+}
+
+solution([2,7])
 
