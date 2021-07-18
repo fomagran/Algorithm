4ac8ecb083345b25061cca90961f0a7a377b8b3a
Fomagran <fomagran6@naver.com>
2021-07-18 18:43:53 +0900

    2021 카카오 인턴쉽 표편집 풀이완성

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,28 +1,97 @@
-func solution(_ numbers:[Int64]) -> [Int64] {
-    return numbers.map{findMinNumber(n:$0)}
+import Foundation
+
+var selectedIndex:Int = 0
+var linkedList = [[Int]]()
+var removeHistory:[Int] = []
+
+func solution(_ n:Int, _ k:Int, _ cmd:[String]) -> String {
+    return makeResultByCommand(cmd: cmd, n: n,k: k)
 }
 
-func makeDecimalToBinary(decimal:Int64) -> String {
-    return "0" + String(decimal, radix: 2)
+func makeResultByCommand(cmd:[String],n:Int,k:Int) -> String {
+    initialSetting(n: n,k:k)
+    for command in cmd {
+        changeByCommands(command: command)
+    }
+
+    var result = Array(repeating: "O", count: n)
+    
+    for r in removeHistory {
+        result[r] = "X"
+    }
+    
+    return result.joined()
 }
 
-func makeBinaryToDecimal(binary:String) -> Int64 {
-    return Int64(binary,radix: 2)!
+func initialSetting(n:Int,k:Int) {
+    selectedIndex = k
+    for i in 0..<n {
+        linkedList.append([i-1,i,i+1])
+    }
 }
 
-func findMinNumber(n:Int64) -> Int64 {
-    var nBinary = makeDecimalToBinary(decimal: n).map{String($0)}
-    for i in 0..<nBinary.count {
-        if nBinary[nBinary.count - (i+1)] == "0" {
-            nBinary[nBinary.count - (i+1)] = "1"
-            if i != 0 {
-                nBinary[nBinary.count - i] = "0"
-            }
-            break
+func changeByCommands(command:String) {
+    let split = command.components(separatedBy: " ")
+    if command.first == "D" {
+        moveColumn(x: Int(split.last!)!, isUp: false)
+    }else if command.first == "U" {
+        moveColumn(x: Int(split.last!)!, isUp: true)
+    }else if command.first == "C" {
+        removeColumn()
+    }else {
+        restoreColumn()
+    }
+}
+
+func past(_ index:Int) -> Int {
+    return linkedList[index][0]
+}
+
+func next(_ index:Int) -> Int{
+    return linkedList[index][2]
+}
+
+func changeLinkedList(selected:Int) {
+    linkedList[selected][1] = -1
+    if past(selected) >= 0 {
+        linkedList[past(selected)][2] = next(selected)
+    }
+    if next(selected) <= linkedList.count - 1 {
+        linkedList[next(selected)][0] = past(selected)
+    }
+}
+
+func moveColumn(x:Int,isUp:Bool){
+    for _ in 0..<x {
+        if isUp {
+            selectedIndex = past(selectedIndex)
+        }else {
+            selectedIndex = next(selectedIndex)
         }
     }
-    return makeBinaryToDecimal(binary: nBinary.joined())
 }
 
-solution([2,7])
+func removeColumn() {
+    changeLinkedList(selected: selectedIndex)
+    removeHistory.append(selectedIndex)
+    if next(selectedIndex) == linkedList.count {
+        selectedIndex = past(selectedIndex)
+    }else {
+        selectedIndex = next(selectedIndex)
+    }
+}
+
+func restoreColumn() {
+    let restore = removeHistory.removeLast()
+    if past(restore) >= 0 {
+        linkedList[past(restore)][2] = restore
+    }
+    if next(restore) <= linkedList.count - 1 {
+        linkedList[next(restore)][0] = restore
+    }
+}
+
+
+
+solution(8, 2, ["D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C"])
 
