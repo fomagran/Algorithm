4f5f35eed986b40a6ce5a659c50294d9139b23ea
Fomagran <fomagran6@naver.com>
2021-07-23 16:42:02 +0900

    2020 카카오 개발자 겨울 인턴쉽 징검다리 건너기 풀이완료

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,97 +1,55 @@
 import Foundation
 
-var selectedIndex:Int = 0
-var linkedList = [[Int]]()
-var removeHistory:[Int] = []
-
-func solution(_ n:Int, _ k:Int, _ cmd:[String]) -> String {
-    return makeResultByCommand(cmd: cmd, n: n,k: k)
-}
-
-func makeResultByCommand(cmd:[String],n:Int,k:Int) -> String {
-    initialSetting(n: n,k:k)
-    for command in cmd {
-        changeByCommands(command: command)
-    }
-
-    var result = Array(repeating: "O", count: n)
+func solution(_ stones:[Int], _ k:Int) -> Int {
     
-    for r in removeHistory {
-        result[r] = "X"
-    }
+    return binarySearch(stones: stones,k: k)
+}
+
+func isCrossable(stones:[Int],k:Int) -> Bool {
     
-    return result.joined()
-}
-
-func initialSetting(n:Int,k:Int) {
-    selectedIndex = k
-    for i in 0..<n {
-        linkedList.append([i-1,i,i+1])
-    }
-}
-
-func changeByCommands(command:String) {
-    let split = command.components(separatedBy: " ")
-    if command.first == "D" {
-        moveColumn(x: Int(split.last!)!, isUp: false)
-    }else if command.first == "U" {
-        moveColumn(x: Int(split.last!)!, isUp: true)
-    }else if command.first == "C" {
-        removeColumn()
-    }else {
-        restoreColumn()
-    }
-}
-
-func past(_ index:Int) -> Int {
-    return linkedList[index][0]
-}
-
-func next(_ index:Int) -> Int{
-    return linkedList[index][2]
-}
-
-func changeLinkedList(selected:Int) {
-    linkedList[selected][1] = -1
-    if past(selected) >= 0 {
-        linkedList[past(selected)][2] = next(selected)
-    }
-    if next(selected) <= linkedList.count - 1 {
-        linkedList[next(selected)][0] = past(selected)
-    }
-}
-
-func moveColumn(x:Int,isUp:Bool){
-    for _ in 0..<x {
-        if isUp {
-            selectedIndex = past(selectedIndex)
-        }else {
-            selectedIndex = next(selectedIndex)
+    var counts = Array(repeating: 0, count: stones.count)
+    
+    for (i,stone) in stones.enumerated() {
+        
+        if stone < 1 {
+            counts[i] = i == 0 ? 1 : counts[i-1] + 1
+        }
+        
+        if counts[i] == k {
+           return false
         }
     }
+    
+    return true
 }
 
-func removeColumn() {
-    changeLinkedList(selected: selectedIndex)
-    removeHistory.append(selectedIndex)
-    if next(selectedIndex) == linkedList.count {
-        selectedIndex = past(selectedIndex)
-    }else {
-        selectedIndex = next(selectedIndex)
+func binarySearch(stones:[Int],k:Int) -> Int {
+    
+    let sort = Set(stones).sorted()
+    
+    if stones.count == 1 { return stones.first! }
+    
+    var maxCount:Int = 0
+    var min = 0
+    var max = sort.count
+    
+    while min <= max {
+        let mid = (min + max) / 2
+        let newStones = stones.map{$0 - sort[mid]}
+    
+        if !isCrossable(stones: newStones, k: k) {
+            maxCount = mid
+            max = mid - 1
+        } else {
+            min = mid + 1
+        }
     }
+
+    return sort[maxCount]
 }
 
-func restoreColumn() {
-    let restore = removeHistory.removeLast()
-    if past(restore) >= 0 {
-        linkedList[past(restore)][2] = restore
-    }
-    if next(restore) <= linkedList.count - 1 {
-        linkedList[next(restore)][0] = restore
-    }
-}
+solution([1,2,3], 1)
 
 
 
-solution(8, 2, ["D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C"])
 
