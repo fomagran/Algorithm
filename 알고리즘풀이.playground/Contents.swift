7d8b22d5adbb88cc05e0f0de167069a396d16aed
Fomagran <fomagran6@naver.com>
2021-07-25 22:33:28 +0900

    2020 KAKAO BLIND RECRUITMENT 블록 이동하기 3차

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,13 +1,16 @@
 import Foundation
 
-struct CurrentLocation {
-    let x1:Int
-    let y1:Int
-    let x2:Int
-    let y2:Int
+struct Current {
+    let x:Int
+    let y:Int
+    let d:Int
+    let dontMove:[Command]
+    let count:Int
 }
 
 enum Command:CaseIterable {
+    case up
+    case down
     case left
     case right
     case rotateClockwise1
@@ -16,192 +19,145 @@ enum Command:CaseIterable {
     case rotateAntiClockwise2
 }
 
-struct Move {
-    var dontMoves:[Command]
-    var current:CurrentLocation
-    var count:Int
-}
-
 var map:[[Int]] = []
 var n:Int = 0
 var answer = 0
-var queue:[Move] = [Move(dontMoves: [], current: CurrentLocation(x1: 0, y1: 0, x2: 1, y2: 0), count: 0)]
+var queue:[Current] = [Current(x: 0, y:0,d:0,dontMove: [], count: 0)]
+let direction = [[1,0],[0,-1],[-1,0],[0,1]]
+let manhattan = [[-1,-1],[-1,1],[1,1],[1,-1]]
+let counterManhattan = [[1,1],[1,-1],[-1,-1],[-1,1]]
 
 func solution(_ board:[[Int]]) -> Int {
     map = board
     n = board.count - 1
     
-    
     while answer == 0 {
         let first = queue.removeFirst()
-        var newCurrent = first.current
-        if isVertical(current: first.current)  {
-            if first.current.y1 > first.current.y2 {
-                newCurrent = CurrentLocation(x1: first.current.x2, y1: first.current.y2, x2: first.current.x1, y2: first.current.y1)
-            }
-        }else {
-            if first.current.x1 > first.current.x2 {
-                newCurrent = CurrentLocation(x1: first.current.x2, y1: first.current.y2, x2: first.current.x1, y2: first.current.y1)
-            }
-        }
-        moveBFS(dontMoves: first.dontMoves, current:newCurrent, count: first.count)
+        moveBFS(current:first)
     }
     return answer
 }
 
-func moveBFS(dontMoves:[Command],current:CurrentLocation,count:Int) {
-    if (current.x1 == n && current.y1 == n) || (current.x2 == n && current.y2 == n) {
-        answer = count
+func moveBFS(current:Current) {
+    if current.x + direction[current.d][0] == n && current.y + direction[current.d][1] == n {
+        answer = current.count
         return
     }
     
-    let allCommand = Command.allCases
-    
-    for move in allCommand {
-        if !dontMoves.contains(move) {
-            moveByCommand(current: current, command: move,count: count)
+    if isEnabled(current: current) {
+        let allCommand = Command.allCases
+        for (i,move) in allCommand.enumerated() {
+            if current.d%2 == 0  {
+                if i == 0 || i == 1 {continue}
+            }else {
+                if i == 2 || i == 3 {continue}
+            }
+            if !current.dontMove.contains(move) {
+                if isRotatable(current: current, rotate: move) {
+                    moveByCommand(current: current, command: move,count: current.count)
+                }
+            }
         }
     }
 }
 
-func moveUp(current:CurrentLocation) -> CurrentLocation {
-    return CurrentLocation(x1:current.x1 , y1: current.y1-1, x2: current.x2, y2: current.y2-1)
+func isEnabled(current:Current) -> Bool {
+    if 0...n ~= current.x && 0...n ~= current.y && 0...n ~= current.x + direction[current.d][0] && 0...n ~= current.y + direction[current.d][1] {
+        if map[current.y][current.x] != 1 && map[current.y + direction[current.d][1]][current.x + direction[current.d][0]] != 1 {
+            return true
+        }else {
+            return false
+        }
+    }
+    return false
 }
 
-func moveDown(current:CurrentLocation) -> CurrentLocation {
-    return CurrentLocation(x1:current.x1 , y1: current.y1+1, x2: current.x2, y2: current.y2+1)
+func isRotatable(current:Current,rotate:Command) -> Bool {
+    var counterDirection = 0
+    if current.d == 0 {
+        counterDirection = 2
+    }else if current.d == 1 {
+        counterDirection = 3
+    }else if current.d == 2 {
+        counterDirection = 0
+    }else {
+        counterDirection = 1
+    }
+    
+    let x2 = current.x + direction[current.d][0]
+    let y2 = current.y + direction[current.d][1]
+    
+    if rotate ==  .rotateClockwise1 {
+        let mx = current.x + manhattan[current.d][0]
+        let my = current.y + manhattan[current.d][1]
+        if 0...n ~= mx && 0...n ~= my {
+            return map[my][mx] == 0
+        }else {
+            return false
+        }
+    }else if rotate == .rotateClockwise2 {
+        let mx = x2 + manhattan[counterDirection][0]
+        let my = y2 + manhattan[counterDirection][1]
+        if 0...n ~= mx && 0...n ~= my {
+            return map[my][mx] == 0
+        }else {
+            return false
+        }
+    }else if rotate == .rotateAntiClockwise1 {
+        let mx = current.x + counterManhattan[current.d][0]
+        let my = current.y + counterManhattan[current.d][1]
+        if 0...n ~= mx && 0...n ~= my {
+            return map[my][mx] == 0
+        }else {
+            return false
+        }
+    }else if rotate == .rotateAntiClockwise2 {
+        let mx = x2 + counterManhattan[counterDirection][0]
+        let my = y2 + counterManhattan[counterDirection][1]
+        if 0...n ~= mx && 0...n ~= my {
+            return map[my][mx] == 0
+        }else {
+            return false
+        }
+    }else {
+        return true
+    }
 }
 
-func moveLeft(current:CurrentLocation) -> CurrentLocation {
-    return CurrentLocation(x1:current.x1-1 , y1: current.y1, x2: current.x2-1, y2: current.y2)
-}
-
-func moveRight(current:CurrentLocation) -> CurrentLocation {
-    return CurrentLocation(x1:current.x1+1 , y1: current.y1, x2: current.x2+1, y2: current.y2)
-}
-
-func rotateClockwise1(current:CurrentLocation) -> CurrentLocation {
-    return CurrentLocation(x1:current.x1 , y1: current.y1, x2: current.x1, y2: current.y1+1)
-}
-
-func rotateVerticalClockwise1(current:CurrentLocation) -> CurrentLocation {
-    return CurrentLocation(x1:current.x1 , y1: current.y1, x2: current.x1-1, y2: current.y1)
-}
-
-func rotateAntiClockwise1(current:CurrentLocation) -> CurrentLocation {
-    return CurrentLocation(x1:current.x1 , y1: current.y1, x2: current.x1, y2: current.y1-1)
-}
-
-func rotateVerticalAntiClockwise1(current:CurrentLocation) -> CurrentLocation {
-    return CurrentLocation(x1:current.x1 , y1: current.y1, x2: current.x1+1, y2: current.y1)
-}
-
-func rotateClockwise2(current:CurrentLocation) -> CurrentLocation {
-    return CurrentLocation(x1:current.x2 , y1: current.y2-1, x2: current.x2, y2: current.y2)
-}
-
-func rotateVerticalClockwise2(current:CurrentLocation) -> CurrentLocation {
-    return CurrentLocation(x1:current.x2+1 , y1: current.y2, x2: current.x2, y2: current.y2)
-}
-
-func rotateAntiClockwise2(current:CurrentLocation) -> CurrentLocation {
-    return CurrentLocation(x1:current.x2 , y1: current.y2+1, x2: current.x2, y2: current.y2)
-}
-
-func rotateVerticalAntiClockwise2(current:CurrentLocation) -> CurrentLocation {
-    return CurrentLocation(x1:current.x2-1 , y1: current.y2, x2: current.x2, y2: current.y2)
-}
-
-func isVertical(current:CurrentLocation) -> Bool {
-    return current.x1 == current.x2
-}
-
-func moveByCommand(current:CurrentLocation,command:Command,count:Int) {
+func moveByCommand(current:Current,command:Command,count:Int) {
+    let nextDirection1 = current.d < 3 ? current.d + 1 : 0
+    let pastDirection1 = current.d > 0 ? current.d - 1 : 3
+    var counterDirection = 0
+    if current.d == 0 {
+        counterDirection = 2
+    }else if current.d == 1 {
+        counterDirection = 3
+    }else if current.d == 2 {
+        counterDirection = 0
+    }else {
+        counterDirection = 1
+    }
+    
+    let nextDirection2 = counterDirection < 3 ? counterDirection + 1 : 0
+    let pastDirection2 = counterDirection > 0 ? counterDirection - 1 : 3
+    
     switch command {
+    case .up:
+        queue.append(Current(x: current.x, y: current.y-1, d: current.d, dontMove: current.dontMove + [.up], count: current.count))
+    case .down:
+        queue.append(Current(x:current.x , y: current.y+1,d: current.d,dontMove: current.dontMove + [.down],count: current.count + 1))
     case .left:
-        if isVertical(current: current) {
-            if current.y1 > 0 {
-                if map[current.y1-1][current.x1] != 1 {
-                    queue.append(Move(dontMoves: [.right], current: moveUp(current: current), count:count+1))
-                }
-            }
-        }else {
-            if current.x1 > 0 {
-                if map[current.y1][current.x1-1] != 1 {
-                    queue.append(Move(dontMoves: [.right], current: moveLeft(current: current), count:count+1))
-                    
-                }
-            }
-        }
+        queue.append(Current(x:current.x-1 , y: current.y,d: current.d,dontMove: current.dontMove + [.left],count: current.count))
     case .right:
-        if isVertical(current: current) {
-            if current.y2 < n {
-                if map[current.y2+1][current.x1] != 1 {
-                    queue.append(Move(dontMoves: [.left], current: moveDown(current: current), count:count+1))
-                }
-            }
-        }else {
-            if current.x2 < n && map[current.y2][current.x2+1] != 1 {
-                queue.append(Move(dontMoves: [.left], current: moveRight(current: current), count:count+1))
-            }
-            
-        }
+        queue.append(Current(x:current.x+1 , y: current.y,d: current.d,dontMove: current.dontMove+[.right],count: current.count + 1))
     case .rotateClockwise1:
-        if isVertical(current: current) {
-            if current.x1 > 0 {
-                if map[current.y1][current.x1-1] != 1 && map[current.y2][current.x2-1] != 1 {
-                    queue.append(Move(dontMoves: [.rotateClockwise1,.rotateAntiClockwise1,.rotateClockwise2,.rotateAntiClockwise2], current: rotateVerticalClockwise1(current: current), count:count+1))
-                }
-            }
-        }else {
-            if current.y1 < n {
-                if map[current.y1+1][current.x1] != 1 && map[current.y2+1][current.x2] != 1 {
-                    queue.append(Move(dontMoves:[.rotateClockwise1,.rotateAntiClockwise1,.rotateClockwise2,.rotateAntiClockwise2], current: rotateClockwise1(current: current), count:count+1))
-                }
-            }
-        }
-    case  .rotateAntiClockwise1:
-        if isVertical(current: current) {
-            if current.x1 < n {
-                if map[current.y1][current.x1+1] != 1 && map[current.y2][current.x2+1] != 1 {
-                    queue.append(Move(dontMoves:[.rotateClockwise1,.rotateAntiClockwise1,.rotateClockwise2,.rotateAntiClockwise2], current: rotateVerticalAntiClockwise1(current: current), count:count+1))
-                }
-            }
-        }else {
-            if current.y1 > 0 {
-                if map[current.y1-1][current.x1] != 1 && map[current.y2-1][current.x2] != 1 {
-                    queue.append(Move(dontMoves: [.rotateClockwise1,.rotateAntiClockwise1,.rotateClockwise2,.rotateAntiClockwise2], current: rotateAntiClockwise1(current: current), count:count+1))
-                }
-            }
-        }
+        queue.append(Current(x:current.x , y: current.y,d: nextDirection1,dontMove: current.dontMove+[.rotateClockwise1],count: current.count+1))
+    case .rotateAntiClockwise1:
+        queue.append(Current(x:current.x , y: current.y,d: pastDirection1,dontMove: current.dontMove+[.rotateAntiClockwise1],count: current.count+1))
     case .rotateClockwise2:
-        if isVertical(current: current) {
-            if current.x1 < n {
-                if map[current.y1][current.x1+1] != 1 && map[current.y2][current.x2+1] != 1 {
-                    queue.append(Move(dontMoves: [.rotateClockwise1,.rotateAntiClockwise1,.rotateClockwise2,.rotateAntiClockwise2], current: rotateVerticalClockwise2(current: current), count:count+1))
-                }
-            }
-        }else {
-            if current.y1 > 0 {
-                if map[current.y1-1][current.x1] != 1 && map[current.y2-1][current.x2] != 1 {
-                    queue.append(Move(dontMoves:[.rotateClockwise1,.rotateAntiClockwise1,.rotateClockwise2,.rotateAntiClockwise2], current: rotateClockwise2(current: current), count:count+1))
-                }
-            }
-        }
+        queue.append(Current(x:current.x+direction[current.d][0] , y: current.y+direction[current.d][1],d: nextDirection2,dontMove: current.dontMove+[.rotateClockwise2],count: current.count+1))
     case .rotateAntiClockwise2:
-        if isVertical(current: current) {
-            if current.x1 > 0 {
-                if map[current.y1][current.x1-1] != 1 && map[current.y2][current.x2-1] != 1 {
-                    queue.append(Move(dontMoves: [.rotateClockwise1,.rotateAntiClockwise1,.rotateClockwise2,.rotateAntiClockwise2], current: rotateVerticalAntiClockwise2(current: current), count:count+1))
-                }
-            }
-        }else {
-            if current.y1 < n {
-                if map[current.y1+1][current.x1] != 1 && map[current.y2+1][current.x2] != 1 {
-                    queue.append(Move(dontMoves:[.rotateClockwise1,.rotateAntiClockwise1,.rotateClockwise2,.rotateAntiClockwise2], current: rotateAntiClockwise2(current: current), count:count+1))
-                }
-            }
-        }
+        queue.append(Current(x:current.x+direction[current.d][0], y:current.y+direction[current.d][1],d: pastDirection2,dontMove: current.dontMove+[.rotateAntiClockwise2],count: current.count+1))
     }
 }
 
