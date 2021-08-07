b1875ecdc65e32c57fde1c7d4e7122fab795c401
Fomagran <fomagran6@naver.com>
2021-08-07 17:37:39 +0900

    프로그래머스 2021 카카오 인턴쉽 거리두기 확인하기 풀이완료!

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,25 +1,61 @@
-func solution(_ n:Int, _ t:Int, _ m:Int, _ timetable:[String]) -> String {
-    var startTime = 540
-    var times:[Int] = timetable.map{Int($0.split(separator: ":")[0])!*60 + Int($0.split(separator: ":")[1])!}.sorted(by:<)
-    var people:Int = 0
-    var last:Int = startTime
-    var hour:String = ""
-    var minute:String = ""
-    
-    for _ in 0..<n {
-        people = m
-        for _ in 0..<m {
-            if times.isEmpty || times.first! > startTime { break }
-            last = times.removeFirst()
-            people -= 1
+typealias Seat = (Int,Int)
+
+func solution(_ places:[[String]]) -> [Int] {
+    var answer:[Int] = []
+    for place in places {
+        var check:Bool = false
+        var applicants:[Seat] = []
+        var xMap:[[String]] = []
+        getSeatLocation(places: place,xMap: &xMap,applicants: &applicants)
+        outer:while !applicants.isEmpty {
+            let first = applicants.removeFirst()
+            for applicant in applicants {
+                if getManhattanDistance(p1: first, p2: applicant) <= 2 {
+                    if !isKeepDistance(p1: first, p2: applicant,xMap: &xMap) {
+                        answer.append(0)
+                        check = true
+                        break outer
+                    }
+                }
+            }
         }
-        startTime += t
+        if !check {
+            answer.append(1)
+        }
     }
-    
-    startTime = people == 0 ? last-1 : startTime-t
-    hour = startTime/60  < 10 ? "0\(startTime/60)" : "\(startTime/60)"
-    minute = startTime%60 < 10 ? "0\(startTime%60)" : "\(startTime%60)"
-    return hour + ":" + minute
+    return answer
 }
 
-solution(10, 60, 45, ["23:59","23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59"])
+func getSeatLocation(places:[String],xMap:inout [[String]],applicants:inout [Seat]) {
+    for (y,place) in places.enumerated() {
+        xMap.append(place.map{String($0)})
+        for (x,p) in place.map({String($0)}).enumerated() {
+            if p == "P" {
+                applicants.append((x,y))
+            }
+        }
+    }
+}
+
+func getManhattanDistance(p1:Seat,p2:Seat) -> Int {
+    return abs(p1.0-p2.0) + abs(p1.1-p2.1)
+}
+
+func isKeepDistance(p1:Seat,p2:Seat,xMap:inout [[String]]) -> Bool {
+    let minX = p1.0 < p2.0 ? p1 : p2
+    let minY = p1.1 < p2.1 ? p1 : p2
+    if p1.0 == p2.0 {
+        return xMap[minY.1+1][p1.0] == "X"
+    }else if p1.1 == p2.1 {
+        return xMap[p1.1][minX.0+1] == "X"
+    }else {
+        if p1.0 < p2.0 {
+            return xMap[p1.1][p1.0+1] == "X" && xMap[p2.1][p2.0-1] == "X"
+        }else {
+            return xMap[p2.1][p2.0+1] == "X" && xMap[p1.1][p1.0-1] == "X"
+        }
+    }
+}
+
+
+solution([["OOOOO", "OOOOO", "OOOOP", "OOOPX", "OOPXP"]])
