9dcdb80dd22fac30710ed0741ee68991eb304f1a
Fomagran <fomagran6@naver.com>
2021-08-08 19:58:32 +0900

    2020 KAKAO BLIND RECRUITMENT  기둥과 보 설치 1차

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,61 +1,121 @@
-typealias Seat = (Int,Int)
+import Foundation
 
-func solution(_ places:[[String]]) -> [Int] {
-    var answer:[Int] = []
-    for place in places {
-        var check:Bool = false
-        var applicants:[Seat] = []
-        var xMap:[[String]] = []
-        getSeatLocation(places: place,xMap: &xMap,applicants: &applicants)
-        outer:while !applicants.isEmpty {
-            let first = applicants.removeFirst()
-            for applicant in applicants {
-                if getManhattanDistance(p1: first, p2: applicant) <= 2 {
-                    if !isKeepDistance(p1: first, p2: applicant,xMap: &xMap) {
-                        answer.append(0)
-                        check = true
-                        break outer
-                    }
+var result:[[Int]] = []
+var pillars:[[Bool]] = []
+var papers:[[Bool]] = []
+
+func solution(_ n:Int, _ build_frame:[[Int]]) -> [[Int]] {
+    pillars = Array(repeating: Array(repeating: false, count: 4), count: n)
+    papers = Array(repeating: Array(repeating: false, count: 4), count: n)
+    
+    for frame in build_frame {
+        if frame[3] == 0 {
+            if frame[2] == 0 {
+                if isRemovablePillar(x: frame[0], y: frame[1]) {
+                    pillars[frame[1]][frame[0]] = false
+                }
+            }else {
+                if isRemovablePaper(x: frame[0], y: frame[1]) {
+                    papers[frame[1]][frame[0]] = false
+                }
+            }
+        }else {
+            if frame[2] == 0 {
+                if isErectablePillar(x: frame[0], y: frame[1]) {
+                    pillars[frame[1]][frame[0]] = true
+                }
+            }else {
+                if isErectablePaper(x: frame[0], y: frame[1]) {
+                    papers[frame[1]][frame[0]] = true
                 }
             }
         }
-        if !check {
-            answer.append(1)
-        }
     }
-    return answer
-}
-
-func getSeatLocation(places:[String],xMap:inout [[String]],applicants:inout [Seat]) {
-    for (y,place) in places.enumerated() {
-        xMap.append(place.map{String($0)})
-        for (x,p) in place.map({String($0)}).enumerated() {
-            if p == "P" {
-                applicants.append((x,y))
+    
+    for (y,pillar) in pillars.enumerated() {
+        for (x,p) in pillar.enumerated() {
+            if p {
+                result.append([x,y,0])
             }
         }
     }
-}
-
-func getManhattanDistance(p1:Seat,p2:Seat) -> Int {
-    return abs(p1.0-p2.0) + abs(p1.1-p2.1)
-}
-
-func isKeepDistance(p1:Seat,p2:Seat,xMap:inout [[String]]) -> Bool {
-    let minX = p1.0 < p2.0 ? p1 : p2
-    let minY = p1.1 < p2.1 ? p1 : p2
-    if p1.0 == p2.0 {
-        return xMap[minY.1+1][p1.0] == "X"
-    }else if p1.1 == p2.1 {
-        return xMap[p1.1][minX.0+1] == "X"
-    }else {
-        if p1.0 < p2.0 {
-            return xMap[p1.1][p1.0+1] == "X" && xMap[p2.1][p2.0-1] == "X"
-        }else {
-            return xMap[p2.1][p2.0+1] == "X" && xMap[p1.1][p1.0-1] == "X"
+    
+    for (y,paper) in papers.enumerated() {
+        for (x,p) in paper.enumerated() {
+            if p {
+                result.append([x,y,1])
+            }
         }
+    }
+    
+    result.sort {
+        if $0[0] == $1[0] {
+            if $0[1] == $1[1] {
+                return $0[2] < $1[2]
+            }else {
+                return $0[1] < $1[1]
+            }
+        }else {
+            return $0[0] < $1[0]
+        }
+    }
+    
+    return result
+}
+
+func isErectablePillar(x:Int,y:Int) -> Bool{
+    return pillars[y-1][x] || papers[y][x-1] || papers[y][x] || y == 0
+}
+
+func isErectablePaper(x:Int,y:Int) -> Bool {
+    if pillars[y-1][x] || pillars[y-1][x+1] {
+        return true
+    }else {
+        return papers[y][x-1] && papers[y][x+1]
     }
 }
 
+func isRemovablePillar(x:Int,y:Int) -> Bool {
+    
+    if pillars[y+1][x] {
+        return false
+    }
+   
+    if papers[y+1][x] {
+        if !papers[y+1][x-1] {
+            return false
+        }
+    }
+    
+    if papers[y+1][x-1] {
+        if !papers[y+1][x+1] {
+            return false
+        }
+    }
+    
+    return true
+}
 
-solution([["OOOOO", "OOOOO", "OOOOP", "OOOPX", "OOPXP"]])
+func isRemovablePaper(x:Int,y:Int) -> Bool {
+    
+    if papers[y][x+1] || pillars[y][x+1] {
+        return false
+    }
+    
+    if papers[y][x-1]{
+       if  !pillars[y-1][x-1] {
+            return false
+        }
+    }
+    
+    if papers[y][x+1]{
+       if  !pillars[y-1][x+2] {
+            return false
+        }
+    }
+    
+    return true
+}
+
+solution(5, [[1,0,0,1],[1,1,1,1],[2,1,0,1],[2,2,1,1],[5,0,0,1],[5,1,0,1],[4,2,1,1],[3,2,1,1]])
+//solution(5, [[0,0,0,1],[2,0,0,1],[4,0,0,1],[0,1,1,1],[1,1,1,1],[2,1,1,1],[3,1,1,1],[2,0,0,0],[1,1,1,0],[2,2,0,1]])
