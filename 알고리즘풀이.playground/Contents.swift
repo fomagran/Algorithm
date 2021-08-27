31a3a604eea63600f1af20b4c79af88b8659a064
Fomagran <fomagran6@naver.com>
2021-08-27 18:55:38 +0900

    N-Queen 3차 통과

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,186 +1,91 @@
-//import Foundation
-//
-//var answer:Set<[[Bool]]> = []
-//
-//func solution(_ n:Int) -> Int {
-//    if n == 1 {
-//        return 1
-//    }
-//
-//    let b = Array(repeating: Array(repeating: true, count: n), count: n)
-//    for (y,row) in b.enumerated() {
-//        for (x,column) in row.enumerated() {
-//            if column {
-//                var history = Array(repeating: Array(repeating: true, count: n), count: n)
-//                history[y][x] = false
-//                findEnablePlace(n: n, depth: 1,history:history)
-//            }
-//        }
-//    }
-//    return answer.count
-//}
-//
-//func findEnablePlace(n:Int,depth:Int,history:[[Bool]]) {
-//    if depth == n-1 {
-//        for (x,bool) in history[n-1].enumerated() {
-//            if bool {
-//                var newHistory = history
-//                newHistory[n-1][x] = false
-//                answer.insert(newHistory)
-//            }
-//        }
-//        return
-//    }
-//
-//    for y in depth..<n {
-//        for x in 0..<n {
-//            if isEnable(history: history, x: x, y: y) {
-//                var newHistory = history
-//                newHistory[y][x] = false
-//                findEnablePlace(n: n, depth: depth+1,history: newHistory)
-//            }
-//        }
-//    }
-//}
-//
-//func isEnable(history:[[Bool]],x:Int,y:Int) -> Bool {
-//    for i in 0..<history.count {
-//        if !history[i][x] {
-//            return false
-//        }
-//    }
-//
-//    var newX1 = x
-//    var newX2 = x
-//
-//    if y < history.count - 1 {
-//        for i in y+1..<history.count {
-//            if newX1 > 0 {
-//                newX1 -= 1
-//                if !history[i][newX1] {
-//                    return false
-//                }
-//            }
-//
-//            if newX2 < history.count - 1 {
-//                newX2 += 1
-//                if !history[i][newX2] {
-//                    return false
-//                }
-//            }
-//        }
-//    }
-//
-//    newX1 = x
-//    newX2 = x
-//
-//    if y > 0 {
-//        for i in stride(from: y-1, through: 0, by: -1) {
-//            if newX1 > 0 {
-//                newX1 -= 1
-//                if !history[i][newX1] {
-//                    return false
-//                }
-//            }
-//
-//            if newX2 < history.count-1 {
-//                newX2 += 1
-//                if !history[i][newX2] {
-//                    return false
-//                }
-//            }
-//        }
-//    }
-//
-//    return true
-//}
-
 import Foundation
 
-var answer:Set<[[Bool]]> = []
+var answer:Int = 0
 
 func solution(_ n:Int) -> Int {
     if n == 1 { return 1 }
     let board = Array(repeating: Array(repeating: true, count: n), count: n)
         for (x,column) in board[0].enumerated() {
             if column {
-                let newBoard = makeVisitPlace(board: board, x: x, y: 0)
                 var history = Array(repeating: Array(repeating: true, count: n), count: n)
                 history[0][x] = false
-                findEnablePlace(n: n, depth: 1, board: newBoard,history:history)
+                findEnablePlace(n: n, depth: 1,history:history)
             }
         }
-    return answer.count
+    return answer
 }
 
-func findEnablePlace(n:Int,depth:Int,board:[[Bool]],history:[[Bool]]) {
+func findEnablePlace(n:Int,depth:Int,history:[[Bool]]) {
     if depth == n-1 {
-        for (y,row) in board.enumerated() {
-            for (x,column) in row.enumerated() {
-                if column {
-                    var newHistory = history
-                    newHistory[y][x] = false
-                    answer.insert(newHistory)
-                }
+        for i in 0..<n {
+            if isEnable(history: history, x: i, y: depth) {
+                answer += 1
             }
         }
         return
     }
-    for (y,row) in board.enumerated() {
-        for (x,column) in row.enumerated() {
-            if column {
-                var newHistory = history
-                newHistory[y][x] = false
-                let newBoard = makeVisitPlace(board: board, x: x, y: y)
-                findEnablePlace(n: n, depth: depth+1, board: newBoard,history: newHistory)
-            }
+    
+    for x in 0..<n {
+        if isEnable(history: history, x: x, y:depth) {
+            var newHistory = history
+            newHistory[depth][x] = false
+            findEnablePlace(n: n, depth: depth+1,history: newHistory)
         }
     }
+
 }
 
-func makeVisitPlace(board:[[Bool]],x:Int,y:Int) -> [[Bool]] {
-    var newBoard = board
-    
-    newBoard[y] = Array(repeating:false, count:board.count)
-    
-    for i in 0..<board.count {
-        newBoard[i][x] = false
+func isEnable(history:[[Bool]],x:Int,y:Int) -> Bool {
+    for i in 0..<history.count {
+        if !history[i][x] {
+            return false
+        }
     }
-    
+
     var newX1 = x
     var newX2 = x
-    
-    if y < board.count - 1 {
-        for i in y+1..<board.count {
+
+    if y < history.count - 1 {
+        for i in y+1..<history.count {
             if newX1 > 0 {
                 newX1 -= 1
-                newBoard[i][newX1] = false
+                if !history[i][newX1] {
+                    return false
+                }
             }
-            
-            if newX2 < board.count - 1 {
+
+            if newX2 < history.count - 1 {
                 newX2 += 1
-                newBoard[i][newX2] = false
+                if !history[i][newX2] {
+                    return false
+                }
             }
         }
     }
-    
+
     newX1 = x
     newX2 = x
-    
+
     if y > 0 {
         for i in stride(from: y-1, through: 0, by: -1) {
             if newX1 > 0 {
                 newX1 -= 1
-                newBoard[i][newX1] = false
+                if !history[i][newX1] {
+                    return false
+                }
             }
-            
-            if newX2 < board.count-1 {
+
+            if newX2 < history.count-1 {
                 newX2 += 1
-                newBoard[i][newX2] = false
+                if !history[i][newX2] {
+                    return false
+                }
             }
         }
     }
-    return newBoard
+
+    return true
 }
+
 
 solution(4)
