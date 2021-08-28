e0f101e947c449340e729e58a679ab7137947b64
Fomagran <fomagran6@naver.com>
2021-08-28 20:39:57 +0900

    [add] 프로그래머스 위클리 챌린지 4주차 직업군 추천하기 풀이 완료!

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,49 +1,52 @@
 import Foundation
 
-func solution(_ n:Int) -> Int {
-    //가능한 배치 수
-    var answer:Int = 0
-    //퀸이 놓인 위치를 기억할 2차원 배열
-    let history = Array(repeating: Array(repeating: true, count: n), count: n)
-    //가능한 배치를 찾는다
-    findEnableArrange(n: n, depth: 0,history:history,answer:&answer)
-    return answer
+func solution(_ table:[String], _ languages:[String], _ preference:[Int]) -> String {
+    let jobs = setContentsLanguageScore(table: table)
+    let scores:[String:Int] = setLangaugeScoreAndPreferenceSum(languages: languages, preference: preference, jobs: jobs)
+    return findMaxJob(scores: scores)
 }
 
-func findEnableArrange(n:Int,depth:Int,history:[[Bool]],answer:inout Int) {
-    //가장 마지막까지 왔을 경우
-    if depth == n {
-        //배치 가능한 것이므로 +1 해줌
-        answer += 1
-        return
-    }
-    //현재 행에 퀸을 놓을 수 있는지 확인
-    for x in 0..<n {
-        //공격하지 못한다면
-        if isUnableAttack(history: history, x: x, y:depth) {
-            var newHistory = history
-            newHistory[depth][x] = false
-            //dfs로 진행
-            findEnableArrange(n: n, depth: depth+1,history: newHistory,answer: &answer)
+func setContentsLanguageScore(table:[String]) -> [String:[String:Int]] {
+    var jobs:[String:[String:Int]] = [:]
+    table.forEach {
+        let split = $0.split(separator: " ")
+        let job = String(split[0])
+        for (i,lang) in split.enumerated() {
+            if i == 0 {
+                jobs[job] = [:]
+                continue
+            }
+            jobs[job]![String(lang)] = 6-i
         }
     }
+    return jobs
 }
-//공격이 불가능한지 체크한다
-func isUnableAttack(history:[[Bool]],x:Int,y:Int) -> Bool {
-    for i in 0..<history.count {
-        //세로 확인
-        if !history[i][x]  {
-            return false
-        //대각선으로 왼쪽 확인
-        }else if 0..<history.count ~= x-abs(y-i) && !history[i][x-abs(y-i)] {
-            return false
-        //대각선으로 오른쪽 확인
-        }else if 0..<history.count ~= x+abs(y-i) && !history[i][x+abs(y-i)] {
-            return false
+
+func setLangaugeScoreAndPreferenceSum(languages:[String],preference:[Int],jobs:[String:[String:Int]]) -> [String:Int] {
+    var scores:[String:Int] = [:]
+    for (i,lang) in languages.enumerated() {
+        for key in jobs.keys {
+            let jobScore = jobs[key]?[lang] == nil ? 0 : jobs[key]![lang]!
+            if scores[key] == nil {
+                scores[key] = preference[i] * jobScore
+            }else {
+                scores[key]! += preference[i] * jobScore
+            }
         }
     }
-    return true
+    return scores
 }
 
+func findMaxJob(scores:[String:Int]) -> String {
+    var answer:[String] = []
+    let max = scores.values.max()
+    for score in scores {
+        if score.value == max {
+            answer.append(score.key)
+        }
+    }
+    return answer.sorted().first!
+}
 
-solution(4)
+solution(["SI JAVA JAVASCRIPT SQL PYTHON C#", "CONTENTS JAVASCRIPT JAVA PYTHON SQL C++", "HARDWARE C C++ PYTHON JAVA JAVASCRIPT", "PORTAL JAVA JAVASCRIPT PYTHON KOTLIN PHP", "GAME C++ C# JAVASCRIPT C JAVA"], ["PYTHON", "C++", "SQL"], [7,5,5])
+
