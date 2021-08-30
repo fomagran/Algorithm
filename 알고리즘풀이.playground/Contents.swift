18310e75f5ed6428f02de282d15b4fedee326c58
Fomagran <fomagran6@naver.com>
2021-08-30 15:14:38 +0900

    [add] 프로그래머스 위클리 챌린지 5주차 모음 사전 풀이완료!

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,52 +1,10 @@
 import Foundation
 
-func solution(_ table:[String], _ languages:[String], _ preference:[Int]) -> String {
-    let jobs = setContentsLanguageScore(table: table)
-    let scores:[String:Int] = setLangaugeScoreAndPreferenceSum(languages: languages, preference: preference, jobs: jobs)
-    return findMaxJob(scores: scores)
+func solution(_ word:String) -> Int {
+    let vowelsDic = ["A":0,"E":1,"I":2,"O":3,"U":4]
+    let count = [781,156,31,6,1]
+
+    return word.enumerated().map{count[$0.offset]*vowelsDic[String($0.element)]!}.reduce(word.count) {$0 + $1}
 }
 
-func setContentsLanguageScore(table:[String]) -> [String:[String:Int]] {
-    var jobs:[String:[String:Int]] = [:]
-    table.forEach {
-        let split = $0.split(separator: " ")
-        let job = String(split[0])
-        for (i,lang) in split.enumerated() {
-            if i == 0 {
-                jobs[job] = [:]
-                continue
-            }
-            jobs[job]![String(lang)] = 6-i
-        }
-    }
-    return jobs
-}
-
-func setLangaugeScoreAndPreferenceSum(languages:[String],preference:[Int],jobs:[String:[String:Int]]) -> [String:Int] {
-    var scores:[String:Int] = [:]
-    for (i,lang) in languages.enumerated() {
-        for key in jobs.keys {
-            let jobScore = jobs[key]?[lang] == nil ? 0 : jobs[key]![lang]!
-            if scores[key] == nil {
-                scores[key] = preference[i] * jobScore
-            }else {
-                scores[key]! += preference[i] * jobScore
-            }
-        }
-    }
-    return scores
-}
-
-func findMaxJob(scores:[String:Int]) -> String {
-    var answer:[String] = []
-    let max = scores.values.max()
-    for score in scores {
-        if score.value == max {
-            answer.append(score.key)
-        }
-    }
-    return answer.sorted().first!
-}
-
-solution(["SI JAVA JAVASCRIPT SQL PYTHON C#", "CONTENTS JAVASCRIPT JAVA PYTHON SQL C++", "HARDWARE C C++ PYTHON JAVA JAVASCRIPT", "PORTAL JAVA JAVASCRIPT PYTHON KOTLIN PHP", "GAME C++ C# JAVASCRIPT C JAVA"], ["PYTHON", "C++", "SQL"], [7,5,5])
-
+solution("I")
