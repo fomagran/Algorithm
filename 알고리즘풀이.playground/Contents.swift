af20680cccf358529b3d750299928421abcd3535
Fomagran <fomagran6@naver.com>
2021-09-03 23:42:11 +0900

    [add] 2019 KAKAO BLIND RECRUITMENT 매칭 점수 1차 95점 8번만 틀린다

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,34 +1,75 @@
 import Foundation
 
-struct Car {
-    var x:Int,y:Int,price:Int,prev:Direction?
+struct Page {
+    var index:Int
+    var url:String
+    var score:Float
+    var externalLinks:[String]
+    var linkScore:Float
 }
 
-enum Direction {
-    case left,right,up,down
-}
+func solution(_ word:String, _ pages:[String]) -> Int {
+    
+    var pageInfos:[Page] = []
+    for (i,page) in pages.enumerated() {
+        var pageInfo = Page(index:i,url: "", score: 0, externalLinks: [], linkScore: 0)
+       let tags = page.split(separator: "<")
+        var linkCount = 0
+        var score:Float = -1
+  
 
-func solution(_ board:[[Int]]) -> Int {
-    var visited:[[Int]] = Array(repeating: Array(repeating:Int.max, count: board.count), count: board.count)
-    dfs(car: Car(x: 1, y: 0, price: 100, prev:.right),board: board,visited: &visited)
-    dfs(car: Car(x: 0, y: 1, price: 100, prev:.down),board:board,visited: &visited)
-    return visited.last!.last!
-}
-
-func dfs(car:Car,board:[[Int]],visited:inout [[Int]]) {
-    let direction:[Direction:(Int,Int)] = [.right:(1,0),.left:(-1,0),.down:(0,1),.up:(0,-1)]
-    if isEnabled(car: car, board: board, visited: visited) {
-        visited[car.y][car.x] = car.price
-        for (key,value) in direction {
-            var newCar = Car(x: car.x + value.0, y: car.y+value.1, price: car.price, prev: key)
-            newCar.price += car.prev == key ? 100 : 600
-            dfs(car: newCar, board: board, visited: &visited)
+        for tag in tags {
+            if String(tag.prefix(6)) == "meta p" {
+                let content = tag.split(separator: " ").last!
+                let url = content.split(separator: "\"")[1]
+                pageInfo.url = String(url)
+            }
+            if String(tag.prefix(5)) == "/body" {
+                pageInfo.score = score
+                break
+            }
+            if String(tag.prefix(6)) == "a href" {
+                let url = tag.split(separator: "\"")[1]
+                pageInfo.externalLinks.append(String(url))
+                linkCount += 1
+            }
+            
+            if String(tag.prefix(4)) == "body" {
+                score += 1
+            }
+            
+            if score > -1 {
+                let contains = tag.split(separator: " ").filter{$0.lowercased().contains(word.lowercased())}
+                for contain in contains {
+                    let new = contain.map{$0.isLetter ? String($0.lowercased()) : " "}.joined().split(separator: " ")
+                   score += Float(new.filter{$0 == word.lowercased()}.count)
+                }
+            }
+        }
+        pageInfos.append(pageInfo)
+    }
+    
+    for pageInfo in pageInfos {
+        for link in pageInfo.externalLinks {
+            let index = pageInfos.firstIndex{$0.url == link}
+            if let index = index {
+            let linkScore:Float = pageInfo.score/Float(pageInfo.externalLinks.count)
+            pageInfos[index].linkScore += linkScore
+            }
         }
     }
+    
+    let max = pageInfos.sorted{($0.score + $0.linkScore) > ($1.score + $1.linkScore)}.first
+
+    let maxScore = max!.score + max!.linkScore
+    
+    for pageInfo in pageInfos {
+        if pageInfo.score + pageInfo.linkScore == maxScore {
+            return pageInfo.index
+        }
+    }
+    
+    return 0
 }
 
-func isEnabled(car:Car,board:[[Int]],visited:[[Int]]) -> Bool {
-    return 0..<board.count ~= car.x && 0..<board.count ~= car.y && car.price <= visited[car.y][car.x] && board[car.y][car.x] == 0
-}
-
-solution([[0, 0, 0, 0, 0], [0, 1, 1, 1, 0], [0, 0, 1, 0, 0], [1, 0, 0, 0, 1], [0, 1, 1, 0, 0]])
+solution("Muzi", ["<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://careers.kakao.com/interview/list\"/>\n</head>  \n<body>\n<a href=\"https://programmers.co.kr/learn/courses/4673\"></a>#!MuziMuzi!)jayg07con&&\n\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://www.kakaocorp.com\"/>\n</head>  \n<body>\ncon%\tmuzI92apeach&2<a href=\"https://hashcode.co.kr/tos\"></a>\n\n\t^\n</body>\n</html>"])
