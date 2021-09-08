fc927e7b23bd2cefd97b92239820b188b25a3b46
Fomagran <fomagran6@naver.com>
2021-09-08 18:22:36 +0900

    [add] 2020 KAKAO BLIND RECRUITMENT 외벽 점검 풀이완료

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,53 +1,21 @@
 import Foundation
 
-struct Weak {
-    let dist:[Int]
-    let weak:[Int]
-    let current:Int
-    let count:Int
-}
-
-var answer:Int = Int.max
-var distCount:Int = 0
-var N:Int = 0
-
 func solution(_ n:Int, _ weak:[Int], _ dist:[Int]) -> Int {
-    distCount = dist.count
-    N = n
-    var reverseDist = Array(dist.reversed())
-    let first = reverseDist.removeFirst()
-    dfs(w: Weak(dist: reverseDist, weak: weak, current: first, count: 0))
-    return answer == Int.max ? -1 : answer
+    var weaks:[Set<[Int]>] = [[weak]]
+    for (friendCount,d) in dist.reversed().enumerated() {
+        var newWeaks:Set<[Int]> = []
+        for w in weaks.first! {
+            for start in w {
+                let end = (n+(start-d))%n
+                let filterWeaks = start > end ? w.filter{$0 < end || start+1...n ~= $0 } : w.filter{ start+1..<end ~= $0 }
+                if filterWeaks.isEmpty { return friendCount+1 }
+                newWeaks.insert(filterWeaks)
+            }
+        }
+        weaks[0] = newWeaks
+    }
+    return -1
 }
 
-func dfs(w:Weak) {
-    if w.count >= answer { return }
-    if w.dist.isEmpty { return }
-    if w.weak.isEmpty {
-        answer = min(answer,w.count+1)
-        return
-    }
-    var newDist = w.dist
-    let dist = newDist.removeFirst()
-    
-    for n in w.weak {
-        var filterClock:[Int] = []
-        if n < (n+w.current)%N {
-            filterClock = w.weak.filter{$0 < n || (n+w.current)%N+1...N ~= $0 }
-        }else {
-            filterClock = w.weak.filter{(n+w.current)%N+1..<n ~= $0 }
-        }
-        dfs(w: Weak(dist: newDist, weak: filterClock, current: dist, count: w.count + 1))
-        var filterAntiClock:[Int] = []
-        let other = (N+(n-w.current))%N
-        if n > (N+(n-w.current))%N {
-            filterAntiClock = w.weak.filter{$0 < other || n+1...N ~= $0 }
-        }else {
-            filterAntiClock = w.weak.filter{ n+1..<other ~= $0 }
-        }
-        dfs(w: Weak(dist: newDist, weak: filterAntiClock, current: dist, count: w.count + 1))
-    }
-}
-
-solution(12,[1, 3, 4, 9, 10],[3, 5, 7])
 //solution(12,[1, 5, 6, 10],[1,2,3,4])
+solution(12,[1, 3, 4, 9, 10],[3, 5, 7])
