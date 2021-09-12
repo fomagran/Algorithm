ea27a96df9b8aacbabbd9fe74f9a126e0ae0149c
Fomagran <fomagran6@naver.com>
2021-09-12 18:59:19 +0900

    [add] 프로그래머스 다단계 칫솔 판매 풀이 완료!

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,109 +1,32 @@
-import Foundation
-
-func solution(_ board:[[Int]], _ skill:[[Int]]) -> Int {
-    var answer = 0
-    var b = board
-    var visit:[[Bool]] = Array(repeating: Array(repeating: false, count: board[0].count), count: board.count)
-
-    for (y,row) in b.enumerated() {
-        for (x,col) in row.enumerated() {
-            if col > 0 {
-                if !visit[y][x] {
-                    answer += 1
-                }
-                visit[y][x] = true
-            }else {
-                if visit[y][x] {
-                    answer -= 1
-                }
-                visit[y][x] = false
-            }
-        }
-    }
-
-    for s in skill {
-        let p = s[0] == 1 ? -s[5] : s[5]
-        for y in s[2]...s[4] {
-            for x in s[1]...s[3] {
-                b[x][y] += p
-                if b[x][y] > 0 {
-                    if !visit[x][y] {
-                        answer += 1
-                    }
-                    visit[x][y] = true
-                }else {
-                    if visit[x][y] {
-                        answer -= 1
-                    }
-                    visit[x][y] = false
-                }
-            }
-        }
-    }
-    return answer
-}
-
-solution([[1,2,3],[4,5,6],[7,8,9]], [[1,1,1,2,2,4],[1,0,0,1,1,2],[2,2,0,2,0,100]])
-solution([[5,5,5,5,5],[5,5,5,5,5],[5,5,5,5,5],[5,5,5,5,5]],     [[1,0,0,3,4,4],[1,2,0,2,3,2],[2,1,0,3,1,2],[1,0,1,3,3,1]])
-
 /*
- 10점부터 n발
+ 자식:부모 딕셔너리 만들어서 정보 저장
+ 판매자:금액 딕셔너리
+ enroll 이름순대로 result 반환
  */
 
-var answers:[[Int]] = []
+import Foundation
 
-func solution(_ n:Int, _ info:[Int]) -> [Int] {
-    let history = Array(repeating: 0, count: 11)
-    let scores = getScore(depth: 10, apeach: info, lion: history)
-    dfs(leftArrow: n, depth: 10, history: history, info: info)
-    print(answers)
-    return []
+func solution(_ enroll:[String], _ referral:[String], _ seller:[String], _ amount:[Int]) -> [Int] {
+    var parentDic:[String:String] = [:]
+    var profitDic:[String:Int] = [:]
+    
+    for i in 0..<enroll.count {
+        profitDic[enroll[i]] = 0
+        parentDic[enroll[i]] = referral[i]
+    }
+    
+    for i in 0..<seller.count {
+        var tenPercent:Int = amount[i]*100
+        var parent:String = seller[i]
+        while parent != "-" && tenPercent > 0 {
+            profitDic[parent]! += tenPercent - tenPercent/10
+            parent = parentDic[parent]!
+            tenPercent = tenPercent/10
+        }
+    }
+    return enroll.map{profitDic[$0]!}
 }
 
-func dfs(leftArrow:Int,depth:Int,history:[Int],info:[Int]) {
-    var newHistory = history
+solution(["john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"], ["-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"], ["young", "john", "tod", "emily", "mary"], [12, 4, 2, 5, 10])
 
-    if leftArrow == 0 {
-        answers.append(newHistory)
-        return
-    }
-
-    if depth == 0 {
-        newHistory[newHistory.count-1] = leftArrow
-        answers.append(newHistory)
-        return
-    }
-
-    for i in 0...leftArrow {
-        newHistory[depth] = i
-        if leftArrow - i < 0 {
-            return
-        }
-        let scores = getScore(depth: depth, apeach: info, lion: newHistory)
-        if scores[0] < scores[1] + (0...depth-1).map({$0}).reduce(0, { $0 + $1}) {
-        dfs(leftArrow: leftArrow-i, depth: depth-i, history: newHistory, info: info)
-        }
-    }
-}
-
-func getScore(depth:Int,apeach:[Int],lion:[Int]) -> [Int] {
-
-    var scores = Array(repeating: 0, count: 2)
-
-    let reversedApeach = Array(apeach.reversed())
-    let reversedLion = Array(lion.reversed())
-
-    for i in 0..<11-depth {
-        if reversedApeach[i] == 0 && reversedLion[i] == 0 {
-            continue
-        }
-        if reversedApeach[i] < reversedLion[i] {
-            scores[1] += 10-i
-        }else {
-            scores[0] += 10-i
-        }
-    }
-    return scores
-}
-//
-solution(5,[2,1,1,1,0,0,0,0,0,0,0])
+solution(["john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"], ["-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"], ["sam", "emily", "jaimie", "edward"], [2, 3, 5, 4])
