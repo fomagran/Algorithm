aab131fd4f82196ad9c4bce4898c42cdd2a6c757
Fomagran <fomagran6@naver.com>
2021-09-24 15:58:43 +0900

    [add] 프로그래머스 빛의 경로 사이클 풀이 완료!!

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,12 +1,6 @@
 import Foundation
 
-/*
- 각 칸마다 위,아래,왼쪽,오른쪽으로 빛을 쐈을 때 사이클을 구한다.
- 전 방향이 위,아래,왼쪽,오른쪽일 경우 R,L이 어떻게 변할지를 구한다.
- 칸과 방향의 방문이력을 체크해서 무한루프를 막는다.
- */
-
-enum Direction {
+enum Direction{
     case up,down,left,right
     var value:(Int,Int) {
         switch self {
@@ -23,82 +17,45 @@ enum Direction {
 }
 
 struct Location:Equatable {
-    var x:Int
-    var y:Int
-    var d:Direction
+    var x:Int,y:Int,d:Direction
 }
 
-let nextL:[Direction:Direction] = [.down:.right,.right:.up,.up:.left,.left:.down]
-let nextR:[Direction:Direction] = [.down:.left,.right:.down,.up:.right,.left:.up]
-var answer:[Int] = []
-var g:[[String]] = []
-var visited:[[[Direction:Bool]]] = []
-
 func solution(_ grid:[String]) -> [Int] {
-    g = grid.map{$0.map{String($0)}}
-    visited = Array(repeating: Array(repeating:[:], count: g[0].count), count: g.count)
-    for y in 0..<g.count {
-        for x in 0..<g[0].count {
-            for d in [Direction.up,.down,.left,.right] {
-                visited[y][x][d] = false
-            }
-        }
-    }
+    let next:[String:[Direction:Direction]] = ["L":[.down:.right,.right:.up,.up:.left,.left:.down],"R":[.down:.left,.right:.down,.up:.right,.left:.up],"S":[.down:.down,.right:.right,.up:.up,.left:.left]]
+    var answer:[Int] = []
+    let g = grid.map{$0.map{String($0)}}
+    var visited = Array(repeating: Array(repeating:[Direction.up:false,Direction.down:false,Direction.right:false,Direction.left:false], count: g[0].count), count: g.count)
     
     for y in 0..<g.count {
         for x in 0..<g[0].count {
             for d in [Direction.up,.down,.left,.right] {
-                checkCycle(first: Location(x: x, y: y, d: d))
+               checkCycle(first: Location(x: x, y: y, d: d), answer: &answer, visited: &visited, g: g, next: next)
             }
         }
     }
-        
     return answer.sorted()
 }
 
-func checkCycle(first:Location) {
-    var newVisited = visited
+func checkCycle(first:Location,answer:inout [Int],visited:inout [[[Direction:Bool]]],g:[[String]],next:[String:[Direction:Direction]]) {
     var location:Location = first
-    location.x += first.d.value.0
-    location.y += first.d.value.1
-    var count:Int = 1
-    
+    var count:Int = 0
     while true {
-        if location == first {
-            answer.append(count)
-            return
-        }
-        if location.x == -1 {
-            location.x = g.count-1
-        }
-        
-        if location.x == g.count-1 {
-            location.x = 0
-        }
-        
-        if location.y == -1 {
-            location.y = g.count-1
-        }
-        
-        if location.y == g.count-1 {
-            location.y = 0
-        }
-        
-        if newVisited[location.y][location.x][location.d]! {
-           return
-        }
-        
-        newVisited[location.y][location.x][location.d] = true
+        if visited[location.y][location.x][location.d]! { break }
+        visited[location.y][location.x][location.d] = true
         count += 1
-        
-        if g[location.y][location.x] == "L" {
-            location.d = nextL[location.d]!
-        }else if g[location.y][location.x] == "R" {
-            location.d = nextR[location.d]!
-        }
+        let direction = g[location.y][location.x]
+        location.d = next[direction]![location.d]!
         location.x += location.d.value.0
         location.y += location.d.value.1
+        location.x = location.x < 0 ? g[0].count-1 : location.x%g[0].count
+        location.y = location.y < 0 ? g.count-1 : location.y%g.count
+        if location == first && count != 0{
+            answer.append(count)
+            break
+        }
     }
 }
 
 solution(["SL","LR"])
+//solution(["S"])
+//solution(["R","R"])
