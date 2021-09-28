d2a3afedefb7aa27490909818d71b4b18e96781e
Fomagran <fomagran6@naver.com>
2021-09-28 12:35:50 +0900

    [add] 삽입정렬 구현해보기

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,5 +1,26 @@
 import Foundation
 
-func solution(_ a:Int, _ b:Int, _ g:[Int], _ s:[Int], _ w:[Int], _ t:[Int]) -> Int64 {
-    return -1
+let numbers = [82,10,9,72,31,45,60]
+
+func insertionSort(nums:[Int],startIndex:Int) -> [Int] {
+    print(nums)
+    if startIndex == nums.count {
+        return nums
+    }
+    var x = startIndex-1
+    var newNums = nums
+    let current = newNums.remove(at: startIndex)
+    
+    while x >= 0 {
+        if current >= nums[x] {
+            newNums.insert(current, at: x+1)
+            return insertionSort(nums:newNums,startIndex: startIndex+1)
+        }
+        x -= 1
+    }
+    
+    newNums.insert(current, at: 0)
+    return insertionSort(nums:newNums,startIndex: startIndex+1)
 }
+
+insertionSort(nums: numbers, startIndex: 1)
