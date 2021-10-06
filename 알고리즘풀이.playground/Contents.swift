4d481cbd16a3f5b124d2d7d270657f5afc588b18
Fomagran <fomagran6@naver.com>
2021-10-06 23:39:44 +0900

    [add] 프로그래머스 위클리 챌린지 9주차 전력망을 둘로 나누기 풀이 완료!

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -6,10 +6,9 @@ import Foundation
  최솟값을 저장해두고 넘어간다면 세는 것을 그만둔다.
  */
 var visited:[[Bool]] = []
+var minCount:Int = Int.max
 func solution(_ n:Int, _ wires:[[Int]]) -> Int {
     visited = Array(repeating: Array(repeating: false, count: n+1), count:n+1)
-    
-    var minCount:Int = Int.max
     
     for wire in wires {
         visited[wire[0]][wire[1]] = true
@@ -19,26 +18,25 @@ func solution(_ n:Int, _ wires:[[Int]]) -> Int {
     for wire in wires {
         var leftCount:Int = 0
         var rightCount:Int = 0
-        dfs(start: wire[0],stop: wire[1], count: &leftCount)
-        dfs(start: wire[1],stop: wire[0] ,count: &rightCount)
+        dfs(parent: nil, start: wire[0],stop: wire[1], count: &leftCount)
+        dfs(parent: nil, start: wire[1],stop: wire[0] ,count: &rightCount)
         minCount = min(minCount,abs(leftCount-rightCount))
     }
     
-    return -1
+    return minCount
 }
 
-func dfs(start:Int,stop:Int,count:inout Int) {
-   let filter = visited[start].enumerated().filter{$0.element}.map{$0.offset}
-    
-    print(filter)
-    
+func dfs(parent:Int?,start:Int,stop:Int,count:inout Int) {
+        
+    let filter = visited[start].enumerated().filter{$0.element && $0.offset != parent}.map{$0.offset}
+        
     count += filter.count
     
     for f in filter {
-        if f == stop {
+        if f == stop{
             continue
         }
-        dfs(start: f, stop: stop, count: &count)
+        dfs(parent:start,start: f, stop: stop, count: &count)
     }
 }
 
