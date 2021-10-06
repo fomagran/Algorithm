0a2aeeac5f2303e7ba9642df2b7faf880e36c9f3
Fomagran <fomagran6@naver.com>
2021-10-06 20:32:01 +0900

    [add] 프로그래머스 위클리 챌린지 9주차 전력망을 둘로 나누기 시작

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,10 +1,5 @@
 import Foundation
 
-func solution(_ sizes:[[Int]]) -> Int {
-    let sorted = sizes.map{$0.sorted()}
-    return sorted.map{$0[0]}.max()! * sorted.map{$0[1]}.max()!
+func solution(_ n:Int, _ wires:[[Int]]) -> Int {
+    return -1
 }
-
-solution([[60, 50], [30, 70], [60, 30], [80, 40]])
-solution([[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]])
-solution([[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]]    )
