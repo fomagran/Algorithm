17ad844a9584c78d88945634e7746ab19470bebf
Fomagran <fomagran6@naver.com>
2021-10-10 23:06:36 +0900

    프로그래머스 금과 은 운반하기 풀이 완료!

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,56 +1,35 @@
 import Foundation
 
-struct Truck {
-    var gold:Int
-    var silver:Int
-    var weight:Int
-    var time:Int64
-}
-
 func solution(_ a:Int, _ b:Int, _ g:[Int], _ s:[Int], _ w:[Int], _ t:[Int]) -> Int64 {
+    var start:Int = 0
+    var end:Int = Int(10e15)
+    var answer:Int = end
     
-    var gold:Int = a
-    var silver:Int = b
-    var time:Int64 = 0
-    var waitingTrucks:[Int64:[Truck]] = [:]
-    
-    for (i,time) in t.enumerated() {
-        if waitingTrucks[Int64(time)] == nil {
-            waitingTrucks[Int64(t[i])] = [Truck(gold: g[i], silver: s[i], weight: w[i], time: Int64(t[i]))]
-        }else {
-            waitingTrucks[Int64(time)]!.append(Truck(gold: g[i], silver: s[i], weight: w[i], time: Int64(t[i])))
-        }
-    }
+    while start <= end {
+        let mid:Int = (start + end)/2
+        var gold:Int = 0
+        var silver:Int = 0
+        var total:Int = 0
         
-    while true {
-        time += 1
-        if waitingTrucks[time] != nil {
-            for truck in waitingTrucks[time]! {
-                var newTruck:Truck = truck
-                var weight:Int = truck.weight
-                if gold > 0 {
-                    gold -= truck.gold > 0 ? truck.gold > weight ? weight : truck.gold : 0
-                    if truck.gold > 0 {
-                        weight = gold < 0 ? weight + gold : weight - truck.gold
-                    }
-                    newTruck.gold -= weight
-                    gold = gold < 0 ? 0 : gold
-                }
-                if weight > 0 {
-                    silver -= truck.silver > 0 ? truck.silver > weight ? weight : truck.silver : 0
-                    newTruck.silver -= weight
-                }
-                if waitingTrucks[time + truck.time*2] == nil {
-                    waitingTrucks[time + truck.time*2] = [newTruck]
-                }else {
-                    waitingTrucks[time + truck.time*2]!.append(newTruck)
-                }
-                if gold <= 0 && silver <= 0 {
-                    return time
-                }
+        for i in 0..<s.count {
+            var count = mid/(t[i]*2)
+            
+            if mid%(t[i]*2) >= t[i] {
+                count += 1
             }
+            gold += min(g[i],w[i] * count)
+            silver += min(s[i],w[i] * count)
+            total += min(g[i] + s[i],w[i] * count)
+        }
+        
+        if gold >= a && silver >= b && total >= a+b {
+            end = mid - 1
+            answer = min(mid,answer)
+        }else {
+            start = mid + 1
         }
     }
+    return Int64(answer)
 }
 
 solution(90, 500, [70,70,0], [0,0,500], [100,100,2], [4,8,1])
