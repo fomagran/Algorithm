82eaec4d8692006ac1029c06af0be6bdb102abfe
Fomagran <fomagran6@naver.com>
2021-10-10 20:15:59 +0900

    프로그래머스 금과 은 운반하기 2차 풀이(하나 빼고 (signal: illegal instruction (core dumped)) 뜸)

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,51 +1,57 @@
 import Foundation
 
-/*
- 시간이 적은 순으로 정렬한다.
- 시간이 적은 순으로 금과 은 무게의 최대치를 담는다.
- 
- 만약 최대 무게보다 금,은의 양이 적거나 같다면 은까지 실는다.
- 
- 만약 a,b의 금,은 양이 0보다 적거나 같다면 넘어간다.
- */
-
 struct Truck {
     var gold:Int
     var silver:Int
     var weight:Int
-    let time:Int
+    var time:Int64
 }
 
 func solution(_ a:Int, _ b:Int, _ g:[Int], _ s:[Int], _ w:[Int], _ t:[Int]) -> Int64 {
     
-    var trucks:[Truck] = []
     var gold:Int = a
     var silver:Int = b
-    var time:Int = 0
+    var time:Int64 = 0
+    var waitingTrucks:[Int64:[Truck]] = [:]
     
-    for i in 0..<g.count {
-        let truck = Truck(gold: g[i], silver: s[i], weight: w[i], time: t[i])
-        trucks.append(truck)
-    }
-    
-    trucks.sort{$0.time < $1.time}
-    
-    while gold > 0 ||  silver > 0 {
-        for (i,truck) in trucks.enumerated() {
-            var newTruck:Truck = truck
-            time += truck.time
-            if truck.gold > 0 {
-                newTruck.gold -= truck.weight
-            }
-            
-            if truck.silver > 0 {
-                
-            }
-            trucks[i] = newTruck
+    for (i,time) in t.enumerated() {
+        if waitingTrucks[Int64(time)] == nil {
+            waitingTrucks[Int64(t[i])] = [Truck(gold: g[i], silver: s[i], weight: w[i], time: Int64(t[i]))]
+        }else {
+            waitingTrucks[Int64(time)]!.append(Truck(gold: g[i], silver: s[i], weight: w[i], time: Int64(t[i])))
         }
     }
-    
-    return -1
+        
+    while true {
+        time += 1
+        if waitingTrucks[time] != nil {
+            for truck in waitingTrucks[time]! {
+                var newTruck:Truck = truck
+                var weight:Int = truck.weight
+                if gold > 0 {
+                    gold -= truck.gold > 0 ? truck.gold > weight ? weight : truck.gold : 0
+                    if truck.gold > 0 {
+                        weight = gold < 0 ? weight + gold : weight - truck.gold
+                    }
+                    newTruck.gold -= weight
+                    gold = gold < 0 ? 0 : gold
+                }
+                if weight > 0 {
+                    silver -= truck.silver > 0 ? truck.silver > weight ? weight : truck.silver : 0
+                    newTruck.silver -= weight
+                }
+                if waitingTrucks[time + truck.time*2] == nil {
+                    waitingTrucks[time + truck.time*2] = [newTruck]
+                }else {
+                    waitingTrucks[time + truck.time*2]!.append(newTruck)
+                }
+                if gold <= 0 && silver <= 0 {
+                    return time
+                }
+            }
+        }
+    }
 }
 
 solution(90, 500, [70,70,0], [0,0,500], [100,100,2], [4,8,1])
+solution(10, 10, [100], [100], [7], [10])
