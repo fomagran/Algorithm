7cb6a6b8c9f3222428e43ddd424cdedb29eabe9d
Fomagran <fomagran6@naver.com>
2021-10-19 20:38:28 +0900

    [add] 프로그래머스 스티커 모으기(2) 풀이 완료!

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,36 +1,16 @@
 import Foundation
 
-func solution(_ a:Int, _ b:Int, _ g:[Int], _ s:[Int], _ w:[Int], _ t:[Int]) -> Int64 {
-    var start:Int = 0
-    var end:Int = Int(10e15)
-    var answer:Int = end
+func solution(_ sticker:[Int]) -> Int{
+    if sticker.count < 4 { return sticker.max()!}
+    var dp1:[Int] = [sticker[0],sticker[0]]
+    var dp2:[Int] = [0,sticker[1]]
     
-    while start <= end {
-        let mid:Int = (start + end)/2
-        var gold:Int = 0
-        var silver:Int = 0
-        var total:Int = 0
-        
-        for i in 0..<s.count {
-            var count = mid/(t[i]*2)
-            
-            if mid%(t[i]*2) >= t[i] {
-                count += 1
-            }
-            gold += min(g[i],w[i] * count)
-            silver += min(s[i],w[i] * count)
-            total += min(g[i] + s[i],w[i] * count)
-        }
-        
-        if gold >= a && silver >= b && total >= a+b {
-            end = mid - 1
-            answer = min(mid,answer)
-        }else {
-            start = mid + 1
-        }
+    for i in 2..<sticker.count {
+        dp1.append(max(dp1[i-2]+sticker[i],dp1[i-1]))
+        dp2.append(max(dp2[i-2]+sticker[i],dp2[i-1]))
     }
-    return Int64(answer)
+    dp1.removeLast()
+    return max(dp1.max()!,dp2.max()!)
 }
 
-solution(90, 500, [70,70,0], [0,0,500], [100,100,2], [4,8,1])
-solution(10, 10, [100], [100], [7], [10])
+solution([14,6,5,11,3,9,2,10])
