b77b62f96fde7c22a90017bd645b6a9133695027
Fomagran <fomagran6@naver.com>
2021-10-23 18:37:30 +0900

    [add] 프로그래머스 110 옮기기 시작

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,16 +1,5 @@
 import Foundation
 
-func solution(_ sticker:[Int]) -> Int{
-    if sticker.count < 4 { return sticker.max()!}
-    var dp1:[Int] = [sticker[0],sticker[0]]
-    var dp2:[Int] = [0,sticker[1]]
-    
-    for i in 2..<sticker.count {
-        dp1.append(max(dp1[i-2]+sticker[i],dp1[i-1]))
-        dp2.append(max(dp2[i-2]+sticker[i],dp2[i-1]))
-    }
-    dp1.removeLast()
-    return max(dp1.max()!,dp2.max()!)
+func solution(_ s:[String]) -> [String] {
+    return []
 }
-
-solution([14,6,5,11,3,9,2,10])
