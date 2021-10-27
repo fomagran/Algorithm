797cc81691ab92490a619a5e3c4b618dea213bda
Fomagran <fomagran6@naver.com>
2021-10-27 21:11:54 +0900

    [add] 프로그래머스 110 옮기기 85.7점 테케 4번,19번,21번 시간 초과

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -3,63 +3,45 @@ import Foundation
 func solution(_ s:[String]) -> [String] {
     var answers:[String] = []
     for str in s {
-        var new:[String] = str.map{String($0)}
+        let remove = remove110(str: str)
+        var new = remove.0
         
-        while true {
-            var last0Index = find111(str: new)
-            if last0Index == nil {
-                answers.append(new.joined())
-                break
-            }
-            
-            if remove110(str: new,last: last0Index!).0 != nil {
-                let remove = remove110(str: new,last: last0Index!)
-                new = remove.0!
-                let index = remove.1 < 0 ? last0Index! : remove.1
-                new.insert("1", at: index)
-                new.insert("1", at: index+1)
-                new.insert("0", at: index+2)
-                last0Index! = index + 3
-            }else {
-                answers.append(new.joined())
-                break
-            }
+        let index = findLast0(str: new)
+        for _ in 0..<remove.1 {
+            new.insert("110", at: index)
         }
+        answers.append(new.joined())
     }
     return answers
 }
 
-func find111(str:[String]) -> Int? {
-    if str.count < 3 {
-        return nil
-    }
-    for i in 0..<str.count - 2 {
-        if str[i...i+2].joined() == "111" {
-            return i
+func findLast0(str:[String]) -> Int {
+    for i in stride(from: str.count-1, through: 0, by: -1) {
+        if str[i] == "0" {
+            return i+1
         }
     }
-    return nil
+    return 0
 }
 
-func remove110(str:[String],last:Int) -> ([String]?,Int) {
-    var map = str
-    var back0Index = -1
-    if last >= str.count-2  {
-        return (nil,back0Index)
-    }
+func remove110(str:String) -> ([String],Int) {
+    let map = str.map{String($0)}
+    var count:Int = 0
+    var stack:[String] = []
     
-    for i in last..<str.count-2 {
-        if map[i...i+2].joined() == "110" {
-            map.remove(at: i)
-            map.remove(at: i)
-            map.remove(at: i)
-            if map.count-1 > i && map[i] == "0" {
-                back0Index = i+1
+    for i in 0..<map.count {
+        stack.append(map[i])
+        if stack.count >= 3 {
+            if stack[stack.count-3...stack.count-1] == ["1","1","0"] {
+                stack.removeLast()
+                stack.removeLast()
+                stack.removeLast()
+                count += 1
             }
-            break
         }
     }
-    return str.count == map.count ? (nil,back0Index) : (map,back0Index)
+
+    return (stack,count)
 }
 
 solution(["1110","100111100","0111111010"])
