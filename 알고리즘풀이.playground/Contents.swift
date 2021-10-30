06484f159fc4567410a689ea671acc210319691f
Fomagran <fomagran6@naver.com>
2021-10-30 17:35:28 +0900

    [add] 하노이의 탑 풀이완료

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,37 +1,20 @@
 import Foundation
 
-struct Pillar {
-    var current:[[Int]]
-    var history:[[Int]]
-}
+var answer: [[Int]] = []
 
 func solution(_ n:Int) -> [[Int]] {
-    var pillars:[[Int]] =  Array(repeating: [], count: 3)
-    pillars[0].append(contentsOf:(1...n).reversed().map{$0})
-    let first = Pillar(current: pillars ,history: [])
-    var queue:[Pillar] = [first]
-    var answer:[[Int]] = []
-    
-    while !queue.isEmpty {
-        let pillar = queue.removeFirst()
-        if pillar.current[2].count == n {
-            answer = pillar.history
-            break
-        }
-        for i in 0..<3 {
-            for j in 0..<3 {
-                let lastHistory = pillar.history.last ?? []
-                if pillar.current[i].last ?? 16 < pillar.current[j].last ?? 16 && lastHistory != [j+1,i+1] {
-                    var newPillar = pillar
-                    let last = newPillar.current[i].removeLast()
-                    newPillar.current[j].append(last)
-                    newPillar.history.append([i+1,j+1])
-                    queue.append(newPillar)
-                }
-            }
-        }
-    }
+    hanoi(n: n, from: 1, to: 3, via: 2)
     return answer
 }
 
-solution(3)
+func hanoi(n: Int, from: Int, to: Int, via: Int){
+    if n == 1 {
+        answer.append([from,to])
+    }else {
+        hanoi(n: n-1,from: from, to: via, via: to)
+        answer.append([from,to])
+        hanoi(n: n-1, from: via, to: to, via: from)
+    }
+}
+
+solution(4)
