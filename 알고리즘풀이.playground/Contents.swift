2ca554b47de6e8b80902b80ce08e3f36a504ec64
Fomagran <fomagran6@naver.com>
2021-10-31 23:51:43 +0900

    [refactoring] 프로그래머스 하노이의 탑 리팩터링

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -3,18 +3,18 @@ import Foundation
 var answer: [[Int]] = []
 
 func solution(_ n:Int) -> [[Int]] {
-    hanoi(n: n, from: 1, to: 3, via: 2)
+    move(n: n, from: 1, to: 3, via: 2)
     return answer
 }
 
-func hanoi(n: Int, from: Int, to: Int, via: Int){
+func move(n: Int, from: Int, to: Int, via: Int){
     if n == 1 {
         answer.append([from,to])
     }else {
-        hanoi(n: n-1,from: from, to: via, via: to)
+        move(n: n-1,from: from, to: via, via: to)
         answer.append([from,to])
-        hanoi(n: n-1, from: via, to: to, via: from)
+        move(n: n-1, from: via, to: to, via: from)
     }
 }
 
-solution(4)
+solution(2)
