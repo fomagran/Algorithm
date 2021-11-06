944b657548c9e42e49f4ccc40274992543a65844
Fomagran <fomagran6@naver.com>
2021-11-06 15:18:55 +0900

    [add] 프로그래머스 월간 코드 챌린지 시즌 1 스타 수열 2차(75점(

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,38 +1,30 @@
-import Foundation
-
-/*
- 새로운 숫자가 나올 때마다 딕셔너리에 바로 앞 숫자와 저장한다.
- 만약 있는 숫자라면 딕셔너리 배열에 추가해준다.
- */
-
 func solution(_ a:[Int]) -> Int {
     var subDic:[Int:[Int]] = [:]
-    var previous:Int = -1
+    if a.count == 1 { return 0}
+
+    subDic[a.first!] = Array(a[0...1])
+    var previous:Int = a[0]
+
     
-    for i in 0..<a.count-1 {
+    for i in 1..<a.count {
         let n:Int = a[i]
         if subDic[n] == nil {
-            subDic[n] = Array(a[i...i+1])
+            subDic[n] = Array(a[i-1...i])
         }else {
             if previous != n {
-                subDic[n]!.append(contentsOf:a[i...i+1])
+                subDic[n]!.append(contentsOf:a[i-1...i])
             }
         }
-        previous = n
-    }
-    
-    let n:Int = a[a.count-1]
-    if a.count > 1 && subDic[n] == nil {
-        subDic[n] = Array(a[a.count-2...a.count-1])
-    }else {
-        if a.count > 2 && a[a.count-3] != n && a[a.count-2] != n {
-            subDic[n]!.append(contentsOf:a[a.count-2...a.count-1])
+        if previous == n {
+            previous = -1
+        }else {
+            previous = n
         }
     }
-
     return subDic.sorted{$0.value.count > $1.value.count}.first?.value.count ?? 0
 }
 
 solution([0])
+solution([0,1,1,0,0,0])
 solution([5,2,3,3,5,3])
 solution([0,3,3,0,7,2,0,2,2,0])
