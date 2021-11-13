9d0ec9a702bca592eb2d55b5fa30e866314d2aa8
Fomagran <fomagran6@naver.com>
2021-11-13 23:24:09 +0900

    [add] 프로그래머스 월간 코드 챌린지 3 공 이동 시뮬레이션 1차 시간초과

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,65 +1,38 @@
-func solution(_ a:[Int]) -> Int {
-    return findMaxLength(a: a)
-}
+import Foundation
 
-func countNumbers(a:[Int]) -> [Int:Int] {
-    var countDic:[Int:Int] = [:]
-    for n in a {
-        if countDic[n] == nil {
-            countDic[n] = 1
-        }else {
-            countDic[n]! += 1
-        }
-    }
-    return countDic
-}
-
-func findLongSubsequence(n:Int,a:[Int]) -> Int {
-    var numbers = a
-    var length:Int = 0
-    if a[0] == n && a[1] != n {
-        length += 2
-        numbers[1] = n
-    }
-    for i in 1..<numbers.count-1 {
-        if a[i] == n {
-            if numbers[i-1] != n {
-                numbers[i-1] = n
-                length += 2
-            }else if numbers[i+1] != n  {
-                numbers[i+1] = n
-                length += 2
+func solution(_ n:Int, _ m:Int, _ x:Int, _ y:Int, _ queries:[[Int]]) -> Int64 {
+    var answer:Int64 = 0
+    for y1 in 0..<n {
+        for x1 in 0..<m {
+            var location:(Int,Int) = (x1,y1)
+            for q in queries {
+                let move = moveByCommand(command: q)
+                location = (location.0+move.0,location.1+move.1)
+                location.0 = max(0,location.0)
+                location.1 = max(0,location.1)
+                location.0 = min(m-1,location.0)
+                location.1 = min(n-1,location.1)
+            }
+            if location == (y,x) {
+                answer += 1
             }
         }
-    }
-    if numbers[a.count-1] == n && numbers[a.count-2] != n {
-        length += 2
-    }
-    return length
-}
-
-func findMaxLength(a:[Int]) -> Int {
-    var answer:Int = 0
-    let sortCounts = countNumbers(a: a).sorted{$0.value > $1.value}
-    for dic in sortCounts {
-        if dic.value * 2 < answer { continue }
-        let length = findLongSubsequence(n:dic.key, a:a)
-        answer = max(answer,length)
     }
     return answer
 }
 
+func moveByCommand(command:[Int]) -> (Int,Int) {
+    switch command[0] {
+    case 0:
+        return (-command[1],0)
+    case 1:
+        return (command[1],0)
+    case 2:
+        return (0,-command[1])
+    default:
+        return (0,command[1])
+    }
+}
 
-solution([0, 3, 1, 6, 0, 2, 0, 7, 1, 3, 4, 0, 5, 1, 1])
-solution([0, 0, 3, 1, 2, 1, 3, 4, 0, 1, 4])
-solution([1, 2, 3, 1, 1, 4, 5, 1])
-solution([2, 1, 1, 3, 4, 1, 1, 5])
-solution([1, 2, 1, 3, 1, 4, 1, 5])
-solution([2, 1, 3, 1, 4, 1, 5, 1])
-solution([1, 2, 1, 3, 1, 4, 1, 5, 1])
-solution([1, 2, 1, 3, 1, 1, 4, 1, 5, 1])
-solution([2, 3, 4, 1, 1, 1, 1, 5, 6, 7])
-
-
-
-
+//solution(2, 2, 0, 0, [[2,1],[0,1],[1,1],[0,1],[2,1]])
+solution(2, 5, 0, 1, [[3,1],[2,2],[1,1],[2,3],[0,1],[2,1]])
