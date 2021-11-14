ad96aedfc0d3ae8932c5c1a0346dfc048668f7e3
Fomagran <fomagran6@naver.com>
2021-11-14 23:18:00 +0900

    [add] 프로그래머스 월간 코드 챌린지 3 공 이동 시뮬레이션 3차

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,31 +1,38 @@
 import Foundation
 
 func solution(_ n:Int, _ m:Int, _ x:Int, _ y:Int, _ queries:[[Int]]) -> Int64 {
-    var gap = (0,0)
-    queries.forEach {
-        let q:(Int,Int) = move(query: $0)
-        gap = (gap.0+q.0,gap.1+q.1)
+    var xCount:Int64 = 0
+    var yCount:Int64 = 0
+    let filterX:[[Int]] = queries.filter{$0[0] == 0 || $0[0] == 1}
+    let filterY:[[Int]] = queries.filter{$0[0] == 2 || $0[0] == 3}
+
+    for i in 0..<n {
+        var location:(Int,Int) = (i,0)
+        for q in filterY {
+            let mv = move(query: q)
+            location = (location.0+mv.0,location.1+mv.1)
+            location.1 = max(0,location.1)
+            location.1 = min(n-1,location.1)
+        }
+        if location.1 == x {
+            yCount += 1
+        }
+    }
+    
+    for i in 0..<m {
+        var location:(Int,Int) = (i,0)
+        for q in filterX {
+            let mv = move(query: q)
+            location = (location.0+mv.0,location.1+mv.1)
+            location.0 = max(0,location.0)
+            location.0 = min(m-1,location.0)
+        }
+        if location.0 == y {
+            xCount += 1
+        }
     }
 
-    var maxX:Int64 = 0
-    var maxY:Int64 = 0
-    
-    if y == 0 || y == m-1{
-        maxY = Int64(min(m-1,abs(gap.0))+1)
-    }else {
-        if m - 1 - y >= abs(gap.0) {
-            maxY = 1
-        }
-    }
-    
-    if x == 0 || x == n-1 {
-        maxX = Int64(min(n-1,abs(gap.1))+1)
-    }else {
-        if n - 1 - x >= abs(gap.1) {
-            maxX = 1
-        }
-    }
-    return maxX * maxY
+    return xCount*yCount
 }
 
 func move(query:[Int]) -> (Int,Int) {
