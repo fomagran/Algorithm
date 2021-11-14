55c46552e1845d54c446a8a84366b1ac85816b0b
Fomagran <fomagran6@naver.com>
2021-11-15 01:27:05 +0900

    [add] 프로그래머스 월간 코드 챌린지3 공 이동 시뮬레이션 풀이완료!

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,38 +1,37 @@
 import Foundation
 
+struct Range {
+    var startX:Int,endX:Int,startY:Int,endY:Int
+    var getArea:Int {
+        return (endX-startX+1)*(endY-startY+1)
+    }
+}
+
+var M:Int!
+var N:Int!
+var range:Range!
+
 func solution(_ n:Int, _ m:Int, _ x:Int, _ y:Int, _ queries:[[Int]]) -> Int64 {
-    var xCount:Int64 = 0
-    var yCount:Int64 = 0
-    let filterX:[[Int]] = queries.filter{$0[0] == 0 || $0[0] == 1}
-    let filterY:[[Int]] = queries.filter{$0[0] == 2 || $0[0] == 3}
-
-    for i in 0..<n {
-        var location:(Int,Int) = (i,0)
-        for q in filterY {
-            let mv = move(query: q)
-            location = (location.0+mv.0,location.1+mv.1)
-            location.1 = max(0,location.1)
-            location.1 = min(n-1,location.1)
-        }
-        if location.1 == x {
-            yCount += 1
-        }
+    range = Range(startX: y, endX: y, startY: x, endY: x)
+    M = m-1
+    N = n-1
+    queries.reversed().forEach {
+        changeRange(query:$0, isEdge: isEdge(query: $0))
     }
-    
-    for i in 0..<m {
-        var location:(Int,Int) = (i,0)
-        for q in filterX {
-            let mv = move(query: q)
-            location = (location.0+mv.0,location.1+mv.1)
-            location.0 = max(0,location.0)
-            location.0 = min(m-1,location.0)
-        }
-        if location.0 == y {
-            xCount += 1
-        }
-    }
+    return isRightQuery(queries: queries, x: x, y: y) ? Int64(range.getArea) : 0
+}
 
-    return xCount*yCount
+func isRightQuery(queries:[[Int]],x:Int,y:Int) -> Bool {
+    var location:(Int,Int) = (range.endX,range.endY)
+    for q in queries {
+        let mv = move(query: q)
+        location = (location.0+mv.0,location.1+mv.1)
+        location.0 = max(0,location.0)
+        location.1 = max(0,location.1)
+        location.0 = min(M,location.0)
+        location.1 = min(N,location.1)
+    }
+    return location == (y,x)
 }
 
 func move(query:[Int]) -> (Int,Int) {
@@ -48,5 +47,27 @@ func move(query:[Int]) -> (Int,Int) {
     }
 }
 
+func isEdge(query:[Int]) -> Bool {
+    return (query[0] == 0 && range.startX == 0) || (query[0] == 1 && range.endX == M) || (query[0] == 2 && range.startY == 0) || (query[0] == 3 && range.endY == N)
+}
+
+func changeRange(query:[Int],isEdge:Bool){
+    switch query[0] {
+    case 0:
+        range.startX = isEdge ? range.startX : min(M,range.startX+query[1])
+        range.endX = min(M,range.endX+query[1])
+    case 1:
+        range.startX = max(0,range.startX-query[1])
+        range.endX = isEdge ? range.endX : max(0,range.endX-query[1])
+    case 2:
+        range.startY = isEdge ? range.startY : min(N,range.startY+query[1])
+        range.endY = min(N,range.endY+query[1])
+    default:
+        range.startY = max(0,range.startY-query[1])
+        range.endY = isEdge ? range.endY : max(0,range.endY-query[1])
+    }
+}
+
 solution(2, 2, 0, 0, [[2,1],[0,1],[1,1],[0,1],[2,1]])
 solution(2, 5, 0, 1, [[3,1],[2,2],[1,1],[2,3],[0,1],[2,1]])
+solution(1000, 1000, 1, 1, [[0,100001],[2,100001]])
