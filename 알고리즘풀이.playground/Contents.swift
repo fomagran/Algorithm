41f5c1299f5a2daa65558b782aaf628688187eb6
Fomagran <fomagran6@naver.com>
2021-11-15 12:45:13 +0900

    [refactoring] 프로그래머스 월간 코드 챌린지 3 공 이동 시뮬레이션 리팩터링

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,71 +1,66 @@
-import Foundation
-
 struct Range {
-    var startX:Int,endX:Int,startY:Int,endY:Int
+    var startX:Int,endX:Int,startY:Int,endY:Int,M:Int,N:Int
     var getArea:Int {
         return (endX-startX+1)*(endY-startY+1)
     }
 }
 
-var M:Int!
-var N:Int!
 var range:Range!
 
 func solution(_ n:Int, _ m:Int, _ x:Int, _ y:Int, _ queries:[[Int]]) -> Int64 {
-    range = Range(startX: y, endX: y, startY: x, endY: x)
-    M = m-1
-    N = n-1
-    queries.reversed().forEach {
-        changeRange(query:$0, isEdge: isEdge(query: $0))
+    range = Range(startX: y, endX: y, startY: x, endY: x,M: m-1,N:n-1)
+    for query in queries.reversed() {
+        if !isEdge(query: query) {
+            if !checkRightQuery(query: query) { return 0 }
+            moveStart(query: query)
+        }
+        extendEnd(query: query)
     }
-    return isRightQuery(queries: queries, x: x, y: y) ? Int64(range.getArea) : 0
-}
-
-func isRightQuery(queries:[[Int]],x:Int,y:Int) -> Bool {
-    var location:(Int,Int) = (range.endX,range.endY)
-    for q in queries {
-        let mv = move(query: q)
-        location = (location.0+mv.0,location.1+mv.1)
-        location.0 = max(0,location.0)
-        location.1 = max(0,location.1)
-        location.0 = min(M,location.0)
-        location.1 = min(N,location.1)
-    }
-    return location == (y,x)
-}
-
-func move(query:[Int]) -> (Int,Int) {
-    switch query[0] {
-    case 0:
-        return (-query[1],0)
-    case 1:
-        return (query[1],0)
-    case 2:
-        return (0,-query[1])
-    default:
-        return (0,query[1])
-    }
+    return Int64(range.getArea)
 }
 
 func isEdge(query:[Int]) -> Bool {
-    return (query[0] == 0 && range.startX == 0) || (query[0] == 1 && range.endX == M) || (query[0] == 2 && range.startY == 0) || (query[0] == 3 && range.endY == N)
+    return (query[0] == 0 && range.startX == 0) || (query[0] == 1 && range.endX == range.M) || (query[0] == 2 && range.startY == 0) || (query[0] == 3 && range.endY == range.N)
 }
 
-func changeRange(query:[Int],isEdge:Bool){
+func extendEnd(query:[Int]) {
     switch query[0] {
     case 0:
-        range.startX = isEdge ? range.startX : min(M,range.startX+query[1])
-        range.endX = min(M,range.endX+query[1])
+        range.endX = min(range.M,range.endX+query[1])
     case 1:
         range.startX = max(0,range.startX-query[1])
-        range.endX = isEdge ? range.endX : max(0,range.endX-query[1])
     case 2:
-        range.startY = isEdge ? range.startY : min(N,range.startY+query[1])
-        range.endY = min(N,range.endY+query[1])
+        range.endY = min(range.N,range.endY+query[1])
     default:
         range.startY = max(0,range.startY-query[1])
-        range.endY = isEdge ? range.endY : max(0,range.endY-query[1])
     }
+}
+
+func moveStart(query:[Int]){
+    switch query[0] {
+    case 0:
+        range.startX = min(range.M,range.startX+query[1])
+    case 1:
+        range.endX =  max(0,range.endX-query[1])
+    case 2:
+        range.startY = min(range.N,range.startY+query[1])
+    default:
+        range.endY = max(0,range.endY-query[1])
+    }
+}
+
+func checkRightQuery(query:[Int]) -> Bool {
+    switch query[0] {
+    case 0:
+        if range.startX+query[1] > range.M { return false }
+    case 1:
+        if range.endX-query[1] < 0 { return false }
+    case 2:
+        if range.startY+query[1] > range.N { return false }
+    default:
+        if range.endY-query[1] < 0 { return false }
+    }
+    return true
 }
 
 solution(2, 2, 0, 0, [[2,1],[0,1],[1,1],[0,1],[2,1]])
