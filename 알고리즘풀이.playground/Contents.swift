9fbe885bf080f1e786cd17ac2b3c3277652efdad
Fomagran <fomagran6@naver.com>
2021-12-10 20:59:07 +0900

    [add] 프로그래머스 위클리 챌린지 피로도 풀이 시작

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,32 +1,10 @@
-func solution(_ n:Int, _ left:Int64, _ right:Int64) -> [Int] {
-    let startColumn:Int = Int(left)/n
-    let endColumn:Int = Int(right)/n
-    let startRow:Int = Int(left)%n
-    let endRow:Int = Int(right)%n
-    var answer:[Int] = []
+import Foundation
 
-    for i in startColumn...endColumn {
-        var numbers:[Int] = Array(repeating: i+1, count:i+1)
-        if i+1 < n {
-            let tail:[Int] = Array(i+2...n)
-            numbers.append(contentsOf:tail)
-        }
-
-        if startColumn == endColumn {
-            return Array(numbers[startRow...endRow])
-        }
-
-        if i == startColumn {
-            answer.append(contentsOf: Array(numbers[startRow...]))
-            continue
-        }
-
-        if i == endColumn {
-            answer.append(contentsOf: Array(numbers[0...endRow]))
-            continue
-        }
-        answer.append(contentsOf: numbers)
+func solution(_ k:Int, _ dungeons:[[Int]]) -> Int {
+    var fatigue:Int = k
+    
+    for dungeon in dungeons {
+        <#body#>
     }
-
-    return answer
+    return -1
 }
