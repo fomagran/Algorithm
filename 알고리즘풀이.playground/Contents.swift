78e458a4cf21168c0ae9406c882e92584f5d6042
Fomagran <fomagran6@naver.com>
2021-12-14 20:52:58 +0900

    [add] 2019 KAKAO BLIND RECRUITMENT 무지의 먹방 라이브 2차 92.9 효율성 2번이 시간초과 나는데 어디서 나는지 모르겠다...

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,64 +1,36 @@
-import Foundation
-
-/*
- k를 음식 수 만큼으로 나눈다.
- 나눈 숫자보다 작은 음식만큼 1부터 N까지 뺀다.
- 나머지에 뺀 숫자들을 다 더하고 남은 음식들로 나눈다.
- 
- 1 2
- 
- 1 2 3
- 
- 1
- 
- 1 2
- 
- 1
- */
-
 func solution(_ food_times:[Int], _ k:Int64) -> Int {
-    
     var foodDic:[Int:Int] = [:]
-    
+    var K = Int(k)
+    var numberCount:[Int:Int] = [:]
     for (i,food) in food_times.enumerated() {
         foodDic[i+1] = food
-    }
-    
-    var share = (k+1)/Int64(food_times.count)
-    var remain = (k+1)%Int64(food_times.count)
-    var foodCount = foodDic.count
-    foodDic = foodDic.filter{$0.value > share}
-    foodCount -= foodDic.count
-    for (key,value) in foodDic {
-        foodDic[key] = value-Int(share)
-    }
-    if foodCount >= 1 {
-        for i in 1...foodCount {
-            remain += Int64(i)
+        if numberCount[food] == nil {
+            numberCount[food] = 1
+        }else {
+            numberCount[food]! += 1
         }
     }
     
-    share = remain/Int64(foodDic.count)
-    remain = remain%Int64(foodDic.count)
+    let sort = foodDic.sorted{$0.value < $1.value}
+    var last:Int = 0
+    var foodCount:Int = food_times.count
+    var index:Int = 0
     
-    while remain > foodDic.count {
-        foodDic = foodDic.filter{$0.value > share}
-        foodCount -= foodDic.count
-        for (key,value) in foodDic {
-            foodDic[key] = value-Int(share)
+    while index < sort.count {
+        let first = sort[index].value
+        K -= foodCount * (first-last)
+        if K < 0 {
+            K += foodCount * (first-last)
+            let filter = sort[index...].sorted{$0.key < $1.key}
+            return filter[K%foodCount].key
         }
-        if foodCount >= 1 {
-            for i in 1...foodCount {
-                remain += Int64(i)
-            }
-        }
-        share = remain/Int64(foodDic.count)
-        remain = remain%Int64(foodDic.count)
+        foodCount -= numberCount[first]!
+        last = first
+        index += numberCount[first]!
     }
-    
-    let sortFood = foodDic.sorted{$0.key < $1.key}
-    
-    return sortFood[Int(remain)].key
+    return -1
 }
 
-solution([3,1,2], 5)
+solution([3, 1, 2], 5)
+solution([4,2,3,6,7,1,5,8], 27)
+solution([4,2,3,6,7,1,5,8], 16)
