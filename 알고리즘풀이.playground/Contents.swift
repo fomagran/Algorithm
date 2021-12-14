2612f60bfd765aa7ba43a6a0932646cbc79adac0
Fomagran <fomagran6@naver.com>
2021-12-14 21:26:59 +0900

    [add] 2019 KAKAO BLIND RECRUITMENT 풀이 완료..

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,36 +1,39 @@
 func solution(_ food_times:[Int], _ k:Int64) -> Int {
-    var foodDic:[Int:Int] = [:]
-    var K = Int(k)
-    var numberCount:[Int:Int] = [:]
-    for (i,food) in food_times.enumerated() {
-        foodDic[i+1] = food
-        if numberCount[food] == nil {
-            numberCount[food] = 1
-        }else {
-            numberCount[food]! += 1
-        }
+    guard food_times.reduce(0, +) > k else {
+        return -1
     }
     
-    let sort = foodDic.sorted{$0.value < $1.value}
+    let food = food_times.enumerated().map{($0.offset,$0.element)}
+    var K = Int(k)
+    
+    let sort = food.sorted{$0.1 < $1.1}
     var last:Int = 0
-    var foodCount:Int = food_times.count
     var index:Int = 0
+    var foodCount = food_times.count
     
     while index < sort.count {
-        let first = sort[index].value
-        K -= foodCount * (first-last)
-        if K < 0 {
-            K += foodCount * (first-last)
-            let filter = sort[index...].sorted{$0.key < $1.key}
-            return filter[K%foodCount].key
+        let first = sort[index].1
+        var max = foodCount * (first-last)
+        var numberCount = 0
+        for i in index..<sort.count {
+            if sort[i].1 == first {
+                numberCount += 1
+            }else {
+                break
+            }
         }
-        foodCount -= numberCount[first]!
+        if K-max < 0 { break}
+        K -= max
+        foodCount -= numberCount
         last = first
-        index += numberCount[first]!
+        index += numberCount
     }
-    return -1
+    
+    let filter = sort[index...].sorted{$0.0 < $1.0}
+    return filter[K%foodCount].0 + 1
 }
 
+solution( [1,1,1,1], 4)
 solution([3, 1, 2], 5)
 solution([4,2,3,6,7,1,5,8], 27)
 solution([4,2,3,6,7,1,5,8], 16)
