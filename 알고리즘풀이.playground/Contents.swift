6ba0a36bfd1c127fffea3e9931d5c6d5eca75c4a
Fomagran <fomagran6@naver.com>
2021-12-15 15:51:34 +0900

    [refactoring] 2019 KAKAO BLIND RECRUITMENT 무지의 먹방 라이브 리팩터링

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,39 +1,19 @@
 func solution(_ food_times:[Int], _ k:Int64) -> Int {
-    guard food_times.reduce(0, +) > k else {
-        return -1
-    }
-    
-    let food = food_times.enumerated().map{($0.offset,$0.element)}
+    if food_times.reduce(0,+) <= k { return -1 }
     var K = Int(k)
-    
-    let sort = food.sorted{$0.1 < $1.1}
+    var foods = food_times.enumerated().sorted{$0.element > $1.element}
     var last:Int = 0
-    var index:Int = 0
-    var foodCount = food_times.count
-    
-    while index < sort.count {
-        let first = sort[index].1
-        var max = foodCount * (first-last)
-        var numberCount = 0
-        for i in index..<sort.count {
-            if sort[i].1 == first {
-                numberCount += 1
-            }else {
-                break
-            }
-        }
-        if K-max < 0 { break}
-        K -= max
-        foodCount -= numberCount
-        last = first
-        index += numberCount
+    while K - foods.count * (foods.last!.element - last) >= 0{
+        let min = foods.removeLast().element
+        K -= (foods.count+1) * (min-last)
+        last = min
     }
-    
-    let filter = sort[index...].sorted{$0.0 < $1.0}
-    return filter[K%foodCount].0 + 1
+    return foods.sorted{$0.offset < $1.offset}[K%foods.count].offset + 1
 }
 
-solution( [1,1,1,1], 4)
+
+
+//solution( [1,1,1,1], 4)
 solution([3, 1, 2], 5)
 solution([4,2,3,6,7,1,5,8], 27)
 solution([4,2,3,6,7,1,5,8], 16)
