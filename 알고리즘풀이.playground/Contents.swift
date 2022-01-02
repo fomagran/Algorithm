ad6a0967aa0f4cb50733004b2209f9edc37197fe
Fomagran <fomagran6@naver.com>
2022-01-02 11:38:24 +0900

    [AlgoExpert] Validate Subsequence 풀이완료!

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,19 +1,24 @@
-func solution(_ food_times:[Int], _ k:Int64) -> Int {
-    if food_times.reduce(0,+) <= k { return -1 }
-    var K = Int(k)
-    var foods = food_times.enumerated().sorted{$0.element > $1.element}
-    var last:Int = 0
-    while K - foods.count * (foods.last!.element - last) >= 0{
-        let min = foods.removeLast().element
-        K -= (foods.count+1) * (min-last)
-        last = min
+import Foundation
+
+func isValidSubsequence(_ array: [Int], _ sequence: [Int]) -> Bool {
+    var first = sequence.first
+    var sCopy = sequence
+    
+    for a in array {
+        if a == first {
+            if !sCopy.isEmpty {
+                sCopy.removeFirst()
+                if !sCopy.isEmpty {
+                    first = sCopy.first
+                }else {
+                    return true
+                }
+            }else {
+                return true
+            }
+        }
     }
-    return foods.sorted{$0.offset < $1.offset}[K%foods.count].offset + 1
+    return false
 }
 
-
-
-//solution( [1,1,1,1], 4)
-solution([3, 1, 2], 5)
-solution([4,2,3,6,7,1,5,8], 27)
-solution([4,2,3,6,7,1,5,8], 16)
+isValidSubsequence([5, 1, 22, 25, 6, -1, 8, 10],[1, 6, -1, 5])
