0ae214908bb7526b67dcc3418624ea8a45492a42
Fomagran <fomagran6@naver.com>
2022-01-03 17:36:40 +0900

    [징검다리] 풀이 완료!

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,16 +1,46 @@
-/*
- 바위 사이의 거리를 오름차순 정렬
- 바위 사이 거리를 적은 순으로 지움.
- 지운 바위 사위 거리를 업데이트 해주고 이진 탐색으로 해당 위치에 넣음
- //0 2 11 14 17 21 25
- // 2 9 3 4 4
- // 2 3 4 4 9
- // 3 4 4 9 11
- // 4 4 7 9 11
- */
-
-import Foundation
+struct Bridge {
+    var start:Int,mid:Int,end:Int,min:Int
+}
 
 func solution(_ distance:Int, _ rocks:[Int], _ n:Int) -> Int {
-    return 0
+    return getMinCount(rocks: rocks, n: n, distance: distance)
 }
+
+func getMinCount(rocks:[Int],n:Int,distance:Int) -> Int {
+    let sortedRock:[Int] = rocks.sorted()
+    var bridge = Bridge(start: 1, mid: 1+distance/2, end: distance,min: 0)
+    while bridge.start <= bridge.end {
+        bridge.mid = (bridge.start+bridge.end)/2
+        print(bridge.mid)
+        let count = getDeletedRockCount(sortedRock, bridge: bridge)
+        print(count)
+        updateBridgeLength(bridge: &bridge, count: count, n: n)
+    }
+    return bridge.min
+}
+
+func getDeletedRockCount(_ rocks:[Int],bridge:Bridge) -> Int {
+    var prev = 0
+    var count = 0
+    for rock in rocks {
+        if rock-prev < bridge.mid {
+            count += 1
+            continue
+        }
+        prev = rock
+    }
+    return count
+}
+
+func updateBridgeLength(bridge:inout Bridge,count:Int,n:Int) {
+    if count > n {
+        bridge.end = bridge.mid - 1
+    }else {
+        bridge.min = bridge.mid
+        bridge.start = bridge.mid + 1
+    }
+}
+
+solution(25, [2,14,11,21,17], 2)
+
+
