d241ee55fbeada168c45a51d0bed4ee35da1b1ac
Fomagran <fomagran6@naver.com>
2022-01-03 09:22:02 +0900

    [징검다리] 문제 풀기 전 설계

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,24 +1,16 @@
+/*
+ 바위 사이의 거리를 오름차순 정렬
+ 바위 사이 거리를 적은 순으로 지움.
+ 지운 바위 사위 거리를 업데이트 해주고 이진 탐색으로 해당 위치에 넣음
+ //0 2 11 14 17 21 25
+ // 2 9 3 4 4
+ // 2 3 4 4 9
+ // 3 4 4 9 11
+ // 4 4 7 9 11
+ */
+
 import Foundation
 
-func isValidSubsequence(_ array: [Int], _ sequence: [Int]) -> Bool {
-    var first = sequence.first
-    var sCopy = sequence
-    
-    for a in array {
-        if a == first {
-            if !sCopy.isEmpty {
-                sCopy.removeFirst()
-                if !sCopy.isEmpty {
-                    first = sCopy.first
-                }else {
-                    return true
-                }
-            }else {
-                return true
-            }
-        }
-    }
-    return false
+func solution(_ distance:Int, _ rocks:[Int], _ n:Int) -> Int {
+    return 0
 }
-
-isValidSubsequence([5, 1, 22, 25, 6, -1, 8, 10],[1, 6, -1, 5])
