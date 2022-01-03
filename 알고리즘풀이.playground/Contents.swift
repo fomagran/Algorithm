58b7ced307ed7d37f7310053700424f00c143b52
Fomagran <fomagran6@naver.com>
2022-01-03 19:23:05 +0900

    [가사 검색] 리팩터링!

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -7,18 +7,13 @@ class Node {
     init(value:String) {
         self.value = value
     }
-    
     func append(_ value:String) {
         self.children[value] = Node(value: value)
     }
 }
 
 class Trie {
-    let root:Node
-    
-    init() {
-        self.root = Node(value:"")
-    }
+    let root:Node = Node(value:"")
     
     func insert(_ word:String) {
         var current = root
@@ -35,55 +30,55 @@ class Trie {
     func getCount(_ word:String) -> Int {
         var current = root
         let map = word.map{String($0)}
-        var check = false
         for w in map {
-            if current.children.keys.contains(w) {
-                current = current.children[w]!
-            }else {
-                check = true
+            if !current.children.keys.contains(w) {
+                return 0
             }
+            current = current.children[w]!
         }
-        return check ? 0 : current.count == 0 ? 1 : current.count
+        return current.count == 0 ? 1 : current.count
     }
 }
 
+struct Tries {
+    var trie:Trie,reverse:Trie
+}
+
 func solution(_ words:[String], _ queries:[String]) -> [Int] {
-    var lengthDic:[Int:[Trie]] = [:]
-    var results:[Int] = []
-    
+    let lengthDic:[Int:Tries] = classifyByWordLength(words)
+    return queriesResults(queries,lengthDic)
+}
+
+func classifyByWordLength(_ words:[String]) -> [Int:Tries] {
+    var lengthDic:[Int:Tries] = [:]
     for word in words {
         if lengthDic[word.count] == nil {
-            let trie = Trie()
-            let reverse = Trie()
-            trie.insert(word)
-            let reveseWord = String(word.reversed())
-            reverse.insert(reveseWord)
-            lengthDic[word.count] = [trie,reverse]
+            let tries = Tries(trie: Trie(), reverse: Trie())
+            tries.trie.insert(word)
+            tries.reverse.insert(String(word.reversed()))
+            lengthDic[word.count] = tries
         }else {
-            lengthDic[word.count]![0].insert(word)
-            let reveseWord = String(word.reversed())
-            lengthDic[word.count]![1].insert(reveseWord)
+            lengthDic[word.count]!.trie.insert(word)
+            lengthDic[word.count]!.reverse.insert(String(word.reversed()))
         }
     }
-    
+    return lengthDic
+}
+
+func queriesResults(_ queries:[String],_ lengthDic:[Int:Tries]) -> [Int] {
+    var results:[Int] = []
     for query in queries {
         let count = query.count
-        let pre = query.map{String($0)}.filter{$0 != "?"}.joined()
-        let isReverse = query.first == "?"
-        var result = 0
         if lengthDic[count] == nil {
             results.append(0)
             continue
         }
-        if isReverse {
-            result = lengthDic[count]![1].getCount(String(pre.reversed()))
-        }else {
-            result = lengthDic[count]![0].getCount(pre)
-        }
-
+        var pre = query.map{String($0)}.filter{$0 != "?"}.joined()
+        pre = query.first == "?" ? String(pre.reversed()) : pre
+        let trie = query.first == "?" ? lengthDic[count]!.reverse : lengthDic[count]!.trie
+        let result = trie.getCount(pre)
         results.append(result)
     }
-
     return results
 }
 
