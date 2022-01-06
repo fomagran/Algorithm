437663ce097046fb64922be5d48ba5e532226c65
Fomagran <fomagran6@naver.com>
2022-01-06 21:36:26 +0900

    [Baekjoon] 팰린드롬 만들기 풀이완료!

--- BackJoon/main.swift
+++ BackJoon/main.swift
@@ -5,16 +5,34 @@
 //  Created by Fomagran on 2022/01/06.
 //
 
-var count:Int = 0
-var max = (0,0)
+/*
+ 주어진 문자를 똑같이 붙여넣는다.
+ 붙여넣은 지점부터 하나씩 삭제해가며 중앙과 같은지 비교한다.
+ */
 
-while count < 9 {
-    let n = Int(readLine() ?? "") ?? 0
-    count += 1
-    if max.1 < n {
-        max = (count,n)
-    }
+import Foundation
+
+var str = readLine()!
+var newStr = (str + str.reversed()).map{String($0)}
+var start = str.count
+var min = Int.max
+
+func isPalindrome(_ str:[String]) -> Bool {
+    let middle = str.count%2 == 0 ? str.count/2-1 : str.count/2
+    let head = str[0...middle].joined()
+    let tail = str[str.count/2..<str.count].reversed().joined()
+    return head == tail
 }
 
-print(max.1)
-print(max.0)
+while newStr.count >= str.count {
+    if isPalindrome(newStr) {
+        min = newStr.count
+    }
+    if newStr.count == str.count {
+        break
+    }
+    newStr.remove(at: start)
+}
+
+print(min)
+
