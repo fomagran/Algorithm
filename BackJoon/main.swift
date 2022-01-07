677d2c0d6aa63d9a5db7ae1fbfc70517d037bcb9
Fomagran <fomagran6@naver.com>
2022-01-07 13:15:18 +0900

    [Baekjoon] A와 B 2 1차 시도

--- BackJoon/main.swift
+++ BackJoon/main.swift
@@ -10,29 +10,21 @@
  붙여넣은 지점부터 하나씩 삭제해가며 중앙과 같은지 비교한다.
  */
 
-import Foundation
+var S:String = readLine()!
+var T:String = readLine()!
+var answer = 0
 
-var str = readLine()!
-var newStr = (str + str.reversed()).map{String($0)}
-var start = str.count
-var min = Int.max
-
-func isPalindrome(_ str:[String]) -> Bool {
-    let middle = str.count%2 == 0 ? str.count/2-1 : str.count/2
-    let head = str[0...middle].joined()
-    let tail = str[str.count/2..<str.count].reversed().joined()
-    return head == tail
-}
-
-while newStr.count >= str.count {
-    if isPalindrome(newStr) {
-        min = newStr.count
-    }
-    if newStr.count == str.count {
+while T.count >= S.count {
+    if S == T {
+        answer = 1
         break
     }
-    newStr.remove(at: start)
+    if T.first == "B" {
+        T = String(T.reversed())
+        T.removeLast()
+        continue
+    }
+    T.removeFirst()
 }
 
-print(min)
-
+print(answer)
