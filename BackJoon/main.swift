cfd377120b81973944b666d93b21bfe75797936f
Fomagran <fomagran6@naver.com>
2022-01-07 13:41:16 +0900

    [Baekjoon] A와 B 2 풀이완료!

--- BackJoon/main.swift
+++ BackJoon/main.swift
@@ -14,17 +14,40 @@ var S:String = readLine()!
 var T:String = readLine()!
 var answer = 0
 
-while T.count >= S.count {
-    if S == T {
+check(T)
+
+
+func check(_ t:String) {
+    if t == S {
         answer = 1
-        break
+        return
     }
-    if T.first == "B" {
-        T = String(T.reversed())
-        T.removeLast()
-        continue
+    if t.count <= S.count || answer == 1 {
+        return
     }
-    T.removeFirst()
+    if t.first == "A" && t.last == "A" {
+       check(removeA(t))
+    }else if t.first == "A" && t.last == "B" {
+        return
+    }else if t.first == "B" && t.last == "A" {
+        check(removeA(t))
+        check(removeB(t))
+    }else {
+        check(removeB(t))
+    }
+}
+
+func removeB(_ s:String) -> String {
+    var new = s
+    new = String(new.reversed())
+    new.removeLast()
+    return new
+}
+
+func removeA(_ s:String) -> String {
+    var new = s
+    new.removeLast()
+    return new
 }
 
 print(answer)
