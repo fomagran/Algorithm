433bd45708d8713ead78eafcd2d238512458d22a
Fomagran <fomagran6@naver.com>
2022-01-19 15:50:36 +0900

    [프로그래머스] 주차 요금 계산 풀이 완료!!

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,23 +1,65 @@
 import Foundation
 
-func solution(_ n:Int, _ k:Int) -> Int {
-    let trit = String(n,radix: k)
-    let numbers = trit.split(separator: "0")
-    return numbers.filter{isPrimeNumber(Int($0)!)}.count
-}
-
-func isPrimeNumber(_ n:Int) -> Bool {
-    if n == 1  { return false }
-    if n == 2 || n == 3 {return true}
-    let sq = Int(sqrt(Double(n)))
-    for i in 2...sq {
-        if n%i == 0 {
-            return false
+func solution(_ fees:[Int], _ records:[String]) -> [Int] {
+    var parkInfo:[String:Int] = [:]
+    var timeInfo:[String:Int] = [:]
+    var feeInfo:[String:Int] = [:]
+    let maxTime = 23*60+59
+    
+    calTimeByRecord(records,&parkInfo, &timeInfo)
+    
+    for (number,time) in parkInfo {
+        if timeInfo[number] == nil {
+            timeInfo[number] = maxTime - time
+        }else {
+            timeInfo[number]! += maxTime - time
         }
     }
-    return true
+    
+    for (number,time) in timeInfo {
+        feeInfo[number] = getTotalFee(time,fees)
+    }
+    
+    return feeInfo.sorted{$0.key < $1.key}.map{$0.value}
 }
 
-solution(437674, 3)
-solution(110011, 10)
+func calTimeByRecord(_ records:[String],_ parkInfo:inout [String:Int],_ timeInfo:inout [String:Int]) {
+    for record in records {
+        let split = record.split(separator: " ")
+        let time = String(split[0])
+        let number = String(split[1])
+        let isIN = split[2] == "IN"
+        if isIN {
+            parkInfo[number] = changeTimeToMinutes(time)
+        }else {
+            let out = changeTimeToMinutes(time)
+            let minutes = out - parkInfo[number]!
+            if timeInfo[number] == nil {
+                timeInfo[number] = minutes
+            }else {
+                timeInfo[number]! += minutes
+            }
+            parkInfo[number] = nil
+        }
+    }
+}
 
+func getTotalFee(_ minutes:Int,_ fees:[Int]) -> Int {
+    let freetime = fees[0]
+    let defaultFee = fees[1]
+    let perTime = fees[2]
+    let fee = fees[3]
+    if minutes <= freetime { return defaultFee }
+    let c = Int(ceil(Double((minutes - freetime))/Double(perTime)))
+    let total = defaultFee + c * fee
+    return total
+}
+
+func changeTimeToMinutes(_ time:String) -> Int {
+    let split = time.split(separator: ":")
+    let hours = Int(split[0])!
+    let minutes = Int(split[1])!
+    return hours*60 + minutes
+}
+
+solution([180, 5000, 10, 600], ["05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"])
