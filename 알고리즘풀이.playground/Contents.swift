3f1340dfa7e583ff1de1d94f6ea19578893bfcf7
Fomagran <fomagran6@naver.com>
2022-01-19 12:59:14 +0900

    [프로그래머스] 2022 KAKAO BLIND RECRUITMENT 신고 결과 받기 풀이완료!

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,85 +1,34 @@
 import Foundation
 
-class Node {
-    var value:String
-    var count:Int = 0
-    var children:[String:Node] = [:]
-    init(value:String) {
-        self.value = value
-    }
-    func append(_ value:String) {
-        self.children[value] = Node(value: value)
-    }
-}
-
-class Trie {
-    let root:Node = Node(value:"")
+func solution(_ id_list:[String], _ report:[String], _ k:Int) -> [Int] {
+    var reportInfo:[String:Set<String>] = [:]
+    var reportCount:[Int] = Array(repeating: 0, count: id_list.count)
+    var idNumber:[String:Int] = [:]
     
-    func insert(_ word:String) {
-        var current = root
-        let map = word.map{String($0)}
-        map.forEach {
-            current.count += 1
-            if !current.children.keys.contains($0) {
-                current.append($0)
-            }
-            current = current.children[$0]!
-        }
+    for (i,id) in id_list.enumerated() {
+        idNumber[id] = i
     }
     
-    func getCount(_ word:String) -> Int {
-        var current = root
-        let map = word.map{String($0)}
-        for w in map {
-            if !current.children.keys.contains(w) {
-                return 0
-            }
-            current = current.children[w]!
-        }
-        return current.count == 0 ? 1 : current.count
-    }
-}
-
-struct Tries {
-    var trie:Trie,reverse:Trie
-}
-
-func solution(_ words:[String], _ queries:[String]) -> [Int] {
-    let lengthDic:[Int:Tries] = classifyByWordLength(words)
-    return queriesResults(queries,lengthDic)
-}
-
-func classifyByWordLength(_ words:[String]) -> [Int:Tries] {
-    var lengthDic:[Int:Tries] = [:]
-    for word in words {
-        if lengthDic[word.count] == nil {
-            let tries = Tries(trie: Trie(), reverse: Trie())
-            tries.trie.insert(word)
-            tries.reverse.insert(String(word.reversed()))
-            lengthDic[word.count] = tries
+    for r in report {
+        let split = r.split(separator: " ")
+        let report = String(split[0])
+        let reported = String(split[1])
+        if reportInfo[reported] == nil {
+            reportInfo[reported] = [report]
         }else {
-            lengthDic[word.count]!.trie.insert(word)
-            lengthDic[word.count]!.reverse.insert(String(word.reversed()))
+            reportInfo[reported]!.insert(report)
         }
     }
-    return lengthDic
-}
-
-func queriesResults(_ queries:[String],_ lengthDic:[Int:Tries]) -> [Int] {
-    var results:[Int] = []
-    for query in queries {
-        let count = query.count
-        if lengthDic[count] == nil {
-            results.append(0)
-            continue
+    
+    for info in reportInfo {
+        if info.value.count >= k {
+            for id in info.value {
+                reportCount[idNumber[id]!] += 1
+            }
         }
-        var pre = query.map{String($0)}.filter{$0 != "?"}.joined()
-        pre = query.first == "?" ? String(pre.reversed()) : pre
-        let trie = query.first == "?" ? lengthDic[count]!.reverse : lengthDic[count]!.trie
-        let result = trie.getCount(pre)
-        results.append(result)
     }
-    return results
+    return reportCount
 }
 
-solution(["frodo", "front", "frost", "frozen", "frame", "kakao"],["fro??", "????o", "fr???", "fro???", "pro?","frozee","f????","?????"])
+solution(["muzi", "frodo", "apeach", "neo"], ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"], 2)
+solution(["con", "ryan"], ["ryan con", "ryan con", "ryan con", "ryan con"], 3)
