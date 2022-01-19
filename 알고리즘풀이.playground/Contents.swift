3d2394ac79ca471c4cfe1bd521920246dc6acf93
Fomagran <fomagran6@naver.com>
2022-01-19 18:42:19 +0900

    [프로그래머스] 2022 KAKAO BLIND RECRUITMENT 양궁대회 풀이완료!

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,65 +1,70 @@
 import Foundation
 
-func solution(_ fees:[Int], _ records:[String]) -> [Int] {
-    var parkInfo:[String:Int] = [:]
-    var timeInfo:[String:Int] = [:]
-    var feeInfo:[String:Int] = [:]
-    let maxTime = 23*60+59
-    
-    calTimeByRecord(records,&parkInfo, &timeInfo)
-    
-    for (number,time) in parkInfo {
-        if timeInfo[number] == nil {
-            timeInfo[number] = maxTime - time
-        }else {
-            timeInfo[number]! += maxTime - time
+var maxScore:Int = -1
+var maxHistory:[[Int]] = []
+
+func solution(_ n:Int, _ info:[Int]) -> [Int] {
+    let history = Array(repeating: 0, count: 11)
+    dfs(leftArrow: n, depth: 10, history: history, info: info)
+    if maxScore <= 0 {
+        return [-1]
+    }
+    return sortByMoreLowerScore()
+}
+
+func sortByMoreLowerScore() -> [Int]{
+    var answer:[Int] = maxHistory.first!
+    var max = 0
+    for history in maxHistory {
+        let numCount = history.filter{$0 != 0 }.count
+        if max < numCount {
+            answer = history
+            max = numCount
         }
     }
-    
-    for (number,time) in timeInfo {
-        feeInfo[number] = getTotalFee(time,fees)
-    }
-    
-    return feeInfo.sorted{$0.key < $1.key}.map{$0.value}
+    return answer.reversed()
 }
 
-func calTimeByRecord(_ records:[String],_ parkInfo:inout [String:Int],_ timeInfo:inout [String:Int]) {
-    for record in records {
-        let split = record.split(separator: " ")
-        let time = String(split[0])
-        let number = String(split[1])
-        let isIN = split[2] == "IN"
-        if isIN {
-            parkInfo[number] = changeTimeToMinutes(time)
-        }else {
-            let out = changeTimeToMinutes(time)
-            let minutes = out - parkInfo[number]!
-            if timeInfo[number] == nil {
-                timeInfo[number] = minutes
-            }else {
-                timeInfo[number]! += minutes
-            }
-            parkInfo[number] = nil
+func dfs(leftArrow:Int,depth:Int,history:[Int],info:[Int]) {
+    var newHistory = history
+    var newLeftArrow = leftArrow
+    
+    if depth == 0 || leftArrow <= 0 {
+        newHistory[0] = depth == 0 ? leftArrow : newHistory[0]
+        let score = getScoreGap(history,info)
+        if maxScore == score {
+            maxHistory.append(newHistory)
+        }else if maxScore < score {
+            maxScore = score
+            maxHistory = [newHistory]
         }
+        return
     }
+    
+    if leftArrow > info[10-depth] {
+        newLeftArrow -= info[10-depth]+1
+        newHistory[depth] = info[10-depth]+1
+        dfs(leftArrow: newLeftArrow, depth: depth-1, history: newHistory, info: info)
+        newHistory[depth] = 0
+    }
+    
+    dfs(leftArrow: leftArrow, depth: depth-1, history: newHistory, info: info)
 }
 
-func getTotalFee(_ minutes:Int,_ fees:[Int]) -> Int {
-    let freetime = fees[0]
-    let defaultFee = fees[1]
-    let perTime = fees[2]
-    let fee = fees[3]
-    if minutes <= freetime { return defaultFee }
-    let c = Int(ceil(Double((minutes - freetime))/Double(perTime)))
-    let total = defaultFee + c * fee
-    return total
+func getScoreGap(_ history:[Int],_ info:[Int]) -> Int {
+    var ryan = 0
+    var apeach = 0
+    for (i,s) in history.enumerated() {
+           if s == 0 {
+               if info[10-i] != 0 {
+                   apeach += i
+               }
+           }else {
+               ryan += i
+           }
+       }
+    return ryan - apeach
 }
 
-func changeTimeToMinutes(_ time:String) -> Int {
-    let split = time.split(separator: ":")
-    let hours = Int(split[0])!
-    let minutes = Int(split[1])!
-    return hours*60 + minutes
-}
+solution(10,[0,0,0,0,0,0,0,0,3,4,3])
 
-solution([180, 5000, 10, 600], ["05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"])
