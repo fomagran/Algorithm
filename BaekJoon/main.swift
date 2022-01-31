5dacfb5dca742e7ec07af0124c6e50424c851ea2
Fomagran <fomagran6@naver.com>
2022-01-30 21:57:11 -0600

    [HackerRank] A Very Big Sum solution

--- BaekJoon/main.swift
+++ BaekJoon/main.swift
@@ -1,38 +1,11 @@
-var word = String(readLine()!)
-let vowels = ["a","e","i","o","u"]
+import Foundation
 
-while word != "end" {
-    let map = word.map{String($0)}
-    var consonantCount = 0
-    var vowelCount = 0
-    var consLetters:[String] = []
-    var isAcceptable = false
-    
-    for s in map {
-        if vowels.contains(s) {
-            consonantCount = 0
-            vowelCount += 1
-            isAcceptable = true
-        }else {
-            vowelCount = 0
-            consonantCount += 1
-        }
-        if consonantCount == 3 || vowelCount == 3 {
-            isAcceptable = false
-            break
-        }
-        if !consLetters.isEmpty && consLetters.last! == s && s != "e" && s != "o" {
-            isAcceptable = false
-            break
-        }
-        consLetters.append(s)
-    }
-    if isAcceptable {
-        print("<\(word)> is acceptable.")
-    }else {
-        print("<\(word)> is not acceptable.")
-    }
-    word = String(readLine()!)
+let count = Int(readLine()!)!
+let nums = String(readLine()!).split(separator: " ").map{Int($0)!}
+
+func aVeryBigSum(ar: [Int]) -> Int {
+    return ar.reduce(0){$0+$1}
 }
 
+print(aVeryBigSum(ar: nums))
 
