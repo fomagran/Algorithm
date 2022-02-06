3211d3f939856edb3f4e9582ff908f677cea52e0
Fomagran <fomagran6@naver.com>
2022-02-05 20:57:52 -0600

    [HackerRank] Staircase solution

--- BaekJoon/main.swift
+++ BaekJoon/main.swift
@@ -1,25 +1,14 @@
 import Foundation
 
-let count = Double(readLine()!)!
-let nums = String(readLine()!).split(separator: " ").map{Int($0)!}
-
-var negativeCount:Double = 0
-var positiveCount:Double = 0
-
-for n in nums {
-    if n < 0 {
-        negativeCount += 1
-    }else if n > 0{
-        positiveCount += 1
+let n:Int = Int(readLine()!)!
+var start = Array(repeating:" ", count: n)
+func staircase(n: Int) {
+    for i in 1...n {
+        start[n-i] = "#"
+        print(start.joined())
     }
 }
-
-let pos = positiveCount/count
-print(String(format: "%.6f", pos))
-let neg = negativeCount/count
-print(String(format: "%.6f", neg))
-let zero = (count - (negativeCount+positiveCount))/count
-print(String(format: "%.6f", zero))
+staircase(n: n)
 
 
 
