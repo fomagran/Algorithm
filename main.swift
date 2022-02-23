ece7850cd875681a4a447f353f71577b334ed02a
Fomagran <fomagran6@naver.com>
2022-02-22 20:21:46 -0600

    [LeetCode] Rotate Array Solution

--- main.swift
+++ main.swift
@@ -1,23 +1,16 @@
 import Foundation
 
-//배열을 sorting
-//가장 큰 숫자부터 차례대로 가장 작은 숫자부터 더해서 target을 넘억가면 out
+//nums의 길이에서 k+1을 뺀 숫자를 축으로 두 개로 나눈다.
+//뒤에 있는 배열을 앞에 붙인다.
 
-func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-    let map = nums.enumerated().map{$0}
-    let sorted = map.sorted{$0.element <  $1.element}
-    let reverse = sorted.reversed()
-    for large in reverse {
-        for small in sorted {
-            if large.element + small.element == target {
-                return [small.offset,large.offset]
-            }
-            if large.element + small.element > target {
-                break
-            }
-        }
-    }
-    return []
+func rotate(_ nums: inout [Int], _ k: Int) {
+    if nums.count == 1 { return }
+    let k = k >= nums.count ? k-nums.count*(k/nums.count) : k
+    let pivot:Int = nums.count-k-1
+    let front:[Int] = Array(nums[0...pivot])
+    let back:[Int] = Array(nums[pivot+1..<nums.count])
+    nums = back + front
 }
 
-print(twoSum([2,7,11,15], 9))
+var nums = [1,2]
+rotate(&nums, 5)
