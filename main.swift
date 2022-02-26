b371578c8a4c50d24732f68ed8d9957f5558d1a4
Fomagran <fomagran6@naver.com>
2022-02-25 18:02:17 -0600

    [LeetCode] SingleNumber 풀이완료!

--- main.swift
+++ main.swift
@@ -1,16 +1,11 @@
-import Foundation
+func singleNumber(_ nums: [Int]) -> Int {
+    var dic:[Int:Bool] = [:]
+    for n in nums {
+        dic[n] = dic[n] == nil ? true : nil
+    }
+    return dic.keys.first!
+  }
 
-//nums의 길이에서 k+1을 뺀 숫자를 축으로 두 개로 나눈다.
-//뒤에 있는 배열을 앞에 붙인다.
+print(singleNumber([4,1,2,1,2]))
 
-func rotate(_ nums: inout [Int], _ k: Int) {
-    if nums.count == 1 { return }
-    let k = k >= nums.count ? k-nums.count*(k/nums.count) : k
-    let pivot:Int = nums.count-k-1
-    let front:[Int] = Array(nums[0...pivot])
-    let back:[Int] = Array(nums[pivot+1..<nums.count])
-    nums = back + front
-}
 
-var nums = [1,2]
-rotate(&nums, 5)
