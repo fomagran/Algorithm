609df902f5cc4478dce587982a17b385eebde2d5
Fomagran <fomagran6@naver.com>
2022-02-27 19:53:59 -0600

    [LeetCode] PlusOne 풀이완료!

--- main.swift
+++ main.swift
@@ -1,8 +1,13 @@
 func plusOne(_ digits: [Int]) -> [Int] {
-    let answer:[Int] = []
+    var answer:[Int] = digits
+    var last:(Int,Int) = (0,0)
+
     for i in stride(from: digits.count-1, through: 0, by: -1) {
-        
+        last = (i,digits[i])
+        answer[i] = answer[i] != 9 ? answer[i] + 1 : 0
+        if answer[i] != 0 { break }
     }
-    return answer
+    return last == (0,9) ? [1] + answer : answer
 }
 
+print(plusOne([0]))
