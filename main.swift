722f0b518a0226ade1eef1ecb85ce365547faa1a
Fomagran <fomagran6@naver.com>
2022-03-05 20:00:50 -0600

    [프로그래머스] 올바른 괄호의 갯수 풀이완료!

--- main.swift
+++ main.swift
@@ -1,12 +1,25 @@
 func solution(_ n:Int) -> Int {
-    if n == 1 { return 1}
-    var cur = 0
-    var answer = 0
-    for i in 1...n-1 {
-        cur += i
-        answer += cur
+    if n <= 2 { return n}
+    var dp:[Int] = Array(repeating: 0, count: n+1)
+    dp[0] = 1
+    dp[1] = 1
+
+    for i in 2...n {
+        let half = i%2 == 1 ? i/2+1 : i/2
+        var last = 0
+        for j in 1...half {
+            dp[i] += dp[j-1] * dp[i-j]
+            last = dp[j-1] * dp[i-j]
+        }
+        dp[i] = i%2 == 0 ? dp[i]*2 : dp[i]*2-last
     }
-    return answer+1
+    return dp.last!
 }
 
-print(solution(5))
+print(solution(7))
+
+
+
+
+
+
