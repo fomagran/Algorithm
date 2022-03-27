3f1c007587d029a05b43e8c202d5ee1c4e9bf6b0
Fomagran <fomagran6@naver.com>
2022-03-27 14:11:02 -0500

    [Programmers] 2019 KAKAO BLIND RECRUITMENT 블록 게임 풀이 완료 및 리팩터링

--- main.swift
+++ main.swift
@@ -1,10 +1,7 @@
 import Foundation
 
-struct Location {
-    let position:CGPoint
-    let number:Int
-    let type:Int
-    let isVertical:Bool
+struct Block {
+    let position:CGPoint,number:Int,type:Int,isVertical:Bool
 }
 
 var numberTypes:[Int:[[[Int]]]] = [
@@ -13,137 +10,75 @@ var numberTypes:[Int:[[[Int]]]] = [
     3:[[[1,1,1],[0,1,0]],[[1,0],[1,1],[1,0]],[[0,1,0],[1,1,1]],[[0,1],[1,1],[0,1]]]
 ]
 
-var canRemoveBlocks:[[Int]] = [
-    [1,2],[1,3],[2,1],[2,2]
-]
-
-var N:Int = 0
-var locations:[Location] = []
+var blocks:[Block] = []
+var count:Int = 0
 
 func solution(_ board:[[Int]]) -> Int {
-    N = board.count
-    findVerticalBox(board)
-    findHorizontalBox(board)
+    findBlocks(board,true)
+    findBlocks(board,false)
     return countRemovableBlocks(board)
 }
 
 func countRemovableBlocks(_ board:[[Int]]) -> Int {
-    var count:Int = 0
-    var sorted = locations.sorted { loc1, loc2 in
-        if loc1.position.y != loc2.position.y {
-            return loc1.position.y > loc2.position.y
-        }else {
-            return loc1.position.x < loc2.position.x
-        }
+    var board = board
+    let sorted = blocks.sorted {$0.position.y < $1.position.y}
+    for block in sorted {
+        checkAboveBlocks(block,&board)
     }
-    print(sorted)
-outer:for (i,location) in sorted.enumerated() {
-    if location.number == 1 {
-        if location.type == 2 {
-                for s in sorted {
-                    let start = s.isVertical ? 0 : -1
-                    for x in start...2{
-                    if s.position.x == location.position.x+CGFloat(x) && location.position.y < s.position.y {
-                        if s.number != location.number || s.type != location.type || s.position != location.position {
-                            continue outer
-                        }
-                    }
-                }
-            }
-            count += 1
-            sorted[i] = Location(position: CGPoint(x: -1, y: -1), number: -1, type: -1, isVertical: false)
-        }else if location.type == 3 {
-                for s in sorted {
-                    let start = s.isVertical ? -1 : -2
-                    for x in start...0{
-                    if s.position.x == location.position.x+CGFloat(x) && location.position.y < s.position.y {
-                        if s.number != location.number || s.type != location.type || s.position != location.position {
-                            continue outer
-                        }
-                    }
-                }
-            }
-            count += 1
-            sorted[i] = Location(position: CGPoint(x: -1, y: -1), number: -1, type: -1, isVertical: false)
-        }
-    }else if location.number == 2 {
-        if location.type == 1 {
-                for s in sorted {
-                    let start = s.isVertical ? 0 : -1
-                    for x in start...1{
-                    if s.position.x == location.position.x+CGFloat(x) && location.position.y < s.position.y {
-                        if s.number != location.number || s.type != location.type || s.position != location.position {
-                            continue outer
-                        }
-                    }
-                }
-            }
-            count += 1
-            sorted[i] = Location(position: CGPoint(x: -1, y: -1), number: -1, type: -1, isVertical: false)
-        }else if location.type == 2 {
-                for s in sorted {
-                    let start = s.isVertical ? -1 : -2
-                    for x in start...1{
-                    if s.position.x == location.position.x+CGFloat(x) && location.position.y < s.position.y {
-                        if s.number != location.number || s.type != location.type || s.position != location.position {
-                            continue outer
-                        }
-                    }
-                }
-            }
-            count += 1
-            sorted[i] = Location(position: CGPoint(x: -1, y: -1), number: -1, type: -1, isVertical: false)
-        }
-    }else if location.number == 3 {
-        if location.type == 0 {
-            for s in sorted {
-                let start = s.isVertical ? -1 : -2
-                for x in start...2 {
-                    if s.position.x == location.position.x+CGFloat(x) && location.position.y < s.position.y {
-                        if s.number != location.number || s.type != location.type {
-                            continue outer
-                        }
-                    }
-                }
-            }
-            count += 1
-            sorted[i] = Location(position: CGPoint(x: -1, y: -1), number: -1, type: -1, isVertical: false)
-        }
-    }
-}
     return count
 }
 
-func findVerticalBox(_ board:[[Int]]) {
-    for i in 0..<N-2 {
-        for j in 0..<N-1 {
-            var block:[[Int]] = Array(repeating: [], count: 3)
-            for k in 0...2 {
-                block[2-k] = Array(board[i+k][j...j+1])
-            }
-            let change = isExistEqual4Number(block)
-            if let change = change {
-                let equal = checkEqualBlock(change, isVertical: true, position: CGPoint(x: j, y: N-(i+3)))
-                if let equal = equal {
-                    locations.append(equal)
-                }
+func checkAboveBlocks(_ block:Block,_ board:inout [[Int]]) {
+    var xs:[Int] = []
+    let x = Int(block.position.x)
+    let y = Int(block.position.y)
+    var remove:[(Int,Int)] = []
+    if block.number == 1 && block.type == 2{
+        xs = [x+1,x+2]
+        remove = [(x,y),(x+1,y),(x+2,y),(x,y-1)]
+    }else if block.number == 1 && block.type == 3 {
+        xs = [x]
+        remove = [(x,y),(x+1,y),(x+1,y-1),(x+1,y-2)]
+    }else if block.number == 2 && block.type == 1 {
+        xs = [x+1]
+        remove = [(x,y),(x+1,y),(x,y-1),(x,y-2)]
+    }else if block.number == 2 && block.type == 2 {
+        xs = [x,x+1]
+        remove = [(x,y),(x+1,y),(x+2,y),(x+2,y-1)]
+    }else if block.number == 3 && block.type == 0 {
+        xs = [x,x+2]
+        remove = [(x,y),(x+1,y),(x+2,y),(x+1,y-1)]
+    }else {
+        return
+    }
+    for x in xs {
+        for y in 0...y-1 {
+            if board[y][Int(x)] != 0 {
+                return
             }
         }
     }
+    for r in remove {
+        board[r.1][r.0] = 0
+    }
+    count += 1
 }
 
-func findHorizontalBox(_ board:[[Int]]) {
-    for i in 0..<N-1 {
-        for j in 0..<N-2 {
-            var block:[[Int]] = Array(repeating: [], count: 2)
-            for k in 0...1 {
-                block[1-k] = Array(board[i+k][j...j+2])
+func findBlocks(_ board:[[Int]],_ isVertical:Bool) {
+    let row = isVertical ? 3 : 2
+    let col = isVertical ? 2 : 3
+    
+    for i in 0...board.count-row {
+        for j in 0...board.count-col {
+            var block:[[Int]] = Array(repeating: [], count: row)
+            for k in 0..<row {
+                block[row-1-k] = Array(board[i+k][j...j+col-1])
             }
             let change = isExistEqual4Number(block)
             if let change = change {
-                let equal = checkEqualBlock(change, isVertical: false, position: CGPoint(x: j, y: N-(i+2)))
+                let equal = checkEqualBlock(change, isVertical: isVertical, position: CGPoint(x: j, y:i+row-1))
                 if let equal = equal {
-                    locations.append(equal)
+                    blocks.append(equal)
                 }
             }
         }
@@ -154,17 +89,17 @@ func isExistEqual4Number(_ block:[[Int]]) -> [[Int]]? {
     var numberCount:[Int:Int] = [:]
     var changeBlock:[[Int]] = []
     for b in block {
-        for e in b {
-            if numberCount[e] == nil {
-                numberCount[e] = 1
+        for n in b {
+            if numberCount[n] == nil && n != 0 {
+                numberCount[n] = 1
             }else {
-                numberCount[e]! += 1
+                numberCount[n]! += 1
             }
         }
     }
     
     for (n,c) in numberCount {
-        if c == 4 && n != 0 {
+        if c == 4 {
             for (i,b) in block.enumerated() {
                 changeBlock.append([])
                 for e in b {
@@ -179,23 +114,31 @@ func isExistEqual4Number(_ block:[[Int]]) -> [[Int]]? {
 }
 
 
-func checkEqualBlock(_ block:[[Int]],isVertical:Bool,position:CGPoint) -> Location? {
-    if isVertical {
-        for i in 1...3 {
-            for o in [1,3] {
-                if numberTypes[i]![o] == block {
-                    return Location(position: position, number: i, type: o, isVertical:isVertical)
-                }
-            }
-        }
-    }else {
-        for i in 1...3 {
-            for e in [0,2] {
-                if numberTypes[i]![e] == block {
-                    return Location(position: position, number: i, type: e, isVertical:isVertical)
-                }
+func checkEqualBlock(_ block:[[Int]],isVertical:Bool,position:CGPoint) -> Block? {
+    let type = isVertical ? [1,3] : [0,2]
+    for i in 1...3 {
+        for t in type {
+            if numberTypes[i]![t] == block {
+                return Block(position: position, number: i, type: t, isVertical:isVertical)
             }
         }
     }
     return nil
 }
+
+print(solution([[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,4,0,0,0],[0,0,0,0,0,4,4,0,0,0],[0,0,0,0,3,0,4,0,0,0],[0,0,0,2,3,0,0,0,5,5],[1,2,2,2,3,3,0,0,0,5],[1,1,1,0,0,0,0,0,0,5]]))
+
+//print(solution([[0,0,0,0,0,0,0,0,0,0]
+//                 ,[0,0,0,0,0,0,0,0,0,0]
+//                 ,[0,0,0,0,0,0,0,0,0,0]
+//                 ,[0,0,0,0,0,0,0,0,0,0]
+//                 ,[0,0,0,2,2,0,0,0,0,0]
+//                 ,[0,0,0,2,1,0,0,0,0,0]
+//                 ,[0,0,0,2,1,0,0,0,0,0]
+//                 ,[0,0,0,0,1,1,0,0,0,0]
+//                 ,[0,0,0,0,0,0,0,0,0,0]
+//                 ,[0,0,0,0,0,0,0,0,0,0]]))
+
+//print(solution([[0, 0, 0, 0, 0], [1, 0, 0, 2, 0], [1, 2, 2, 2, 0], [1, 1, 0, 0, 0], [0, 0, 0, 0, 0]]))
+
+//print(solution([[0, 0, 1, 1, 1], [0, 0, 0, 1, 0], [3, 0, 0, 2, 0], [3, 2, 2, 2, 0], [3, 3, 0, 0, 0]]))
