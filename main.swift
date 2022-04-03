aa5cdfc7f74cf74e6fc32127810b536b6e0116f9
Fomagran <fomagran6@naver.com>
2022-04-02 21:04:30 -0500

    [LeetCode] Reverse Integer Solution

--- main.swift
+++ main.swift
@@ -1,29 +1,14 @@
 import Foundation
 
-func longestPalindrome(_ s: String) -> String {
-    let str:[String] = s.map{String($0)}
-    var maxLength:Int = 1
-    var palStartAndEnd:(Int,Int) = (0,0)
-    for i in 0..<str.count-1 {
-    jfor:for j in stride(from: str.count-1, through: i+1, by: -1) {
-        let length = j-i+1
-        if length <= maxLength { continue }
-        
-        let middle = length%2 != 0 ? length/2 - 1 : length/2
-        
-        for m in 0...middle {
-            if str[i+m] != str[j-m] {
-                continue jfor
-            }
-        }
-        maxLength = length
-        palStartAndEnd = (i,j)
+func reverse(_ x: Int) -> Int {
+    let nOrP = x < 0 ? -1 : 1
+    var reverse = Array(String(x).map{String($0)}.reversed())
+    if x < 0 {
+        reverse.removeLast()
     }
-    }
-    let start = s.index(s.startIndex,offsetBy: palStartAndEnd.0)
-    let end = s.index(s.startIndex,offsetBy: palStartAndEnd.1)
-    return String(s[start...end])
+    let answer = Int(Int32(reverse.joined()) ?? 0)
+    return answer * nOrP
 }
 
 
-print(longestPalindrome("babad"))
+print(reverse(123))
