d509ffb7480153210a337ff3f58d516f3d125c49
Fomagran <fomagran6@naver.com>
2022-04-02 20:08:27 -0500

    [LeetCode] Logest Palindromic Substring Solution

--- main.swift
+++ main.swift
@@ -1,38 +1,29 @@
 import Foundation
 
 func longestPalindrome(_ s: String) -> String {
-
     let str:[String] = s.map{String($0)}
     var maxLength:Int = 1
-    var longestPalindrome:[String] = []
+    var palStartAndEnd:(Int,Int) = (0,0)
     for i in 0..<str.count-1 {
-        for j in stride(from: str.count-1, to: i+1, by: -1) {
-            var length = j-i+1
-            var palindrom:[String] = Array(repeating: "", count: length)
-            if length <= maxLength { continue }
-            
-            let middle = length%2 != 0 ? length/2 - 1 : length/2
-            for m in 0...middle {
-                if str[i+m] != str[j-m] {
-                    length += 1
-                    break
-                }else {
-                    palindrom[m] = str[i+m]
-                    palindrom[length-m] = str[j-m]
-                }
-                print(palindrom)
-            }
-            
-            if length == j-i+1 {
-                if length > maxLength {
-                    maxLength = length
-                    longestPalindrome = palindrom
-                }
+    jfor:for j in stride(from: str.count-1, through: i+1, by: -1) {
+        let length = j-i+1
+        if length <= maxLength { continue }
+        
+        let middle = length%2 != 0 ? length/2 - 1 : length/2
+        
+        for m in 0...middle {
+            if str[i+m] != str[j-m] {
+                continue jfor
             }
         }
+        maxLength = length
+        palStartAndEnd = (i,j)
     }
-    return longestPalindrome.joined()
+    }
+    let start = s.index(s.startIndex,offsetBy: palStartAndEnd.0)
+    let end = s.index(s.startIndex,offsetBy: palStartAndEnd.1)
+    return String(s[start...end])
 }
 
 
-print(longestPalindrome("cbbd"))
+print(longestPalindrome("babad"))
