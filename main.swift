27e316a98a8d2f49b233897d0b090f7fba99aff4
Fomagran <fomagran6@naver.com>
2022-04-28 22:46:13 -0500

    [LeetCode] Remove Nth End of List Refactoring

--- main.swift
+++ main.swift
@@ -11,8 +11,7 @@ func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
     var currentIndex:Int = 1
     var currentNode:ListNode? = head
     var sz:ListNode? = nil
-    let length:Int = countNodeList(head)
-    let N:Int = length - (n-1)
+    let N:Int = removeStartIndex()
     
     if N == 1 {
         return head?.next
@@ -20,43 +19,42 @@ func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
     
     while currentNode != nil {
         if currentIndex == N {
-            append(currentNode?.next, &sz)
-            break
+            append(currentNode?.next)
+            return sz
         }
         if sz == nil {
             sz = ListNode(head!.val,nil)
         }else {
-            append(ListNode(currentNode!.val,nil), &sz)
+            append(ListNode(currentNode!.val,nil))
         }
         currentNode = currentNode!.next
         currentIndex += 1
     }
     
+    func append(_ add:ListNode?) {
+        var node:ListNode? = sz
+        while node?.next != nil {
+            node = node?.next!
+        }
+        node?.next = add
+    }
+    
+    func removeStartIndex() -> Int {
+        var count:Int = 0
+        var current:ListNode? = head
+        while current != nil {
+            current = current!.next
+            count += 1
+        }
+        return count - (n-1)
+    }
+    
     return sz
-}
-
-func append(_ add:ListNode?,_ root:inout ListNode?) {
-    var node:ListNode? = root
-    while node?.next != nil {
-        node = node?.next!
-    }
-    node?.next = add
-}
-
-
-func countNodeList(_ head:ListNode?) -> Int {
-    var count:Int = 0
-    var current:ListNode? = head
-    while current != nil {
-        current = current!.next
-        count += 1
-    }
-    return count
 }
 
 
 var head:ListNode? = ListNode(1)
-append(ListNode(2),&head)
+
 //append(ListNode(3),&head)
 //append(ListNode(4),&head)
 //append(ListNode(5),&head)
