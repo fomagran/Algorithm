03c51bf7fa9a390ee447dcd83fdfd4b1f6fa27c0
Fomagran <fomagran6@naver.com>
2022-05-01 17:54:50 -0500

    [LeetCode] Merge Two Sorted Lists Solution

--- main.swift
+++ main.swift
@@ -1,22 +1,57 @@
-func isValid(_ s: String) -> Bool {
-    let sChars = s.map{$0}
-    let brackets:[Character:Character] = [")":"(","}":"{","]":"["]
-    var openBrackets:[Character] = []
-    
-    for c in sChars {
-        if c == "(" ||  c == "{" || c == "["   {
-            openBrackets.append(c)
-        }else {
-            if openBrackets.isEmpty {
-                return false
-            }
-            
-            if openBrackets.removeLast() != brackets[c] {
-                return false
-            }
-        }
-    }
-    return openBrackets.isEmpty
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
 }
 
-print(isValid("]"))
+func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
+    var l1:ListNode? = list1
+    var l2:ListNode? = list2
+    var answer:ListNode? = nil
+    
+    if list1 == nil || list2 == nil {
+        return list1 == nil ? list2 : list1
+    }
+    
+    while l1 != nil || l2 != nil {
+        if l1 == nil {
+            append(l2)
+            return answer
+        }
+        
+        if l2 == nil {
+            append(l1)
+            return answer
+        }
+        
+        if l1!.val < l2!.val {
+            if answer == nil {
+                answer = ListNode(l1!.val)
+            }else {
+                append(ListNode(l1!.val))
+            }
+            l1 = l1?.next
+        }else {
+            if answer == nil {
+                answer = ListNode(l2!.val)
+            }else {
+            append(ListNode(l2!.val))
+            }
+            l2 = l2?.next
+        }
+    }
+    
+    func append(_ add:ListNode?) {
+           var node:ListNode? = answer
+           while node?.next != nil {
+               node = node?.next!
+           }
+           node?.next = add
+       }
+    
+    return answer
+}
+
+
