4d9cfaee25633bd6fb73f95938bd4fe1e1995668
Fomagran <fomagran6@naver.com>
2022-05-01 17:22:59 -0500

    [LeetCode] Valid Parentheses Solution

--- main.swift
+++ main.swift
@@ -1,33 +1,22 @@
-func findMedianSortedArrays(_ nums1: [Int], _ nums2: [Int]) -> Double {
-    var point1:Int = 0
-    var point2:Int = 0
-    let allCount = nums1.count + nums2.count
-    let medianIndex:[Int] = allCount%2 == 0 ? [allCount/2-1,allCount/2] : [allCount/2]
-    var mergeArray:[Int] = []
+func isValid(_ s: String) -> Bool {
+    let sChars = s.map{$0}
+    let brackets:[Character:Character] = [")":"(","}":"{","]":"["]
+    var openBrackets:[Character] = []
     
-    
-    while point1 < nums1.count || point2 < nums2.count {
-        if point1 == nums1.count {
-            mergeArray.append(contentsOf: nums2[point2..<nums2.count])
-            break
-        }
-        
-        if point2 == nums2.count {
-            mergeArray.append(contentsOf: nums1[point1..<nums1.count])
-            break
-        }
-        
-        mergeArray.append(min(nums1[point1],nums2[point2]))
-        
-        if nums1[point1] < nums2[point2] {
-            point1 += 1
+    for c in sChars {
+        if c == "(" ||  c == "{" || c == "["   {
+            openBrackets.append(c)
         }else {
-            point2 += 1
+            if openBrackets.isEmpty {
+                return false
+            }
+            
+            if openBrackets.removeLast() != brackets[c] {
+                return false
+            }
         }
     }
-
-    return medianIndex.map{Double(mergeArray[$0])}.reduce(0,+)/Double(medianIndex.count)
+    return openBrackets.isEmpty
 }
 
-
-print(findMedianSortedArrays([1,3,5], [2,4,8]))
+print(isValid("]"))
