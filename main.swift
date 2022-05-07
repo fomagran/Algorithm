08f50bc756e49874000185daff1ae26c4a7b7d96
Fomagran <fomagran6@naver.com>
2022-05-07 18:12:14 -0500

    [LeetCode] Search in Rotated Sorted Array Solution

--- main.swift
+++ main.swift
@@ -1,22 +1,36 @@
-func nextPermutation(_ nums: inout [Int]) {
-    var lastLessIndex:Int = -1
-    var lastBiggerIndex:Int = -1
-    for i in 0..<nums.count-1 {
-        if nums[i] < nums[i+1] {
-            lastLessIndex = i
-        }
-    }
-    if lastLessIndex < 0 {
-        nums.reverse()
-        return
-    }
+func search(_ nums: [Int], _ target: Int) -> Int {
+    var l:Int = 0
+    var r:Int = nums.count-1
     
-    for j in lastLessIndex+1..<nums.count {
-        if nums[j] > nums[lastLessIndex] {
-            lastBiggerIndex = j
+    while l < r {
+        let m:Int = (l+r)/2
+        if nums[l] > nums[m] {
+            if nums[l] <= target || nums[m] >= target {
+                r = m
+                continue
+            }
+        }else {
+            if nums[l]...nums[m] ~= target {
+                r = m
+                continue
+            }
         }
+        
+        if nums[r] > nums[m+1] {
+            if nums[m+1]...nums[r] ~= target {
+                l = m+1
+                continue
+            }
+        }else {
+            if nums[r] <= target || nums[m+1] >= target {
+                l = m+1
+                continue
+            }
+        }
+        return -1
     }
-    
-    nums.swapAt(lastLessIndex, lastBiggerIndex)
-    nums = nums[0...lastLessIndex] + nums[lastLessIndex+1..<nums.count].reversed()
+    return nums[r] == target ? r : -1
 }
+
+
+print(search([5,1,3],3))
