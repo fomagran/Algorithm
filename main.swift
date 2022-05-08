2fc3c53983493d2255cdaab22412574b283b9d22
Fomagran <fomagran6@naver.com>
2022-05-08 14:26:03 -0500

    [LeetCode] Find First and Last Position of Element in Sorted Array Solution

--- main.swift
+++ main.swift
@@ -1,36 +1,51 @@
-func search(_ nums: [Int], _ target: Int) -> Int {
+func searchRange(_ nums: [Int], _ target: Int) -> [Int] {
+    if nums.isEmpty { return [-1,-1] }
     var l:Int = 0
     var r:Int = nums.count-1
     
-    while l < r {
-        let m:Int = (l+r)/2
-        if nums[l] > nums[m] {
-            if nums[l] <= target || nums[m] >= target {
-                r = m
-                continue
-            }
-        }else {
+    func findTargetIndex() {
+        while l < r {
+            let m:Int = (l+r)/2
             if nums[l]...nums[m] ~= target {
                 r = m
-                continue
+            }else {
+                l = m+1
             }
         }
-        
-        if nums[r] > nums[m+1] {
-            if nums[m+1]...nums[r] ~= target {
-                l = m+1
-                continue
-            }
-        }else {
-            if nums[r] <= target || nums[m+1] >= target {
-                l = m+1
-                continue
-            }
-        }
-        return -1
     }
-    return nums[r] == target ? r : -1
+    
+    findTargetIndex()
+    
+    if nums[r] != target {
+        return [-1,-1]
+    }
+    
+    func findStartIndex() -> Int {
+        var start = r-1
+        while start > 0 {
+            if nums[start] == target {
+                start -= 1
+                continue
+            }
+            break
+        }
+        return start+1
+    }
+    
+    func findEndIndex() -> Int {
+        var end = l+1
+        while end < nums.count {
+            if nums[end] == target {
+                end += 1
+                continue
+            }
+            break
+        }
+        return end-1
+    }
+    
+    return [findStartIndex(),findEndIndex()]
 }
 
 
-print(search([5,1,3],3))
+print(searchRange([], 6))
