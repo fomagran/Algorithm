3105154bde455396132c8f0a99a9246558b1bb0e
Fomagran <fomagran6@naver.com>
2022-05-13 22:18:59 -0500

    [LeetCode] Search Insert Position Solution

--- main.swift
+++ main.swift
@@ -1,40 +1,8 @@
-public class ListNode {
-    public var val: Int
-    public var next: ListNode?
-    public init() { self.val = 0; self.next = nil; }
-    public init(_ val: Int) { self.val = val; self.next = nil; }
-    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
-}
-
-func mergeKLists(_ lists: [ListNode?]) -> ListNode? {
-    var answer:ListNode? = nil
-    var current:ListNode? = nil
-    var first:ListNode? = nil
-    var last:ListNode? = nil
-    
-    for list in lists {
-        if answer == nil {
-            appendAll(list)
-            continue
-        }else {
-            
+func searchInsert(_ nums: [Int], _ target: Int) -> Int {
+    for (i,n) in nums.enumerated() {
+        if n >= target {
+            return i
         }
     }
-    
-    func appendAll(_ l:ListNode?) {
-        var current = l
-        while current != nil {
-            if let _ = answer {
-                answer?.next = ListNode(current!.val)
-                answer = answer?.next
-            }else {
-                answer = current
-            }
-            current = current?.next
-        }
-    }
-    
-    return answer
+    return nums.count
 }
-
-
