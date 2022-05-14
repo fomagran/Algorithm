254db55d08ce6e0ca5775b1f1b60276d49b22d38
Fomagran <fomagran6@naver.com>
2022-05-13 21:21:42 -0500

    [LeetCode] Longest Valid Parentheses Solution

--- main.swift
+++ main.swift
@@ -1,51 +1,25 @@
-func searchRange(_ nums: [Int], _ target: Int) -> [Int] {
-    if nums.isEmpty { return [-1,-1] }
-    var l:Int = 0
-    var r:Int = nums.count-1
-    
-    func findTargetIndex() {
-        while l < r {
-            let m:Int = (l+r)/2
-            if nums[l]...nums[m] ~= target {
-                r = m
-            }else {
-                l = m+1
-            }
-        }
-    }
-    
-    findTargetIndex()
-    
-    if nums[r] != target {
-        return [-1,-1]
-    }
-    
-    func findStartIndex() -> Int {
-        var start = r-1
-        while start > 0 {
-            if nums[start] == target {
-                start -= 1
+func longestValidParentheses(_ s: String) -> Int {
+    var openIndex:[Int] = []
+    let smap = s.map{String($0)}
+    var maxLength:Int = -1
+    var current:Int = 0
+
+    for (i,char) in smap.enumerated() {
+        if char == "(" {
+            openIndex.append(i-current)
+        }else {
+            if !openIndex.isEmpty {
+                let last:Int = openIndex.removeLast()
+                maxLength = max(maxLength,i-last)
+                current = i-last+1
                 continue
             }
-            break
         }
-        return start+1
+        current = 0
     }
-    
-    func findEndIndex() -> Int {
-        var end = l+1
-        while end < nums.count {
-            if nums[end] == target {
-                end += 1
-                continue
-            }
-            break
-        }
-        return end-1
-    }
-    
-    return [findStartIndex(),findEndIndex()]
+
+    return maxLength+1
 }
 
 
-print(searchRange([], 6))
+print(longestValidParentheses("(()()"))
