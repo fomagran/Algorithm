6c3ef5c24385f826942c7725937ed6578139b34c
Fomagran <fomagran6@naver.com>
2022-05-15 16:57:07 -0500

    [LeetCode] Trapping Rain Water Solution

--- main.swift
+++ main.swift
@@ -1,15 +1,24 @@
-func firstMissingPositive(_ nums: [Int]) -> Int {
-    var dic:[Int:Bool] = [:]
+func trap(_ height: [Int]) -> Int {
+    var answer:Int = 0
+    var left:Int = 0
+    var right:Int = height.count-1
+    var leftMax:Int = 0
+    var rightMax:Int = 0
     
-    nums.forEach {
-        dic[$0] = true
+    while left < right {
+        leftMax = max(height[left],leftMax)
+        rightMax = max(height[right],rightMax)
+        
+        if leftMax < rightMax {
+            answer += leftMax - height[left]
+            left += 1
+        }else {
+            answer += rightMax - height[right]
+            right -= 1
+        }
     }
-    
-    var current = 1
-    while dic[current] != nil {
-        current += 1
-    }
-    return current
+
+    return answer
 }
 
-print(firstMissingPositive([1,2,0]))
+print(trap([0,1,0,2,1,0,1,3,2,1,2,1]))
