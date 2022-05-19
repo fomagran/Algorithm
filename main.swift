1615076cc12dd86ca73240cdf30a379cde8a6455
Fomagran <fomagran6@naver.com>
2022-05-18 20:49:39 -0500

    [LeetCode] Swap Nodes In Pairs (using array) Solution

--- main.swift
+++ main.swift
@@ -1,27 +1,29 @@
-func mergeKLists(_ lists: [ListNode?]) -> ListNode? {
-    var array:[Int] = []
-    
-    for list in lists {
-        var node = list
-        while node != nil {
-            array.append(node!.val)
-            node = node?.next
-        }
-    }
-    
-    var node:ListNode? = nil
-    var root:ListNode? = nil
-    
-    for n in array.sorted() {
-        if node == nil {
-            node = ListNode(n)
-            root = node
-        }else {
-            node?.next = ListNode(n)
-            node = node?.next
-        }
-    }
-    
-    return root
-}
+func swapPairs(_ head: ListNode?) -> ListNode? {
+       if head?.next == nil { return head }
+       var current:ListNode? = ListNode(head!.next!.val,ListNode(head!.val))
+       var root:ListNode? = current
+       var array:[Int] = []
+       var h:ListNode? = head
+   
 
+       while h != nil {
+           array.append(h!.val)
+           h = h?.next
+       }
+     
+       var x = 3
+       
+       current = current?.next
+       
+       while x < array.count {
+           current?.next = ListNode(array[x],ListNode(array[x-1]))
+           current = current?.next?.next
+           x += 2
+       }
+       
+       if array.count%2 == 1 {
+           current?.next = ListNode(array[array.count-1])
+       }
+       
+       return root
+   }
