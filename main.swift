2c11faef67051c669a1a81cf72f337aa804d8579
Fomagran <fomagran6@naver.com>
2022-05-27 20:37:40 -0500

    [LeetCode] Group Anagrams Solution

--- main.swift
+++ main.swift
@@ -1,19 +1,14 @@
-//func rotate(_ matrix: inout [[Int]]) {
-//    for (i,mat) in matrix.enumerated() {
-//        let col:Int = matrix.count-1-i
-//        for (j,m) in mat.enumerated(){
-//            matrix[j][col] = m
-//        }
-//    }
-//}
-
-func rotate(_ matrix: inout [[Int]]) {
-    let matrixCopy = matrix
-    for i in 0..<matrixCopy.count {
-        matrix[i] = matrixCopy.map{$0[i]}.reversed()
+func groupAnagrams(_ strs: [String]) -> [[String]] {
+    var dic:[String:[String]] = [:]
+    for str in strs {
+        let sort = str.map{String($0)}.sorted().joined()
+        if dic[sort] == nil {
+            dic[sort] = [str]
+        }else {
+            dic[sort]!.append(str)
+        }
     }
+    return Array(dic.values)
 }
 
-var m = [[1,2,3],[4,5,6],[7,8,9]]
-
-rotate(&m)
+groupAnagrams(["eat","tea","tan","ate","nat","bat"])
