ea5b31b2d341791daa4209808bb59b8ff59a1afe
Fomagran <fomagran6@naver.com>
2022-05-30 20:19:36 -0500

    [LeetCode] Minimum Path Sum DFS Version

--- main.swift
+++ main.swift
@@ -1,33 +1,30 @@
-func uniquePaths(_ m: Int, _ n: Int) -> Int {
-    if m == 1 || n == 1 {
-        return 1
-    }
-    let bigger:Int = m > n ? m-1 : n-1
-    let less:Int = m > n ? n : m
-    var paths:[[Int]] = [(1...less).map{$0}]
+func minPathSum(_ grid: [[Int]]) -> Int {
+    var answer:Int = Int.max
+    let m:Int = grid.count-1
+    let n:Int = grid[0].count-1
     
-    for i in 1..<bigger {
-        var newPath:[Int] = [1]
-        let prev:[Int] = paths[i-1]
-        for j in 1..<less {
-            newPath.append(newPath[j-1] + prev[j])
+    func dfs(c:Int,r:Int,sum:Int) {
+        
+        if r == m && c == n {
+            answer = min(answer,sum+grid[r][c])
+            return
         }
-        paths.append(newPath)
+        
+        if r > m || c > n   {
+            return
+        }
+        
+        if sum+grid[r][c] >= answer {
+            return
+        }
+        
+        dfs(c: c+1, r: r, sum: sum+grid[r][c])
+        dfs(c: c, r: r+1, sum: sum+grid[r][c])
     }
     
-    return paths[bigger-1][less-1]
+    dfs(c: 0, r: 0, sum:0)
+    
+    return answer
 }
 
-//1
-//1 2 3 4 5
-//1 3 6 10 15
-//1 4 10 20 35
-//1 5 15 35 70
-//1 6 21 56 126
-//1 7 28 84 210
-//1 8 36 120 330
-
-//5 10 15 35 70
-
-
-print(uniquePaths(1,1))
+print(minPathSum([[7,1,3,5,8,9,9,2,1,9,0,8,3,1,6,6,9,5],[9,5,9,4,0,4,8,8,9,5,7,3,6,6,6,9,1,6],[8,2,9,1,3,1,9,7,2,5,3,1,2,4,8,2,8,8],[6,7,9,8,4,8,3,0,4,0,9,6,6,0,0,5,1,4],[7,1,3,1,8,8,3,1,2,1,5,0,2,1,9,1,1,4],[9,5,4,3,5,6,1,3,6,4,9,7,0,8,0,3,9,9],[1,4,2,5,8,7,7,0,0,7,1,2,1,2,7,7,7,4],[3,9,7,9,5,8,9,5,6,9,8,8,0,1,4,2,8,2],[1,5,2,2,2,5,6,3,9,3,1,7,9,6,8,6,8,3],[5,7,8,3,8,8,3,9,9,8,1,9,2,5,4,7,7,7],[2,3,2,4,8,5,1,7,2,9,5,2,4,2,9,2,8,7],[0,1,6,1,1,0,0,6,5,4,3,4,3,7,9,6,1,9]]))
