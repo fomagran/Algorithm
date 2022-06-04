7b443d1466fb1d4886ab961de8de3c29b41785e3
Fomagran <fomagran6@naver.com>
2022-06-04 16:16:08 -0500

    [LeetCode] Search a 2D Matrix Binary Search Version

--- main.swift
+++ main.swift
@@ -1,59 +1,33 @@
 /*
- matrix wrap
- prevent to x < 0 or x > matrix.count
- or y < 0 or y > matrix.count
- -----
- |    |
- |    |
- |    |
- |    |
- |    |
- ------
- if matrix[y][x] == 0 {
-    matrix[y-1][x] = 0
-    matrix[y][x-1] = 0
-    matrix[y+1][x] = 0
-    matrix[y][x+1] = 0
- }
- 
- if matrix[x][y] == 0 {
-    matrix[y-1][x] = 0
-    matrix[y][x-1] = 0
-    matrix[y+1][x] = 0
-    matrix[y][x+1] = 0
- }
- 
+ search matrix's each row first value, last value
+ if target is between first value and last value
+ search above row's all element and if target < element stop for loop
+ return false
+ or meet the target value rreturn true
  */
 
-
-func setZeroes(_ matrix: inout [[Int]]) {
-    let matrixCopy:[[Int]] = matrix
-    for y in 0..<matrix.count {
-        for x in 0..<matrix[0].count {
-            if matrixCopy[y][x] == 0 {
-                changeAllRow(x)
-                changeAllColumn(y)
+func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
+    for m in matrix {
+        if m[0]...m[m.count-1] ~= target {
+            
+            var l:Int = 0
+            var r:Int = m.count-1
+            
+            while l < r {
+                let middle:Int = (l+r)/2
+                if m[l]...m[middle] ~= target {
+                    r = middle
+                }else {
+                    l = middle+1
+                }
             }
+            
+            return m[l] == target
         }
     }
-    
-    func changeAllRow(_ x:Int) {
-        for i in 0..<matrix.count {
-            matrix[i][x] = 0
-        }
-    }
-
-    func changeAllColumn(_ y:Int) {
-        for i in 0..<matrix[0].count {
-            matrix[y][i] = 0
-        }
-    }
+    return false
 }
 
 
 
-var matrix:[[Int]] = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
-print(setZeroes(&matrix),matrix)
-
-
-
+print(searchMatrix([[1,3]], 3))
