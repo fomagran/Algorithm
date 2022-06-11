9c69c4e77e9938e9a5295c61acb07cac9afdbbc6
Fomagran <fomagran6@naver.com>
2022-06-10 22:01:05 -0500

    [LeetCode] String to Integer(atoi) Refactoring

--- main.swift
+++ main.swift
@@ -7,48 +7,31 @@
  
  */
 
-import Darwin
-
-print(Int("s"))
+import Foundation
 
 func myAtoi(_ s: String) -> Int {
     var record:String = ""
-    //2
-    var negative:String = ""
-    var trimS = s
-
-    let stringMap:[Character] = s.map{$0}
+    var trimS = String(NSString(string: s.trimmingCharacters(in: .whitespaces)))
+    var isNegative:Bool = false
+    
+    if trimS.first == "-" || trimS.first == "+" {
+        isNegative = trimS.first == "-"
+        trimS.removeFirst()
+    }
+    
+    let stringMap:[Character] = trimS.map{$0}
     
     for c in stringMap {
-        if c == "-" || c == "+"  {
-            if Int(record) == nil && negative == "" {
-            negative = String(c)
-                continue
-            }else {
-                break
-            }
-        }
-        
-        //1
-        if c == " " {
-            if record == "" && negative == "" {
-                continue
-            }
-            break
-        }
-        
-        //3
         if Int(String(c)) == nil {
             break
         }
-        
         record += String(c)
     }
     
     let min:Double = -pow(2, 31)
     let max:Double = pow(2,31)-1
     
-    let answer:Double = negative != "-" ? (Double(record) ?? 0) : -(Double(record) ?? 0)
+    let answer:Double = !isNegative ? (Double(record) ?? 0) : -(Double(record) ?? 0)
     
     if min...max ~= answer {
         return Int(answer)
