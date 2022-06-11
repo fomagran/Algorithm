b196101746f4d94bbfd314e9363e0078866cd884
Fomagran <fomagran6@naver.com>
2022-06-10 22:34:37 -0500

    [LeetCode] Integer to roman Solution

--- main.swift
+++ main.swift
@@ -1,43 +1,38 @@
 /*
- ignore white space
- '-' negative, '+' positive
- stop non digit
- convert integer
- check -2^31 ~ 2^31-1 if the number is out of range return -2^31(less) or 2^31-1(greater)
+ 1. num을 string으로 바꿔 각 자리의 숫자를 확인한다.
+ 2. 만약 1~5,4,6~8,9인지 확인해 각 조건에 맞게 Symbol을 넣어준다.
  
  */
 
-import Foundation
-
-func myAtoi(_ s: String) -> Int {
-    var record:String = ""
-    var trimS = String(NSString(string: s.trimmingCharacters(in: .whitespaces)))
-    var isNegative:Bool = false
+func intToRoman(_ num: Int) -> String {
+    let roman = [1:["I","V"],10:["X","L"],100:["C","D"],1000:["M"]]
+    let numToString:[String] = String(num).map{String($0)}
+    let val:[Int] = [1,10,100,1000]
+    var answer:String = ""
     
-    if trimS.first == "-" || trimS.first == "+" {
-        isNegative = trimS.first == "-"
-        trimS.removeFirst()
-    }
-    
-    let stringMap:[Character] = trimS.map{$0}
-    
-    for c in stringMap {
-        if Int(String(c)) == nil {
-            break
+    for (i,n) in numToString.enumerated() {
+        let intN:Int = Int(n) ?? 0
+        let key = val[numToString.count - i - 1]
+        if 1...3 ~= intN {
+            for _ in 0..<intN {
+                answer += roman[key]![0]
+            }
+        }else if intN == 4 {
+            answer += roman[key]![0]
+            answer += roman[key]![1]
+        }else if intN == 5 {
+            answer += roman[key]![1]
+        }else if 6...8 ~= intN {
+            answer += roman[key]![1]
+            for _ in 5..<intN {
+                answer += roman[key]![0]
+            }
+        }else if intN == 9 {
+            answer += roman[key]![0]
+            answer += roman[key*10]![0]
         }
-        record += String(c)
     }
-    
-    let min:Double = -pow(2, 31)
-    let max:Double = pow(2,31)-1
-    
-    let answer:Double = !isNegative ? (Double(record) ?? 0) : -(Double(record) ?? 0)
-    
-    if min...max ~= answer {
-        return Int(answer)
-    }
-    
-    return answer < min ? Int(min) : Int(max)
+    return answer
 }
 
-print(myAtoi("  +  413"))
+print(intToRoman(1994))
