a6d28cc81e4fe565e5cf06844e4640c24d1741e4
Fomagran <fomagran6@naver.com>
2022-06-11 17:00:09 -0500

    [LeetCode] Integer to Roman clean version

--- main.swift
+++ main.swift
@@ -1,38 +1,19 @@
-/*
- 1. num을 string으로 바꿔 각 자리의 숫자를 확인한다.
- 2. 만약 1~5,4,6~8,9인지 확인해 각 조건에 맞게 Symbol을 넣어준다.
- 
- */
-
-func intToRoman(_ num: Int) -> String {
-    let roman = [1:["I","V"],10:["X","L"],100:["C","D"],1000:["M"]]
-    let numToString:[String] = String(num).map{String($0)}
-    let val:[Int] = [1,10,100,1000]
-    var answer:String = ""
+private let values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    private let symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
     
-    for (i,n) in numToString.enumerated() {
-        let intN:Int = Int(n) ?? 0
-        let key = val[numToString.count - i - 1]
-        if 1...3 ~= intN {
-            for _ in 0..<intN {
-                answer += roman[key]![0]
+    func intToRoman(_ num: Int) -> String {
+        
+        var int = num
+        var sym = ""
+        
+        while int > 0 {
+            for (i, d) in values.enumerated() where int - d >= 0 {
+                int -= d
+                sym += symbols[i]
+                break
             }
-        }else if intN == 4 {
-            answer += roman[key]![0]
-            answer += roman[key]![1]
-        }else if intN == 5 {
-            answer += roman[key]![1]
-        }else if 6...8 ~= intN {
-            answer += roman[key]![1]
-            for _ in 5..<intN {
-                answer += roman[key]![0]
-            }
-        }else if intN == 9 {
-            answer += roman[key]![0]
-            answer += roman[key*10]![0]
         }
+        return sym
     }
-    return answer
-}
 
 print(intToRoman(1994))
