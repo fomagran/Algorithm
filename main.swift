3675d418a589c35858692b26587fbb17cb4dcbcd
Fomagran <fomagran6@naver.com>
2022-06-12 22:25:11 -0500

    [LeetCode] 4Sum Solution

--- main.swift
+++ main.swift
@@ -1,44 +1,54 @@
 /*
- 1. sort the nums
- 2. use 2 pointer (start left index is a 0 index, right index is nums.count -1
- 3. if target - (leftindex num + right index num) < 0
- that means sum should be less, so make the middle point and iterate left + 1..<num.count-1 find closest sum, if the sum has far from the closest then break
- 4. if target - (leftindex num + right index num) > 0
- that means sum should be large, so make the middle point and iterate nums.count ... 1 find closest sum, if the sum has far from the closest then break
- 5. if the middle point itreate is done, if 3 case right index - 1, if 4 case left index + 1
+ Idea: first a,b,c,d
+ a index = 0
+ b index = 1(0+1)
+ c index = nums.count-2
+ d index = nums.count -1
  
- if the gap between target and closest value is 0, we gonna return target
+ 4 sum is a + b + c + d
+ a,c,d is fix
+ 2) b is goint to increase + 1
+ like this 0 1 nums.count -2 nums.count -1
+ and 0 2 nums.count-2 nums.count - 1....
+ 
+ if b is c -1 , then c is goint to decrease -1
+ and b is going to 2)
+ and if c == a+1
+ then a+1
+ 
+ whie this is proceeding, if a+b+c+d is the target
+ append answer a,b,c,d
+ 
+ Time complexity O(n^3)
+ Space complexity O(1)
  
  */
 
-
-func threeSumClosest(_ nums: [Int], _ target: Int) -> Int {
+func fourSum(_ nums: [Int], _ target: Int) -> [[Int]] {
+    if nums.count < 4 { return [] }
+    if nums.count == 4 { return nums.reduce(0,+) == target ? [nums] : [] }
     let sorted = nums.sorted(by: <)
-    var closest:Int = target < 0 ? Int.min : Int.max
+    var result:[[Int]] = []
     
-    for i in 0..<nums.count {
-        let leftValue:Int = sorted[i]
-        var right:Int = nums.count - 1
-        var middle:Int = i+1
-        while middle < right {
-            let rightValue = sorted[right]
-            let middleValue = sorted[middle]
-            let sum = leftValue+rightValue+middleValue
-            
-            if abs(target-closest) > abs(target-sum) {
-                closest = sum
-            }
-            
-            if sum <= target {
-                middle += 1
-            } else {
-                right -= 1
+    for a in 0..<nums.count {
+        for b in a+1..<nums.count {
+            for c in b+1..<nums.count {
+                for d in c+1..<nums.count {
+                    let sum = sorted[a] + sorted[b] + sorted[c] + sorted[d]
+                    if sum == target {
+                        result.append([sorted[a],sorted[b],sorted[c],sorted[d]])
+                        break
+                    }
+                    
+                    if sum > target {
+                        break
+                    }
+                }
             }
         }
     }
     
-    return closest
+    return result
 }
 
-print(threeSumClosest([-55,-24,-18,-11,-7,-3,4,5,6,9,11,23,33]
-                      ,0))
+print(fourSum([1,0,-1,0,-2,2], 0))
