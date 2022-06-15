35c611d0e887636ced55b23a6bcc3ae9b9e91f6d
Fomagran <fomagran6@naver.com>
2022-06-14 22:08:04 -0500

    [LeetCode] Target Sum 2차

--- main.swift
+++ main.swift
@@ -1,22 +1,33 @@
 func findTargetSumWays(_ nums: [Int], _ target: Int) -> Int {
     let sum:Int = nums.reduce(0,+)
-    var count:Int = 0
-    if sum == target {
-        count += 1
-    }
-    var sums:[Int] = []
+    var dic:[Int:Int] = [:]
     
     for i in 0..<nums.count {
-        sums.append(nums[i])
-        for (j,_) in sums.enumerated() {
-            if sum - sums[j]*2 == target {
-                count += 1
+        for (key,value) in dic {
+            let new = key + nums[i]*2
+            if dic[new] == nil {
+                dic[new] = value
+            }else {
+                dic[new]! += value
             }
-            sums[j] += nums[i]
         }
+        
+        if dic[sum-nums[i]*2] == nil {
+            dic[sum-nums[i]*2] = 1
+        }else {
+            dic[sum-nums[i]*2]! += 1
+        }
+        print(dic)
     }
     
-    return count
+    if dic[sum] == nil {
+        dic[sum] = 1
+    }else {
+        dic[sum]! += 1
+    }
+    
+    return dic[target] ?? 0
 }
-
-print(findTargetSumWays([1,1,1,1,1], 3))
+print(findTargetSumWays([1,2,1],0))
+//print(findTargetSumWays([0,0,0,0,1], 1))
+//print(findTargetSumWays([1], 1))
