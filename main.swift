556f5358becba38b18193bb47cf1b8fbcebf9577
Fomagran <fomagran6@naver.com>
2022-06-18 18:20:57 -0500

    [LeetCode] Subarray sum Solution

--- main.swift
+++ main.swift
@@ -1,23 +1,18 @@
-func findTargetSumWays(_ nums: [Int], _ target: Int) -> Int {
-    let sum:Int = nums.reduce(0,+)
+func subarraySum(_ nums: [Int], _ k: Int) -> Int {
     var dic:[Int:Int] = [0:1]
+    var latestSum:Int = 0
+    var answer:Int = 0
     
-    for n in nums{
-        for (key,value) in dic{
-            dic[key+n*2] = (dic[key+n*2] ?? 0) + value
-        }
+    for n in nums {
+        latestSum += n
+        answer += (dic[latestSum - k] ?? 0)
+        dic[latestSum] = (dic[latestSum] ?? 0) + 1
     }
     
-    return dic[sum-abs(target)] ?? 0
+    return answer
 }
 
-
-
-
-
-print(findTargetSumWays([1,2,3,1,2,3],6))
-//print(findTargetSumWays([1,1,1,1,1],3))
-//print(findTargetSumWays([0,0,0,0,1], 1))
-//print(findTargetSumWays([1000], -1000))
-
+print(subarraySum([-1,1,1,-1,1], 1))
+print(subarraySum([1,2,3], 3))
+print(subarraySum([100,1,2,3,4], 4))
 
