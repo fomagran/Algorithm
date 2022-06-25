28363820c068b0d65bfda84ab154e7b410aa5a65
Fomagran <fomagran6@gmail.com>
2022-06-24 20:43:53 -0500

    [LeetCode] Start N-Queens

--- main.swift
+++ main.swift
@@ -1,18 +1,5 @@
-func subarraySum(_ nums: [Int], _ k: Int) -> Int {
-    var dic:[Int:Int] = [0:1]
-    var latestSum:Int = 0
-    var answer:Int = 0
-    
-    for n in nums {
-        latestSum += n
-        answer += (dic[latestSum - k] ?? 0)
-        dic[latestSum] = (dic[latestSum] ?? 0) + 1
-    }
-    
-    return answer
-}
+func solveNQueens(_ n: Int) -> [[String]] {
 
-print(subarraySum([-1,1,1,-1,1], 1))
-print(subarraySum([1,2,3], 3))
-print(subarraySum([100,1,2,3,4], 4))
+    
+}
 
