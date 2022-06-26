f8461ecd465bddaa9add1ef6d7d5403e72119b77
Fomagran <fomagran6@gmail.com>
2022-06-26 00:41:04 -0500

    [LeetCode] Edit Distance Solution

--- main.swift
+++ main.swift
@@ -1,65 +1,36 @@
-
-
-
-func solveNQueens(_ n: Int) -> [[String]] {
+func minDistance(_ word1: String, _ word2: String) -> Int {
+    if word1.isEmpty || word2.isEmpty {
+        return word1.isEmpty ? word2.count : word1.count
+    }
     
-    var answer:[[String]] = []
-    let nDots:[String] = Array(repeating: ".", count: n)
-    let nChecked:[Bool] = Array(repeating:false, count: n)
+    let word1Map:[String] = [""] + word1.map{String($0)}
+    let word2Map:[String] = [""] + word2.map{String($0)}
     
-    func dfs(_ depth:Int,_ current:[[String]],_ enable:[[Bool]]) {
-        if depth == n {
-            answer.append(current.map{$0.joined()})
-            return
-        }
-        
-        for x in 0..<n {
-            if enable[depth][x] {
-                var newEnable = enable
-                for y in 0..<n {
-                    newEnable[y][x] = false
-                    if y == depth {
-                        newEnable[y] = nChecked
-                    }
-                    if x-abs(depth-y) >= 0 {
-                        newEnable[y][x-abs(depth-y)] = false
-                    }
-                    
-                    if x+(abs(depth-y)) < n {
-                        newEnable[y][x+(abs(depth-y))] = false
-                    }
-                }
-                var newDots = nDots
-                newDots[x] = "Q"
-                dfs(depth+1,current + [newDots], newEnable)
+    var dp:[[Int]] = Array(repeating:Array(repeating:0, count: word1Map.count),count:word2Map.count)
+    
+    dp[0] = (0..<word1Map.count).map{$0}
+    
+    for i in 0..<word2Map.count {
+        dp[i][0] = i
+    }
+    
+    for y in 1..<word2Map.count {
+        for x in 1..<word1Map.count {
+            if word2Map[y] == word1Map[x] {
+                dp[y][x] = dp[y-1][x-1]
+            }else {
+                let min:Int = min(min(dp[y-1][x-1],dp[y-1][x]),dp[y][x-1])
+                dp[y][x] = min + 1
             }
         }
     }
     
-    let initEnable:[[Bool]] = Array(repeating:Array(repeating:true, count: n), count: n)
-    dfs(0, [], initEnable)
+    for d in dp {
+        print(d)
+    }
     
-    return answer
+    return dp.last?.last ?? 0
 }
 
-print((solveNQueens(4)))
-
-            
-/*
- .Q..
- ...Q
- Q...
- ..Q.
- 
- ..Q.
- Q...
- ...Q
- .Q..
- 
- ...Q
- .Q..
- Q...
- ..Q.
- 
- */
-
+print(minDistance("a"
+                  ,"b"))
