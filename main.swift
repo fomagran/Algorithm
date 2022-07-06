22dda41299774d589faa39956d964905bbd595b3
Fomagran <fomagran6@gmail.com>
2022-07-06 13:20:42 -0500

    [LeetCode] Binary Tree Inorder Traversal Solution

--- main.swift
+++ main.swift
@@ -3,8 +3,19 @@ public class TreeNode {
     public var val: Int
     public var left: TreeNode?
     public var right: TreeNode?
-    public init() { self.val = 0; self.left = nil; self.right = nil; }
-    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    
+    public init() {
+        self.val = 0
+        self.left = nil
+        self.right = nil
+    }
+    
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+    
     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
         self.val = val
         self.left = left
@@ -13,15 +24,21 @@ public class TreeNode {
 }
 
 
+var answer:[Int] = []
+
 func inorderTraversal(_ root: TreeNode?) -> [Int] {
-    
+    searchByInOrder(root)
+    return answer
 }
 
-let test:[Int?] = [1,2,nil,3,4,nil,5,6]
-let r = TreeNode(0)
-
-var current = 0
-
-while current < test.count {
-    
+func searchByInOrder(_ node:TreeNode?) {
+    if node != nil {
+        if node?.left != nil {
+            searchByInOrder(node?.left)
+        }
+        answer.append(node!.val)
+        if node?.right != nil {
+            searchByInOrder(node?.right)
+        }
+    }
 }
