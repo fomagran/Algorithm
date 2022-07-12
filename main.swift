c54095cc58ee49aa936ef17734d751b5f23821a0
Fomagran <fomagran6@gmail.com>
2022-07-11 21:37:11 -0500

    [LeetCode] K Closest points to origin 1st attempt

--- main.swift
+++ main.swift
@@ -1,44 +1,60 @@
+import Darwin
 
-public class TreeNode {
-    public var val: Int
-    public var left: TreeNode?
-    public var right: TreeNode?
-    
-    public init() {
-        self.val = 0
-        self.left = nil
-        self.right = nil
-    }
-    
-    public init(_ val: Int) {
-        self.val = val
-        self.left = nil
-        self.right = nil
-    }
-    
-    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
-        self.val = val
-        self.left = left
-        self.right = right
-    }
-}
-
-
-var answer:[Int] = []
-
-func inorderTraversal(_ root: TreeNode?) -> [Int] {
-    searchByInOrder(root)
-    return answer
-}
-
-func searchByInOrder(_ node:TreeNode?) {
-    if node != nil {
-        if node?.left != nil {
-            searchByInOrder(node?.left)
-        }
-        answer.append(node!.val)
-        if node?.right != nil {
-            searchByInOrder(node?.right)
+func kClosest(_ points: [[Int]], _ k: Int) -> [[Int]] {
+    var closest:[([Int],Double)] = []
+    for point in points {
+        let d = getDistance((0,0),(point[0],point[1]))
+        
+        if closest.count < k {
+            insert((point,getDistance((0,0),(point[0],point[1]))))
+        }else {
+            if closest.last!.1 > d {
+                closest.removeLast()
+                insert((point,getDistance((0,0),(point[0],point[1]))))
+            }
         }
     }
+    
+    func insert(_ d:([Int],Double)) {
+        if closest.isEmpty {
+            closest.append(d)
+            return
+        }
+        
+        var left:Int = 0
+        var right:Int = closest.count - 1
+        
+        while left < right {
+            let mid:Int = (left + right)/2
+            print(closest)
+            if closest[left].1...closest[mid].1 ~= d.1 {
+                right = mid
+            }else {
+                left = mid + 1
+            }
+        }
+        
+        print(left,right,d.1)
+        
+        if closest[left].1 >= d.1 {
+            closest.insert(d, at: left)
+        }else {
+            if left == closest.count - 1{
+                closest.append(d)
+            }else {
+                closest.insert(d, at: left+1)
+            }
+        }
+        
+    }
+    
+    return closest.map{$0.0}
 }
+
+func getDistance(_ p1:(Int,Int),_ p2:(Int,Int)) -> Double {
+    let xSquared:Int = (p2.0 - p1.0) * (p2.0 - p1.0)
+    let ySquared:Int = (p2.1 - p1.1) * (p2.1 - p1.1)
+    return sqrt(Double(xSquared + ySquared))
+}
+
+print(kClosest([[9,0],[7,10],[-4,-2],[3,-9],[9,1],[-5,-1]], 5))
