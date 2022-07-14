e511805954c93376fee45e5359c0ff70a93eb8ae
Fomagran <fomagran6@gmail.com>
2022-07-13 20:37:18 -0500

    [LeetCode] K Closest Points to Origin Solution

--- main.swift
+++ main.swift
@@ -1,60 +1,53 @@
 import Darwin
 
+struct Point {
+    let location: [Int]
+    var distance: Int {
+        let xSquared:Int = location[0] * location[0]
+        let ySquared:Int = location[1] * location[1]
+        return xSquared + ySquared
+    }
+}
+
 func kClosest(_ points: [[Int]], _ k: Int) -> [[Int]] {
-    var closest:[([Int],Double)] = []
+    var closest:[Point] = []
     for point in points {
-        let d = getDistance((0,0),(point[0],point[1]))
-        
-        if closest.count < k {
-            insert((point,getDistance((0,0),(point[0],point[1]))))
-        }else {
-            if closest.last!.1 > d {
-                closest.removeLast()
-                insert((point,getDistance((0,0),(point[0],point[1]))))
-            }
-        }
+        insert(Point(location: point))
     }
     
-    func insert(_ d:([Int],Double)) {
+    func insert(_ point:Point) {
+        
         if closest.isEmpty {
-            closest.append(d)
+            closest.append(point)
             return
         }
         
-        var left:Int = 0
-        var right:Int = closest.count - 1
+        var left: Int = 0
+        var right: Int = closest.count - 1
         
-        while left < right {
-            let mid:Int = (left + right)/2
-            print(closest)
-            if closest[left].1...closest[mid].1 ~= d.1 {
-                right = mid
-            }else {
+        while left <= right {
+            let mid: Int = (left + right)/2
+            let midDistance: Int = closest[mid].distance
+            
+            if point.distance == midDistance {
+                left = mid
+                break
+            } else if point.distance > midDistance {
+                right = mid - 1
+            } else {
                 left = mid + 1
             }
         }
         
-        print(left,right,d.1)
+        closest.insert(point, at: left)
         
-        if closest[left].1 >= d.1 {
-            closest.insert(d, at: left)
-        }else {
-            if left == closest.count - 1{
-                closest.append(d)
-            }else {
-                closest.insert(d, at: left+1)
-            }
+        if closest.count > k {
+            closest.removeFirst()
         }
-        
     }
     
-    return closest.map{$0.0}
+    return closest.map{Array($0.location)}
 }
 
-func getDistance(_ p1:(Int,Int),_ p2:(Int,Int)) -> Double {
-    let xSquared:Int = (p2.0 - p1.0) * (p2.0 - p1.0)
-    let ySquared:Int = (p2.1 - p1.1) * (p2.1 - p1.1)
-    return sqrt(Double(xSquared + ySquared))
-}
-
-print(kClosest([[9,0],[7,10],[-4,-2],[3,-9],[9,1],[-5,-1]], 5))
+print(kClosest([[3,3],[5,-1],[-2,4]]
+               ,2))
