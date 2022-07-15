5713633c9ae31ac9aa09dbad990e2469b3aeaf0c
Fomagran <fomagran6@gmail.com>
2022-07-14 22:55:48 -0500

    [LeetCode] Longest Consecutive Sequence start

--- main.swift
+++ main.swift
@@ -1,28 +1,3 @@
-func findKthLargest(_ nums: [Int], _ k: Int) -> Int {
-    var quickArray:[Int] = nums
-    var K = nums.count - k
+func longestConsecutive(_ nums: [Int]) -> Int {
     
-    func quickSelect(_ l:Int,_ r:Int) -> Int {
-        let pivot:Int = quickArray[r]
-        var p:Int = l
-        
-        for i in l..<r {
-            if quickArray[i] <= pivot {
-                quickArray.swapAt(p,i)
-                p += 1
-            }
-        }
-        
-        quickArray.swapAt(p,r)
-
-        if p == K {
-            return quickArray[p]
-        }else if p > K {
-            return quickSelect(l,p-1)
-        }else {
-            return quickSelect(p+1,r)
-        }
-    }
-
-    return quickSelect(0,nums.count-1)
 }
