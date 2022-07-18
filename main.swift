ea55a6733bf1f21324a1f33e05b8523d4e85ece8
Fomagran <fomagran6@gmail.com>
2022-07-17 19:24:03 -0500

    [LeetCode] Unique Binary Search Trees Solution

--- main.swift
+++ main.swift
@@ -1,67 +1,32 @@
-struct Sequence {
-    var head: Int
-    var tail: Int
-    var length: Int {
-        return abs(tail - head + 1)
-    }
-}
-
-func longestConsecutive(_ nums: [Int]) -> Int {
-    var sequences: [Int:Sequence] = [:]
-    var maxLength: Int = 0
+func numTrees(_ n: Int) -> Int {
     
-    for n in nums {
-        
-        if sequences[n] != nil {
-            continue
-        }
-        
-        if sequences[n-1] == nil && sequences[n+1] == nil {
-            sequences[n] = Sequence(head: n, tail: n)
-        }
-        
-        if sequences[n-1] != nil && sequences[n+1] != nil {
-            sequences[n] = Sequence(head: sequences[n-1]!.head, tail: sequences[n+1]!.tail)
-            let head = sequences[n-1]!.head
-            let tail = sequences[n+1]!.tail
-            sequences[tail]!.head = head
-            sequences[head]!.tail = tail
-        }
-        
-        if sequences[n-1] != nil && sequences[n+1] == nil {
-            sequences[n] = Sequence(head: sequences[n-1]!.head, tail: n)
-            let head = sequences[n-1]!.head
-            sequences[head]!.tail = n
-        }
-        
-        if sequences[n+1] != nil && sequences[n-1] == nil {
-            sequences[n] = Sequence(head: n, tail: sequences[n+1]!.tail)
-            let tail = sequences[n+1]!.tail
-            sequences[tail]!.head = n
-        }
-
-        maxLength = max(maxLength,sequences[n]!.length)
+    if n == 1 {
+        return 1
     }
     
-    return maxLength
+    var dp: [Int] = [1,1]
+    
+    for i in 2...n {
+        var total: Int = 0
+        
+        for j in 0..<i {
+            let left: Int = dp[j]
+            let right: Int = dp[i-j-1]
+            total += left * right
+        }
+        
+        dp.append(total)
+    }
+    
+    return dp[n]
 }
 
-print(longestConsecutive([-4,-1,4,-5,1,-6,9,-6,0,2,2,7,0,9,-3,8,9,-2,-6,5,0,3,4,-2]))
+print(numTrees(3))
 
-//-4
-//-4 -1
-//-4 -1 4
-//-5-4 -1 4
-//-5-4 -1 1 4
-//-6-5-4 -1 1 4
-//-6-5-4 -1 1 4 9
-//-6-5-4 -1 1 4 9
-//-6-5-4 -101 4 9
-//-6-5-4 -1012 4 9
-//-6-5-4 -1012 4 7 9
-//-6-5-4 -1012 4 7 9
-//-6-5-4-3 -1012 4 7 9
-//-6-5-4-3 -1012 4 789
-//-6-5-4-3-2-1012 4 7 9
-//-6-5-4-3-2-1012 45 7 9
-//-6-5-4-3-2-1012345 7 9
+
+//1 2 5 12 25 52
+
+
+
+
+
