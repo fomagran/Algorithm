f3ced1c946cb9393df73c4fd9c134126151726e1
Fomagran <fomagran6@gmail.com>
2022-07-19 08:19:08 -0500

    [LeetCode] Odd Even Linked List Solution

--- main.swift
+++ main.swift
@@ -1,32 +1,45 @@
-func numTrees(_ n: Int) -> Int {
-    
-    if n == 1 {
-        return 1
-    }
-    
-    var dp: [Int] = [1,1]
-    
-    for i in 2...n {
-        var total: Int = 0
-        
-        for j in 0..<i {
-            let left: Int = dp[j]
-            let right: Int = dp[i-j-1]
-            total += left * right
-        }
-        
-        dp.append(total)
-    }
-    
-    return dp[n]
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
 }
 
-print(numTrees(3))
 
+func oddEvenList(_ head: ListNode?) -> ListNode? {
+        var index: Int = 0
+        var current: ListNode? = head
+        var oddTail: ListNode? = nil
+        var oddRoot: ListNode? = nil
+        var evenTail: ListNode? = nil
+        var evenRoot: ListNode? = nil
 
-//1 2 5 12 25 52
-
-
-
-
+        
+        while current != nil {
+            if index % 2 == 0 {
+                if oddTail == nil {
+                    oddTail = ListNode(current!.val)
+                    oddRoot = oddTail
+                }else {
+                    oddTail?.next = ListNode(current!.val)
+                    oddTail = oddTail?.next
+                }
+            }else {
+                 if evenTail == nil {
+                    evenTail = ListNode(current!.val)
+                    evenRoot = evenTail
+                }else {
+                    evenTail?.next = ListNode(current!.val)
+                    evenTail = evenTail?.next
+                }
+            }
+            index += 1
+            current = current?.next
+        }
+        
+        oddTail?.next = evenRoot
+        
+        return oddRoot
+    }
 
