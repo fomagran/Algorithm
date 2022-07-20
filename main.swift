51034553a09aa9c367cff693cff86e681bec7f31
Fomagran <fomagran6@gmail.com>
2022-07-19 20:52:43 -0500

    [LeetCode] Find Largest Value in Each Tree Row

--- main.swift
+++ main.swift
@@ -1,45 +1,43 @@
-public class ListNode {
-    public var val: Int
-    public var next: ListNode?
-    public init() { self.val = 0; self.next = nil; }
-    public init(_ val: Int) { self.val = val; self.next = nil; }
-    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+
+public class TreeNode {
+     public var val: Int
+     public var left: TreeNode?
+     public var right: TreeNode?
+     public init() { self.val = 0; self.left = nil; self.right = nil; }
+     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+         self.val = val
+         self.left = left
+         self.right = right
+     }
 }
 
-
-func oddEvenList(_ head: ListNode?) -> ListNode? {
-        var index: Int = 0
-        var current: ListNode? = head
-        var oddTail: ListNode? = nil
-        var oddRoot: ListNode? = nil
-        var evenTail: ListNode? = nil
-        var evenRoot: ListNode? = nil
-
-        
-        while current != nil {
-            if index % 2 == 0 {
-                if oddTail == nil {
-                    oddTail = ListNode(current!.val)
-                    oddRoot = oddTail
-                }else {
-                    oddTail?.next = ListNode(current!.val)
-                    oddTail = oddTail?.next
-                }
-            }else {
-                 if evenTail == nil {
-                    evenTail = ListNode(current!.val)
-                    evenRoot = evenTail
-                }else {
-                    evenTail?.next = ListNode(current!.val)
-                    evenTail = evenTail?.next
-                }
+func largestValues(_ root: TreeNode?) -> [Int] {
+    var maxNodes = [Int]()
+    
+        func dfs(_ node: TreeNode?,_ level:Int) {
+            if node == nil {
+                return
             }
-            index += 1
-            current = current?.next
-        }
-        
-        oddTail?.next = evenRoot
-        
-        return oddRoot
-    }
+            
+            if maxNodes.count == level {
+                maxNodes.append(node!.val)
+            } else {
+                maxNodes[level] = max(maxNodes[level],node?.val ?? Int.min)
+            }
 
+            dfs(node?.left,level+1)
+            dfs(node?.right,level+1)
+        }
+    
+    dfs(root,0)
+    
+    return maxNodes
+}
+
+let root:TreeNode? = TreeNode(1)
+root?.left = TreeNode(2)
+root?.right = TreeNode(3)
+
+
+print(largestValues(root))
