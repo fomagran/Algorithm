7714d0c5d7033bec228466cfb13a12475c70e78d
Fomagran <fomagran6@gmail.com>
2022-07-21 20:33:00 -0500

    [LeetCode] lengthOfLongestSubstriing optimize solution

--- main.swift
+++ main.swift
@@ -1,43 +1,21 @@
-
-public class TreeNode {
-     public var val: Int
-     public var left: TreeNode?
-     public var right: TreeNode?
-     public init() { self.val = 0; self.left = nil; self.right = nil; }
-     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
-     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
-         self.val = val
-         self.left = left
-         self.right = right
-     }
-}
-
-func largestValues(_ root: TreeNode?) -> [Int] {
-    var maxNodes = [Int]()
+func lengthOfLongestSubstring(_ s: String) -> Int {
+    var maxLength: Int = 0
+    var dic:[Character:[Int]] = [:]
+    var start: Int = 0
+    var end: Int = -1
     
-        func dfs(_ node: TreeNode?,_ level:Int) {
-            if node == nil {
-                return
-            }
-            
-            if maxNodes.count == level {
-                maxNodes.append(node!.val)
-            } else {
-                maxNodes[level] = max(maxNodes[level],node?.val ?? Int.min)
-            }
-
-            dfs(node?.left,level+1)
-            dfs(node?.right,level+1)
+    for (i,c) in s.enumerated() {
+        if dic[c] == nil {
+            dic[c] = [i]
+        } else {
+            let first = dic[c]!.removeFirst()
+            start = first >= start ? first+1 : start
+            dic[c]!.append(i)
         }
-    
-    dfs(root,0)
-    
-    return maxNodes
+        end += 1
+        maxLength = max(maxLength,end-start+1)
+    }
+    return maxLength
 }
 
-let root:TreeNode? = TreeNode(1)
-root?.left = TreeNode(2)
-root?.right = TreeNode(3)
-
-
-print(largestValues(root))
+print(lengthOfLongestSubstring("abcabcbb"))
