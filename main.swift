abedc3d4f7ffa16803103e3cdbd6d4000a8456e1
Fomagran <fomagran6@gmail.com>
2022-07-27 22:07:02 -0500

    [LeetCode]  Implement Trie(Prefix Tree) Solution!

--- main.swift
+++ main.swift
@@ -1,68 +1,62 @@
-public class TreeNode {
-    public var val: Int
-    public var left: TreeNode?
-    public var right: TreeNode?
-    public init() { self.val = 0; self.left = nil; self.right = nil; }
-    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
-    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
-        self.val = val
-        self.left = left
-        self.right = right
+class Node {
+    var value:Character
+
+    var children:[Character:Node] = [:]
+    
+    init(value:Character) {
+        self.value = value
+    }
+    
+    func append(_ value:Character) {
+        self.children[value] = Node(value: value)
     }
 }
 
-func isValidBST(_ root: TreeNode?) -> Bool {
+class Trie {
     
-    var isValid: Bool = true
+    var root: Node = Node(value: " ")
     
-    func check(node: TreeNode?,minValue: Int, maxValue:Int) {
-        if node == nil || !isValid {
-            return
-        }
-        
-        if (node!.left?.val ?? Int.min) >= node!.val || (node!.right?.val ?? Int.max) <= node!.val {
-            isValid = false
-            return
-        }
-        
-        if minValue >= node!.val || node!.val >= maxValue  {
-            isValid = false
-            return
-        }
-        
-        check(node: node?.left, minValue: minValue, maxValue: min(maxValue,node!.val))
-        check(node: node?.right, minValue: max(minValue,node!.val), maxValue: maxValue)
-    
+    init() {
     }
     
-    check(node: root, minValue: Int.min, maxValue: Int.max)
-
-    return isValid
+    func insert(_ word: String) {
+        let map = word.map{$0}
+        var current = root
+        
+        map.forEach {
+            if !current.children.keys.contains($0) {
+                current.append($0)
+            }
+            current = current.children[$0]!
+        }
+        current.append("?")
+    }
+    
+    func search(_ word: String) -> Bool {
+        let map = word.map{$0}
+        var current = root
+        for c in map {
+            if !current.children.keys.contains(c) {
+                return false
+            }
+            current = current.children[c]!
+        }
+        return current.children.keys.contains("?")
+    }
+    
+    func startsWith(_ prefix: String) -> Bool {
+        let map = prefix.map{$0}
+        var current = root
+        for c in map {
+            if !current.children.keys.contains(c) {
+                return false
+            }
+            current = current.children[c]!
+        }
+        return true
+    }
 }
 
-let root:TreeNode = TreeNode(120)
-root.left = TreeNode(70)
-root.right = TreeNode(140)
-root.left?.left = TreeNode(50)
-root.left?.right = TreeNode(100)
-root.right?.left = TreeNode(130)
-root.right?.right = TreeNode(160)
-root.left?.left?.left = TreeNode(20)
-root.left?.left?.right = TreeNode(55)
-root.left?.left?.left?.left = TreeNode(75)
-root.left?.left?.left?.right = TreeNode(110)
-root.left?.left?.right?.left = TreeNode(119)
-root.left?.left?.right?.right = TreeNode(135)
 
-print(isValidBST(root))
-
-
-
-/*
-                    120
-            70              140
-        50    100      130     160
-       20 55 75 110  119 135  150 200
- */
 
 
