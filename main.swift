5657a428771a1fb5f495b4a79f8e4510bd7b8bdc
Fomagran <fomagran6@gmail.com>
2022-08-07 16:28:38 -0500

    [LeetCode] House Robber Solution

--- main.swift
+++ main.swift
@@ -1,35 +1,16 @@
-func largestRectangleArea(_ heights: [Int]) -> Int {
-    var maxArea: Int = 0
-    var stack: [Int] = []
-    let heights = heights + [0]
-    
-    for (i,height) in heights.enumerated() {
-        let current: Int = height
-        
-        while !stack.isEmpty && current <= heights[stack.last!] {
-            let last = stack.removeLast()
-            let lastHeight = heights[last]
-            let area = lastHeight * (i - (stack.last ?? -1) - 1)
-            maxArea = max(maxArea,area)
-        }
-        stack.append(i)
+func rob(_ nums: [Int]) -> Int {
+    if nums.count <= 2 {
+        return nums.max()!
     }
-    
-    return maxArea
+
+    var dp: [Int] = [nums[0],max(nums[0],nums[1])]
+
+    for i in 2..<nums.count {
+        let current: Int = nums[i]
+        dp.append(max(dp[i-1],dp[i-2] + current))
+    }
+
+    return dp.last!
 }
 
-print(largestRectangleArea([3,4,5,6,5,5,7,4,1,2,3]))
-print(largestRectangleArea([2,1,5,6,2,3]))
-print(largestRectangleArea([2,4]))
-
-
-
-/*
-       7
-    6  7
-   56557
-  4565574
- 34565574  3
- 34565574 23
- 34565574123
- */
+print(rob([2,1,1,2]))
