ec05904ab2299737506b9560d62d20c1f7dafe60
Fomagran <fomagran6@gmail.com>
2022-08-06 20:19:15 -0500

    [LeetCode] Largest Rectangle in Histogram TLE

--- main.swift
+++ main.swift
@@ -2,23 +2,35 @@ func largestRectangleArea(_ heights: [Int]) -> Int {
     var maxArea: Int = 0
     for i in 0..<heights.count {
         let current: Int = heights[i]
-        var area: Int = 0
-        for j in i..<heights.count {
-            let compare: Int = heights[j]
-            if compare < current {
+        var left = i-1
+        var right = i+1
+        
+        while left >= 0 {
+            if heights[left] < current {
                 break
-            }else {
-                area += current
             }
-            maxArea = max(maxArea,area)
+            left -= 1
         }
+        
+        while right <= heights.count - 1 {
+            if heights[right] < current {
+                break
+            }
+            right += 1
+        }
+        
+        left += 1
+        right -= 1
+        
+        maxArea = max(maxArea,current*(right-left+1))
     }
     
     return maxArea
 }
 
+print(largestRectangleArea([3,4,5,6,5,5,7,4,1,2,3]))
 print(largestRectangleArea([2,1,5,6,2,3]))
-
+print(largestRectangleArea([2,4]))
 
 
 
