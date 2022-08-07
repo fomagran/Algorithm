9b7a4871db0b8920a99d2b1e2a963349104b57a2
Fomagran <fomagran6@gmail.com>
2022-08-07 17:05:48 -0500

    [LeetCode] House Robbing Refactoring

--- main.swift
+++ main.swift
@@ -1,16 +1,11 @@
 func rob(_ nums: [Int]) -> Int {
-    if nums.count <= 2 {
-        return nums.max()!
-    }
+    var dp: [Int] = [0,0]
 
-    var dp: [Int] = [nums[0],max(nums[0],nums[1])]
-
-    for i in 2..<nums.count {
-        let current: Int = nums[i]
-        dp.append(max(dp[i-1],dp[i-2] + current))
+    for (i,num) in nums.enumerated() {
+        dp.append(max(dp[i+1],dp[i] + num))
     }
 
     return dp.last!
 }
 
-print(rob([2,1,1,2]))
+print(rob([2,1,1,2,1,1,2]))
