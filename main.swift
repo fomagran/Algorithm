6ae40f1d37a36f698b15c729af2b01231157c6e3
Fomagran <fomagran6@gmail.com>
2022-08-07 21:51:01 -0500

    [LeetCode] House Robber 2 Solution!

--- main.swift
+++ main.swift
@@ -1,11 +1,27 @@
 func rob(_ nums: [Int]) -> Int {
-    var dp: [Int] = [0,0]
-
-    for (i,num) in nums.enumerated() {
-        dp.append(max(dp[i+1],dp[i] + num))
+    if nums.count == 1 {
+        return nums[0]
     }
+    
+    var nums = nums
+    let last = nums.removeLast()
+    
+    func getMaxMoney() -> Int {
+        var dp: [Int] = [0,0]
+        for (i,num) in nums.enumerated() {
+            dp.append(max(dp[i+1],dp[i] + num))
+        }
+        return dp.last!
+    }
+    
+    let maxMoney1: Int = getMaxMoney()
+    nums.removeFirst()
+    nums.append(last)
+    let maxMoney2: Int = getMaxMoney()
 
-    return dp.last!
+    return max(maxMoney1,maxMoney2)
 }
 
-print(rob([2,1,1,2,1,1,2]))
+
+
+print(rob([0,1]))
