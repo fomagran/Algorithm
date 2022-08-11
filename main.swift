dba45436813a7d9bfa285feaace07f9e1f3f6821
Fomagran <fomagran6@gmail.com>
2022-08-10 20:35:26 -0500

    [LeetCode] Top K Frequent Elements using sorted

--- main.swift
+++ main.swift
@@ -1,27 +1,11 @@
-func rob(_ nums: [Int]) -> Int {
-    if nums.count == 1 {
-        return nums[0]
+func topKFrequent(_ nums: [Int], _ k: Int) -> [Int] {
+    var countDic: [Int:Int] = [:]
+    
+    for num in nums {
+        countDic[num,default:0] += 1
     }
     
-    var nums = nums
-    let last = nums.removeLast()
-    
-    func getMaxMoney() -> Int {
-        var dp: [Int] = [0,0]
-        for (i,num) in nums.enumerated() {
-            dp.append(max(dp[i+1],dp[i] + num))
-        }
-        return dp.last!
-    }
-    
-    let maxMoney1: Int = getMaxMoney()
-    nums.removeFirst()
-    nums.append(last)
-    let maxMoney2: Int = getMaxMoney()
-
-    return max(maxMoney1,maxMoney2)
+    return Array(countDic.sorted{$0.value > $1.value}.map{$0.key}[0..<k])
 }
 
-
-
-print(rob([0,1]))
+print(topKFrequent([1,1,1,2,2,3],1))
