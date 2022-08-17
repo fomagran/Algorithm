086b17da34c984b186fe790a8d0ca9f2dfd62022
Fomagran <fomagran6@gmail.com>
2022-08-16 21:39:01 -0500

    [LeetCode] Two Sum 2 O(n) Solution!

--- main.swift
+++ main.swift
@@ -1,13 +1,20 @@
-func isPalindrome(_ s: String) -> Bool {
-    var original:String = ""
-    
-    s.forEach {
-        if $0.isLetter || $0.isNumber {
-            original += $0.lowercased()
+func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
+    func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
+        var left: Int = 0
+        var right: Int = numbers.count - 1
+        
+        while left < right {
+            if numbers[left] + numbers[right] == target {
+                break
+            } else if numbers[left] + numbers[right] > target {
+                right -= 1
+            } else {
+                left += 1
+            }
         }
+        
+        return [left+1,right+1]
     }
     
-    return original == String(original.reversed())
+    return []
 }
-
-print(isPalindrome("A man, a plan, a canal: Panama"))
