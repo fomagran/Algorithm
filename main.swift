a0b23d0b18731889b0da0f5fae84b2d395c5d581
Fomagran <fomagran6@gmail.com>
2022-08-17 07:49:03 -0500

    [LeetCode] Best Time to buy and Sell Stock TLE O(n^2)

--- main.swift
+++ main.swift
@@ -1,20 +1,11 @@
-func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
-    func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
-        var left: Int = 0
-        var right: Int = numbers.count - 1
-        
-        while left < right {
-            if numbers[left] + numbers[right] == target {
-                break
-            } else if numbers[left] + numbers[right] > target {
-                right -= 1
-            } else {
-                left += 1
-            }
+func maxProfit(_ prices: [Int]) -> Int {
+    var maxProfit: Int = 0
+    
+    for i in 0..<prices.count-1 {
+        for j in i+1..<prices.count {
+            maxProfit = max(maxProfit,prices[j] - prices[i])
         }
-        
-        return [left+1,right+1]
     }
     
-    return []
+    return maxProfit
 }
