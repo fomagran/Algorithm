291a3d6d70bd15e5dce4cbdd2f8b7a9999742486
Fomagran <fomagran6@gmail.com>
2022-08-18 20:58:41 -0500

    [LeetCode] Longest Repeating Character Replacement Solution!

--- main.swift
+++ main.swift
@@ -1,11 +1,25 @@
-func maxProfit(_ prices: [Int]) -> Int {
-    var minValue: Int = Int.max
-    var maxProfit: Int = 0
+func characterReplacement(_ s: String, _ k: Int) -> Int {
+    var dic: [Character:Int] = [:]
+    var left: Int = 0
+    var maxLength: Int = 0
+    let sMap: [Character] = s.map{$0}
+    var maxCharCount: Int = 0
     
-    for price in prices {
-        minValue = min(minValue,price)
-        maxProfit = max(maxProfit,price - minValue)
+    for (right,char) in s.enumerated() {
+        dic[char,default: 0] += 1
+        maxCharCount = max(maxCharCount,dic[char]!)
+        
+        while right - left + 1 - maxCharCount > k {
+            dic[sMap[left]]! -= 1
+            left += 1
+        }
+        
+        maxLength = max(maxLength,right - left + 1)
     }
     
-    return maxProfit
+    return maxLength
+    
 }
+
+print(characterReplacement("AABABBA"
+                           ,1))
