d59ad4ecfaf7dc04bea048b3818b8addb8a4ad5e
Fomagran <fomagran6@gmail.com>
2022-08-20 18:49:27 -0500

    [LeetCode] Permutation in String Solution!

--- main.swift
+++ main.swift
@@ -1,25 +1,48 @@
-func characterReplacement(_ s: String, _ k: Int) -> Int {
+func checkInclusion(_ s1: String, _ s2: String) -> Bool {
     var dic: [Character:Int] = [:]
-    var left: Int = 0
-    var maxLength: Int = 0
-    let sMap: [Character] = s.map{$0}
-    var maxCharCount: Int = 0
+    var dicCopy:[Character:Int] = [:]
+    let s2Map:[Character] = s2.map{$0}
     
-    for (right,char) in s.enumerated() {
-        dic[char,default: 0] += 1
-        maxCharCount = max(maxCharCount,dic[char]!)
-        
-        while right - left + 1 - maxCharCount > k {
-            dic[sMap[left]]! -= 1
-            left += 1
-        }
-        
-        maxLength = max(maxLength,right - left + 1)
+    s1.forEach {
+        dic[$0,default:0] += 1
+        dicCopy[$0,default:0] += 1
     }
     
-    return maxLength
+    var left: Int = 0
     
+    for (i,c) in s2.enumerated() {
+        if dic[c] == nil {
+            if dicCopy[c] != nil {
+                while s2Map[left] != c {
+                    let leftChar = s2Map[left]
+                    dic[leftChar,default:0] = min(dicCopy[leftChar,default: 0] + 1,dic[leftChar]!)
+                    left += 1
+                }
+                left += 1
+            } else {
+                dic = dicCopy
+                left = i+1
+            }
+            continue
+        }
+        
+        dic[c]! -= 1
+        
+        if dic[c]! == 0 {
+            dic[c] = nil
+        }
+        
+        if dic.isEmpty {
+            return true
+        }
+    }
+    
+    return false
 }
 
-print(characterReplacement("AABABBA"
-                           ,1))
+
+print(checkInclusion("hello","ooolleoooleh"))
+print(checkInclusion("abcdef", "abcdeabf"))
+print(checkInclusion("abcdef", "abcdoabcdef"))
+print(checkInclusion("abcdef", "abcdegf"))
+print(checkInclusion("abcdef", "abbcde"))
