77fb4710c360107a6be7cb01e27cdc85d22dc0f8
Fomagran <fomagran6@gmail.com>
2022-08-23 21:03:51 -0500

    [Programmers] 2022 KAKAO TECH INTERNSHIP Solution!

--- main.swift
+++ main.swift
@@ -1,24 +1,64 @@
-func solution(_ survey:[String], _ choices:[Int]) -> String {
-    var charDic: [Character:Int] = ["R":0,"T":0,"C":0,"F":0,"J":0,"M":0,"A":0,"N":0]
-    
-    for i in 0..<survey.count {
-        let chars: [Character] = Array(survey[i])
+import Foundation
 
-        if choices[i] < 4 {
-            charDic[chars[0]]! += 4 - choices[i]
-        } else {
-            charDic[chars[1]]! += choices[i] - 4
+func solution(_ queue1:[Int], _ queue2:[Int]) -> Int {
+    var cumulativeSum: [Int] = [0]
+    let q1Length: Int = queue1.count
+    let q2Length: Int = queue2.count
+    var answer: Int = Int.max
+    
+    for q in queue1 {
+        let last = cumulativeSum.last!
+        cumulativeSum.append(last + q)
+    }
+    
+    for q in queue2 {
+        let last = cumulativeSum.last!
+        cumulativeSum.append(last + q)
+    }
+    
+    let halfTotal: Int = cumulativeSum.last!/2
+    
+    if cumulativeSum.last!%2 == 1 {
+        return -1
+    }
+    
+    if cumulativeSum[q1Length] == halfTotal {
+        return 0
+    }
+    
+    func getWorkCount(l: Int, r: Int) -> Int {
+        if 0..<queue1.count ~= l {
+            if r == q1Length - 1 {
+                return l
+            }
+            if 0..<q1Length ~= r {
+                return l + r + q2Length + 1
+            } else {
+                return l + r - q1Length + 1
+            }
+        }
+        
+        return r + 1 + (l-q1Length)
+    }
+    
+    var start: Int = 0
+    
+    for i in 1..<cumulativeSum.count {
+        if cumulativeSum[i] - cumulativeSum[start] > halfTotal {
+            while cumulativeSum[i] - cumulativeSum[start] > halfTotal {
+                start += 1
+            }
+        }
+        
+        if cumulativeSum[i] - cumulativeSum[start] == halfTotal {
+            answer = min(answer,getWorkCount(l: start, r: i-1))
         }
     }
     
-    let RT = charDic["R"]! >= charDic["T"]! ? "R" : "T"
-    let CF = charDic["C"]! >= charDic["F"]! ? "C" : "F"
-    let JM = charDic["J"]! >= charDic["M"]! ? "J" : "M"
-    let AN = charDic["A"]! >= charDic["N"]! ? "A" : "N"
-    
-    return RT + CF + JM + AN
+    return answer == Int.max ? -1 : answer
 }
 
-print(solution(["AN", "CF", "MJ", "RT", "NA"], [5,3,2,7,5]))
-print(solution(["RT","CF","JM","AN","NA"], [1,1,1,2,1]))
-print(solution(["TR", "RT", "TR"], [7,1,3]))
+print(solution([3,2,7,2], [4,6,5,1]))
+print(solution([1,2,1,2], [1,10,1,2]))
+print(solution([2,1], [1,2]))
+print(solution([1,1,3], [3]))
