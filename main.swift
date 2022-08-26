19f2a361d5efaf05d585e337c6ceabbd16906aad
Fomagran <fomagran6@gmail.com>
2022-08-25 21:32:40 -0500

    [Programmers] 2022 KAKAO TECH INTERNSHIP 코딩 테스트 공부 풀이!

--- main.swift
+++ main.swift
@@ -1,51 +1,33 @@
-import Foundation
-
 func solution(_ alp:Int, _ cop:Int, _ problems:[[Int]]) -> Int {
-    var sortedProblems: [[Int]] = problems.sorted{max($0[0],$0[1]) < max($1[0],$1[1])}
-    sortedProblems.insert([1,1,1,1,100], at: 0)
-    var currentAlp: Int = alp
-    var currentCop: Int = cop
-    var answer: Int = 0
+    var maxAlp: Int = alp
+    var maxCop: Int = cop
     
-    for i in 0..<problems.count {
-        let nextProblem = sortedProblems[i+1]
-        var minTime = Int.max
-        for j in 0...i {
-            let canSolveMinTime = getMinTime(sortedProblems[j], nextProblem)
-            if minTime > canSolveMinTime{
-                minTime = canSolveMinTime
+    problems.forEach {
+        maxAlp = max(maxAlp,$0[0])
+        maxCop = max(maxCop,$0[1])
+    }
+    
+    var dp: [[Int]] = Array(repeating:Array(repeating:10000,count:maxCop+1),count:maxAlp+1)
+    dp[alp][cop] = 0
+    
+    for currentAlp in alp...maxAlp {
+        for currentCop in cop...maxCop {
+            var minAlp = min(currentAlp+1,maxAlp)
+            var minCop = min(currentCop+1,maxCop)
+            dp[minAlp][currentCop] = min(dp[minAlp][currentCop],dp[currentAlp][currentCop] + 1)
+            dp[currentAlp][minCop] = min(dp[currentAlp][minCop],dp[currentAlp][currentCop] + 1)
+            
+            problems.forEach { problem in
+                if currentAlp >= problem[0] && currentCop >= problem[1] {
+                    minAlp = min(maxAlp,currentAlp+problem[2])
+                    minCop = min(maxCop,currentCop+problem[3])
+                    dp[minAlp][minCop] = min(dp[minAlp][minCop],dp[currentAlp][currentCop] + problem[4])
+                }
             }
         }
-        
-        currentAlp = max(currentAlp,nextProblem[0])
-        currentCop = max(currentCop,nextProblem[1])
-        answer += minTime
     }
     
-    func getMinTime(_ current:[Int],_ next:[Int]) -> Int {
-        let lackOfAlp: Int = next[0] - currentAlp
-        let lackOfCop: Int = next[1] - currentCop
-        
-        //case 1 문제 안풀고 늘리기
-        let bothTime: Int = lackOfAlp + lackOfCop
-        
-        //case 2 알고력 문제로 코딩력 시간으로
-        let onlyAlp = lackOfAlp > 0 ? lackOfAlp/current[2] * current[4] + lackOfCop : lackOfCop
-        
-        //case 3 알고력 시간으로 코딩력 문제로
-        let onlyCop = lackOfCop > 0 ? lackOfCop/current[3] * current[4] + lackOfAlp : lackOfAlp
-        
-        //case 4 코딩력 알고력 모두 문제로
-        var bothProblem = onlyAlp - lackOfCop
-        let lackOfCop2 = lackOfCop - current[3] * lackOfAlp/current[2]
-        if lackOfCop2 > 0 {
-            bothProblem += lackOfCop2/current[3] * current[4]
-        }
-        
-        return min(bothTime,onlyAlp,onlyCop,bothProblem)
-    }
-    
-    return answer
+    return dp[maxAlp][maxCop]
 }
 
 
