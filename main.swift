8a512c745cf683b8dafb9deac4c17787bad6d805
Fomagran <fomagran6@gmail.com>
2022-08-29 21:15:29 -0500

    [LeetCode] Sliding Window Maximum using Binary Search Solution

--- main.swift
+++ main.swift
@@ -1,74 +1,53 @@
-import Foundation
-
-struct Node {
-    let current: Int
-    let maxIntensity: Int
-}
-
-func solution(_ n:Int, _ paths:[[Int]], _ gates:[Int], _ summits:[Int]) -> [Int] {
-    var answer: [Int] = [Int.max,Int.max]
-    var connection: [Int:[[Int]]] = [:]
-    var summitDic: [Int:Bool] = [:]
-    var gateDic: [Int:Bool] = [:]
-    var bfsQueue: [Node] = []
-    var intensities: [Int] = Array(repeating: Int.max, count: n+1)
+func maxSlidingWindow(_ nums: [Int], _ k: Int) -> [Int] {
+    var stack = [Int]()
+    var answer = [Int]()
     
-    for summit in summits {
-        summitDic[summit] = true
+    if k == 1 {
+        return nums
     }
     
-    for gate in gates {
-        gateDic[gate] = true
-    }
-    
-    for path in paths {
-        connection[path[0],default:[]].append([path[1],path[2]])
-        connection[path[1],default:[]].append([path[0],path[2]])
-    }
-    
-    for gate in gates {
-        bfsQueue.append(Node(current: gate, maxIntensity: -1))
-        bfs()
-    }
-    
-    func bfs() {
-        while !bfsQueue.isEmpty {
-            let first = bfsQueue.removeFirst()
-            checkNextNode(first)
-        }
-    }
-
-    func checkNextNode(_ node: Node) {
-        if summitDic[node.current] != nil || node.maxIntensity > intensities[node.current] {
-            return
+    for (i,num) in nums.enumerated() {
+        if stack.isEmpty {
+            stack.append(num)
+            continue
         }
         
-        for next in connection[node.current,default:[]] where gateDic[next[0]] == nil {
-            let maxIntensity = max(node.maxIntensity,next[1])
-            
-            if maxIntensity < intensities[next[0]] {
-                updateAnswer(next[0], maxIntensity)
-                intensities[next[0]] = maxIntensity
-                bfsQueue.append(Node(current: next[0], maxIntensity:maxIntensity))
-            }
+        if i >= k {
+            let index = getIndexByBinarySearch(nums[i-k])
+            stack.remove(at: index)
+        }
+
+        let index = getIndexByBinarySearch(nums[i])
+        stack.insert(num,at:index)
+        
+        if stack.count == k {
+            answer.append(stack.first!)
         }
     }
     
-    func updateAnswer(_ index: Int,_ maxIntensity: Int) {
-        if summitDic[index] != nil {
-            if maxIntensity == answer[1] {
-                answer[0] = min(answer[0],index)
-            } else if maxIntensity < answer[1] {
-                answer = [index,maxIntensity]
+    func getIndexByBinarySearch(_ num: Int) -> Int {
+        var left = 0
+        var right = stack.count - 1
+        
+        while left <= right {
+            let mid = (left+right)/2
+            let midValue = stack[mid]
+            
+            if num == midValue {
+                left = mid
+                break
+            } else if num < midValue {
+                left = mid+1
+            } else {
+                right = mid-1
             }
         }
+        
+        return left
     }
-                                  
+    
     return answer
 }
 
-print(solution(6, [[1, 2, 3], [2, 3, 5], [2, 4, 2], [2, 5, 4], [3, 4, 4], [4, 5, 3], [4, 6, 1], [5, 6, 1]], [1, 3], [5]))
-print(solution(7, [[1, 4, 4], [1, 6, 1], [1, 7, 3], [2, 5, 2], [3, 7, 4], [5, 6, 6]], [1], [2,3,4]))
-print(solution(7, [[1, 2, 5], [1, 4, 1], [2, 3, 1], [2, 6, 7], [4, 5, 1], [5, 6, 1], [6, 7, 1]], [3,7], [1,5]))
-print(solution(5, [[1, 3, 10], [1, 4, 20], [2, 3, 4], [2, 4, 6], [3, 5, 20], [4, 5, 6]], [1, 2], [5]))
-
+print(maxSlidingWindow([1,1], 1))
+print(maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3))
