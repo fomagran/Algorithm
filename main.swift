f329e9e8afa85c90a6b3c53709a732b40f44c9d1
Fomagran <fomagran6@gmail.com>
2022-09-07 21:02:47 -0500

    [LeetCode] KoKo Eating Bananas 1st solution!

--- main.swift
+++ main.swift
@@ -1,21 +1,40 @@
-func search(_ nums: [Int], _ target: Int) -> Int {
-    var left = 0
-    var right = nums.count - 1
-    
-    while left <= right {
-        let mid = (left + right)/2
+func minEatingSpeed(_ piles: [Int], _ h: Int) -> Int {
+    let sortedPiles = piles.sorted{$0 < $1}
+
+outer:for i in 0..<piles.count {
+        let current = sortedPiles[i]
+        var total = i
         
-        if nums[mid] == target {
-            left = mid
-            break
-        } else if nums[mid] < target {
-            left = mid + 1
-        } else {
-            right = mid - 1
+        for j in i..<piles.count {
+           total += sortedPiles[j]%current == 0 ? sortedPiles[j]/current : sortedPiles[j]/current + 1
+           if total > h {
+               continue outer
+           }
         }
+
+      var left = i == 0 ? 1 : sortedPiles[i-1]
+      var right = current
+        
+        while left <= right {
+            let mid = (left + right)/2
+            total = i
+            
+            for j in i..<piles.count {
+                total += sortedPiles[j]%mid == 0 ? sortedPiles[j]/mid : sortedPiles[j]/mid + 1
+            }
+            
+            if total > h {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+            
+        }
+        
+        return left
     }
     
-    return nums[min(left,nums.count-1)] == target ? left : -1
+    return 1
 }
 
-print(search([-1,0,3,5,9,12], 13))
+print(minEatingSpeed([31288470], 968709470))
