7b6afcef9eb8d200e97c7b8c041946c01e549055
Fomagran <fomagran6@gmail.com>
2022-09-08 18:42:09 -0500

    [LeetCode] Find Minimum in Rotated Sorted Array Start

--- main.swift
+++ main.swift
@@ -1,25 +1,5 @@
-func minEatingSpeed(_ piles: [Int], _ h: Int) -> Int {
+func findMin(_ nums: [Int]) -> Int {
     
-      var left = 1
-      var right = 1000000000
-        
-        while left <= right {
-            let mid = (left + right)/2
-            var total = 0
-            
-            piles.forEach { pile in
-                total += pile%mid == 0 ? pile/mid : pile/mid + 1
-            }
-
-            if total > h {
-                left = mid + 1
-            } else {
-                right = mid - 1
-            }
-            
-        }
-    
-    return left
 }
 
 print(minEatingSpeed([31288470], 968709470))
