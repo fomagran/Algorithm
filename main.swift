5db1da08ed553d5569f43df72cfc94079091cd82
Fomagran <fomagran6@gmail.com>
2022-09-11 18:46:17 -0500

    [LeetCode] Time Based Key-Value Store Refactor

--- main.swift
+++ main.swift
@@ -1,42 +1,38 @@
 class TimeMap {
     var hashMap = [String:[(Int,String)]]()
-    
-    init() {
-        
-    }
-    
+
     func set(_ key: String, _ value: String, _ timestamp: Int) {
         hashMap[key,default:[]].append((timestamp,value))
     }
     
     func get(_ key: String, _ timestamp: Int) -> String {
-        if hashMap[key,default:[]].isEmpty {
+        if hashMap[key]?.first?.0 ?? Int.max > timestamp  {
             return ""
         } else {
-            if hashMap[key]!.first!.0 > timestamp {
-                return ""
-            }
-            
             if hashMap[key]!.last!.0 > timestamp {
-                var left = 0
-                var right = hashMap[key]!.count - 1
-                
-                while left <= right {
-                    let mid = (left + right)/2
-                    
-                    if hashMap[key]![mid].0 == timestamp {
-                        return hashMap[key]![mid].1
-                    } else if hashMap[key]![mid].0 > timestamp  {
-                        right = mid - 1
-                    } else {
-                        left = mid + 1
-                    }
-                }
-                return hashMap[key]![right].1
+                return findClosestTimestamp(hashMap[key]!, timestamp)
             } else {
                 return hashMap[key]!.last!.1
             }
         }
+    }
+    
+    func findClosestTimestamp(_ timestamps: [(Int,String)], _ target: Int) -> String {
+        var left = 0
+        var right = timestamps.count - 1
+        
+        while left <= right {
+            let mid = (left + right)/2
+            
+            if timestamps[mid].0 == target {
+                return timestamps[mid].1
+            } else if timestamps[mid].0 > target  {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        }
+        return timestamps[right].1
     }
 }
 
@@ -44,7 +40,7 @@ class TimeMap {
 let timeMap = TimeMap()
 timeMap.set("love", "high", 10);
 timeMap.set("love", "low", 20);
-print(timeMap.get("love", 5))
+print(timeMap.get("l", 5))
 print(timeMap.get("love", 10))
 //timeMap.set("foo", "bar2", 4)
 print(timeMap.get("love", 15))
