4f6ef8c56148d4865507b4a4a5ab95777df6f341
Fomagran <fomagran6@gmail.com>
2022-09-10 20:15:14 -0500

    [LeetCode] Find Minimum Rotated Sorted Array

--- main.swift
+++ main.swift
@@ -1,5 +1,22 @@
 func findMin(_ nums: [Int]) -> Int {
+    var left = 0
+    var right = nums.count - 1
     
+    while left < right {
+        let mid = (left + right)/2
+        
+        if nums[mid] < nums[right] {
+            right = mid
+        }  else {
+            left = mid + 1
+        }
+    }
+    
+    return nums[left]
 }
 
-print(minEatingSpeed([31288470], 968709470))
+print(findMin([3,4,5,1,2]))
+print(findMin([4,5,6,7,0,1,2]))
+print(findMin([11,13,15,17]))
+print(findMin([1]))
+print(findMin([3,1,2]))
