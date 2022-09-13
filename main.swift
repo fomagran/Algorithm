24640d46bb39cdb56886f6d23c6670f799474243
Fomagran <fomagran6@gmail.com>
2022-09-12 20:30:29 -0500

    [LeetCode] Reverse Linked List Refactor

--- main.swift
+++ main.swift
@@ -1,11 +1,8 @@
 func reverseList(_ head: ListNode?) -> ListNode? {
-    func reverseNode(_ nextNode: ListNode?, _ tailNode:ListNode?) -> ListNode? {
-        if let node = nextNode {
-            var newNode = ListNode(node.val)
-            newNode.next = tailNode
-            return reverseNode(node.next,newNode)
-        }
-        return tailNode
+    func reverseNode(_ node: ListNode?, _ tailNode:ListNode?) -> ListNode? {
+        let newTail = ListNode(node?.val ?? 0)
+        newTail.next = tailNode
+        return node == nil ? tailNode : reverseNode(node?.next,newTail)
     }
     return reverseNode(head,nil)
 }
