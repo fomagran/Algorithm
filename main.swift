09d9c37bf06fea5c07e5d7c8155bdceb15dd3f2d
Fomagran <fomagran6@gmail.com>
2022-09-14 20:57:06 -0500

    [LeetCode] Copy List with Random Pointer Solution!

--- main.swift
+++ main.swift
@@ -1,56 +1,32 @@
-public class ListNode {
-    public var val: Int
-    public var next: ListNode?
-    public init() { self.val = 0; self.next = nil; }
-    public init(_ val: Int) { self.val = val; self.next = nil; }
-    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
-}
-
-func reorderList(_ head: ListNode?) {
-    var node: ListNode? = head
-    var normalNode: ListNode? = ListNode(head!.val)
-    var normalRoot = normalNode
-    var reversedNode: ListNode? = ListNode(head!.val)
-    var count = 0
-    var root = head
+func copyRandomList(_ head: Node?) -> Node? {
+    var node = head
+    var root = Node(-1)
+    var copiedNode = root
     
     while node != nil {
-        count += 1
-        normalNode?.next = node?.next
-        normalNode = normalNode?.next
-        let new: ListNode? = ListNode(node!.val)
-        new?.next = reversedNode
-        reversedNode = new
+        let newNode = Node(node!.val)
+        newNode.next = node?.next
+        node?.next = newNode
+        node = newNode.next
+    }
+    
+    node = head
+    
+    while node != nil {
+        let newNode = node?.next
+        newNode?.random = node?.random?.next
+        node = newNode?.next
+    }
+    
+    node = head
+    
+    while node != nil {
+        let newNode = node?.next
+        copiedNode.next = newNode
+        copiedNode = copiedNode.next!
+        node?.next = newNode?.next
         node = node?.next
     }
     
-    for i in 1...count {
-        if i == 1 {
-            normalRoot = normalRoot?.next
-            continue
-        }
-        
-        if i%2 == 1 {
-            root?.next = normalRoot
-            normalRoot = normalRoot?.next
-        } else {
-            root?.next = reversedNode
-            reversedNode = reversedNode?.next
-        }
-        
-        root = root?.next
-    }
-    
-    root?.next = nil
+    return root.next
 }
-
-let head: ListNode? = ListNode(1)
-head?.next = ListNode(2)
-//head?.next?.next = ListNode(3)
-//head?.next?.next?.next = ListNode(4)
-//head?.next?.next?.next?.next = ListNode(5)
-//head?.next?.next?.next?.next?.next = ListNode(6)
-
-reorderList(head)
-
-
