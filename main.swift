410d0cc64ee56ae6e0676b9e674961adc03235f7
Fomagran <fomagran6@gmail.com>
2022-09-15 21:47:08 -0500

    [LeetCode] Linked List Cycle Solution!

--- main.swift
+++ main.swift
@@ -1,35 +1,12 @@
-public class Node:Hashable {
-    public var val: Int
-    public var next: Node?
-    public var random: Node?
-    public init(_ val: Int) {
-        self.val = val
-        self.next = nil
-        self.random = nil
-    }
-}
-
-func copyRandomList(_ head: Node?) -> Node? {
-    var node = head
-    var copiedDic =  [Node: Node]()
-    
-    while node != nil {
-        copiedDic[node!] = Node(node!.val)
-        node = node!.next
+func hasCycle(_ head: ListNode?) -> Bool {
+    if head == nil {
+        return false
     }
     
-    node = head
-    
-    while node != nil {
-        let copiedNode = copiedDic[node!]
-        if let next = node!.next {
-            copiedNode?.next = copiedDic[next]
-        }
-        if let random = node!.random {
-            copiedNode?.random = copiedDic[random]
-        }
-        node = node!.next
+    if head!.val == Int.max {
+        return true
     }
     
-    return copiedDic[head!]
+    head!.val = Int.max
+    return hasCycle(head!.next)
 }
