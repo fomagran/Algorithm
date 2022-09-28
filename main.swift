f86af92cf0cc73233afcc59c3f3ace5c09b8dc69
Fomagran <fomagran6@gmail.com>
2022-09-27 19:42:36 -0500

    [LeetCode] Maximum Depth of Binary Tree

--- main.swift
+++ main.swift
@@ -1,31 +1,15 @@
-public class TreeNode {
-    public var val: Int
-    public var left: TreeNode?
-    public var right: TreeNode?
-    public init() { self.val = 0; self.left = nil; self.right = nil; }
-    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
-    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
-        self.val = val
-        self.left = left
-        self.right = right
-    }
-}
-
-func invertTree(_ root: TreeNode?) -> TreeNode? {
-    func dfs(_ node: TreeNode?) {
-        if node?.left == nil {
+func maxDepth(_ root: TreeNode?) -> Int {
+    var answer:Int = -1
+    
+    func dfs(_ node: TreeNode?,_ depth: Int) {
+        if node == nil {
+            answer = max(answer,depth)
             return
         }
-        
-        
-        node?.left = node?.right
-        node?.right = node?.left
-        
-        dfs(node?.left)
-        dfs(node?.right)
+        dfs(node?.left,depth+1)
+        dfs(node?.right,depth+1)
     }
-    dfs(root)
     
-    
-    return root
+    dfs(root,0)
+    return answer
 }
