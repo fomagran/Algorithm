e46f6361edb51301b9ce05f9175dcebacd9dbaf0
Fomagran <fomagran6@gmail.com>
2022-10-02 18:04:24 -0500

    [LeetCode] Same Tree Solution using Array

--- main.swift
+++ main.swift
@@ -11,51 +11,51 @@ public class TreeNode {
     }
 }
 
-func isBalanced(_ root: TreeNode?) -> Bool {
-    var isBalanced = true
+func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+    var pVal = [Int]()
+    var qVal = [Int]()
     
-    func dfs(_ node: TreeNode?) -> Int {
-        if node == nil || !isBalanced {
-            return 0
+    func dfs(_ node: TreeNode?,_ isP: Bool, _ n: Int) {
+        if isP {
+            pVal.append(node?.val ?? Int.min)
+        } else {
+            qVal.append(node?.val ?? Int.min)
+        }
+
+        if node == nil {
+            return
         }
         
-        let left = dfs(node?.left)
-        let right = dfs(node?.right)
-        
-        if abs(left-right) > 1 {
-            isBalanced = false
-            return 0
-        }
-        
-        return max(left,right) + 1
+        dfs(node?.left,isP,n+1)
+        dfs(node?.right,isP,n+1)
     }
     
-    return dfs(root) != -1
+    dfs(p, true,0)
+    dfs(q,false,0)
+    
+    for i in 0..<pVal.count {
+        if pVal[i] != qVal[i] {
+            return false
+        }
+    }
+    
+    return true
 }
 
-let root:TreeNode? = TreeNode(0)
-root?.left = TreeNode(1)
-root?.right = TreeNode(2)
-root?.left?.left = TreeNode(3)
-root?.left?.right = TreeNode(4)
-root?.right?.left = TreeNode(5)
-root?.right?.right = TreeNode(6)
-root?.left?.left?.left = TreeNode(7)
-root?.left?.left?.right = TreeNode(8)
-root?.left?.right?.left = TreeNode(9)
-root?.left?.right?.right = TreeNode(10)
-root?.right?.left?.left = TreeNode(11)
-root?.right?.left?.right = TreeNode(12)
-root?.right?.right?.left = TreeNode(13)
-root?.right?.right?.right = TreeNode(14)
-root?.left?.left?.left?.left = TreeNode(15)
-root?.left?.left?.left?.right = TreeNode(16)
+let p:TreeNode? = TreeNode(0)
+p?.left = TreeNode(1)
+p?.right = TreeNode(2)
+p?.left?.left = TreeNode(3)
+p?.left?.right = TreeNode(4)
+p?.right?.left = TreeNode(5)
+p?.right?.right = TreeNode(6)
 
-//                0
-//        1              2
-//    3       4       5     6
-//   7  8    9  10  11 12 13 14
-// 15 16
+let q:TreeNode? = TreeNode(0)
+q?.left = TreeNode(1)
+q?.right = TreeNode(2)
+q?.left?.left = TreeNode(3)
+q?.left?.right = TreeNode(4)
+q?.right?.left = TreeNode(5)
+q?.right?.right = TreeNode(6)
 
-isBalanced(root)
-
+print(isSameTree(p, q))
