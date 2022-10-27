acb028e74e2b408ab92737ece637c803a1d88cf2
Fomagran <fomagran6@gmail.com>
2022-10-26 19:47:31 -0500

    [LeetCode] Task Scheduler Start

--- main.swift
+++ main.swift
@@ -1,25 +1,3 @@
-func findKthLargest(_ nums: [Int], _ k: Int) -> Int {
-    var sortedNums = nums
-    return quickSelect(&sortedNums,nums.count - k,0,nums.count - 1)
-}
-
-func quickSelect(_ nums: inout [Int],_ k: Int, _ start: Int, _ end: Int) -> Int {
-    var p = start
-
-    for i in start..<end {
-        if nums[i] <= nums[end] {
-            nums.swapAt(i,p)
-            p += 1
-        }
-    }
+func leastInterval(_ tasks: [Character], _ n: Int) -> Int {
     
-    nums.swapAt(p,end)
-
-    if p < k {
-        quickSelect(&nums,k,p+1,nums.count-1)
-    } else if p > k {
-        quickSelect(&nums,k,start,p-1)
-    }
-
-    return nums[k]
 }
