24fa03b90257ef89242076b29801a47db3dd010a
Kalid An <fomagran6@gmail.com>
2022-10-28 15:31:14 -0500

    [LeetCode] Task Scheduler Solution Binary Search & Sort

--- main.swift
+++ main.swift
@@ -1,34 +1,56 @@
 func leastInterval(_ tasks: [Character], _ n: Int) -> Int {
-    var taskDic = [Character: Int]()
-    var lastDic = [Character: Int]()
-    var current = 0
+  var tasksCountDic = [Character: Int]()
+  var least = 0
 
-    tasks.forEach {
-        taskDic[$0,default:0] += 1
-        lastDic[$0] = Int.min
+  tasks.forEach {
+    tasksCountDic[$0, default: 0] += 1
+  }
+
+  var queue = tasksCountDic.map { ($0.key, $0.value) }.sorted { $0.1 > $1.1 }
+
+  while !queue.isEmpty {
+    var time = 0
+    var removedTasks = [(Character, Int)]()
+
+    for i in 0...n {
+      if !queue.isEmpty {
+        let task = queue.removeFirst()
+        time += 1
+        removedTasks.append((task.0,task.1-1))
+      }
+    }
+
+    for removedTask in removedTasks {
+      if removedTask.1 > 0 {
+        insert(removedTask,&queue)
+      }
     }
     
-    outer: while !taskDic.isEmpty {
-        for (task,_) in taskDic {
+    least += queue.isEmpty ? time : n+1
 
-            if lastDic[task]! < current - n {
-                lastDic[task] = current
-                taskDic[task]! -= 1
+  }
 
-                if taskDic[task]! == 0 {
-                    taskDic[task] = nil
-                }
-                
-                current += 1
-                continue outer
-            }
-        }
-        
-        current += 1
+  return least
+}
 
+func insert(_ task: (Character, Int), _ tasks: inout [(Character, Int)]) {
+  var left = 0
+  var right = tasks.count - 1
+
+  while left <= right {
+    let mid = (left + right)/2
+
+    if tasks[mid].1 == task.1 {
+      left = mid + 1
+      break
+    } else if tasks[mid].1 > task.1 {
+      left = mid + 1
+    } else {
+      right = mid - 1
     }
-    
-    return current
+  }
+
+  tasks.insert(task,at: left)
 }
 
 //print(leastInterval(["A","A","A","B","B","B"], 2))
