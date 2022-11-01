2b98be9296ccffe64314d07b3cdc178dc3cef55e
Fomagran <fomagran6@gmail.com>
2022-10-31 20:55:44 -0500

    [LeetCode] Task Scheduler MaxHeap Solution!

--- main.swift
+++ main.swift
@@ -1,36 +1,80 @@
 func leastInterval(_ tasks: [Character], _ n: Int) -> Int {
-    var taskDic = [Character: Int]()
-    var lastDic = [Character: Int]()
-    var current = 0
-
+    var tasksCountDic = [Character: Int]()
+    var least = 0
+    
     tasks.forEach {
-        taskDic[$0,default:0] += 1
-        lastDic[$0] = Int.min
+        tasksCountDic[$0, default: 0] += 1
     }
     
-    outer: while !taskDic.isEmpty {
-        for (task,_) in taskDic {
-
-            if lastDic[task]! < current - n {
-                lastDic[task] = current
-                taskDic[task]! -= 1
-
-                if taskDic[task]! == 0 {
-                    taskDic[task] = nil
-                }
-                
-                current += 1
-                continue outer
+    var queue = [Int]()
+    
+    for task in tasksCountDic.values {
+        insert(task, &queue)
+    }
+    
+    while !queue.isEmpty {
+        var time = 0
+        var removedTasks = [Int]()
+        
+        for _ in 0...n {
+            if !queue.isEmpty {
+                let task = remove(&queue)
+                time += 1
+                removedTasks.append(task-1)
             }
         }
         
-        current += 1
-
+        for removedTask in removedTasks {
+            if removedTask > 0 {
+                insert(removedTask,&queue)
+            }
+        }
+        
+        least += queue.isEmpty ? time : n+1
     }
     
-    return current
+    return least
 }
 
-//print(leastInterval(["A","A","A","B","B","B"], 2))
-//print(leastInterval(["A","A","A","B","B","B"], 0))
-print(leastInterval(["A","A","A","A","A","A","B","C","D","E","F","G"], 2))
+func insert(_ task: Int, _ tasks: inout [Int]) {
+    tasks.append(task)
+    var index = tasks.count - 1
+    
+    while (index-1)/2 >= 0 && tasks[(index-1)/2] < tasks[index] {
+        tasks.swapAt((index-1)/2,index)
+        index = (index-1)/2
+    }
+}
+
+func remove(_ tasks: inout [Int]) -> Int {
+    if tasks.isEmpty {
+        return 0
+    }
+    
+    tasks.swapAt(0, tasks.count - 1)
+    let last = tasks.removeLast()
+    
+    var index = 0
+    
+    while 2 * index + 1 < tasks.count {
+        let leftChildIndex = 2 * index + 1
+        let rightChildIndex = 2 * index + 2
+        var largeChildIndex = leftChildIndex
+        
+        if rightChildIndex < tasks.count && tasks[leftChildIndex] < tasks[rightChildIndex] {
+            largeChildIndex = rightChildIndex
+        }
+        
+        if tasks[index] > tasks[largeChildIndex] {
+            break
+        } else {
+            tasks.swapAt(index, largeChildIndex)
+        }
+        index = largeChildIndex
+    }
+    
+    return last
+}
+
+print(leastInterval(["A","A","A","A","A","A","B","C","D","E","F","G"]
+                    ,2))
