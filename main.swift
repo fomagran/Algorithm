244a43f1b2ba75f619befabb962e4a65208dc292
Fomagran <fomagran6@gmail.com>
2022-11-07 21:10:41 -0600

    [LeetCode] Move Zeroes

--- main.swift
+++ main.swift
@@ -1,22 +1,12 @@
-
-func isSymmetric(_ root: TreeNode?) -> Bool {
-    var leftValues = [Int?]()
-    var rightValues = [Int?]()
-    dfs(root?.left,&leftValues,true)
-    dfs(root?.right,&rightValues,false)
+func moveZeroes(_ nums: inout [Int]) {
+    var current = 0
     
-    return leftValues == rightValues
-}
-
-func dfs(_ node: TreeNode?,_ values: inout [Int?],_ isLeft: Bool) {
-    values.append(node?.val)
-    if let node = node {
-        if isLeft {
-            dfs(node.left,&values,isLeft)
-            dfs(node.right,&values,isLeft)
-        } else {
-            dfs(node.right,&values,isLeft)
-            dfs(node.left,&values,isLeft)
+    for i in 0..<nums.count {
+        if nums[current] == 0 {
+            nums.remove(at: current)
+            nums.append(0)
+            continue
         }
+        current += 1
     }
 }
