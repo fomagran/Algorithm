aa38a88581b733537b93213281379850727c6d19
Fomagran <fomagran6@gmail.com>
2022-11-15 20:42:13 -0600

    [LeetCode] Find Median from Data Stream Binary Search Solution!

--- main.swift
+++ main.swift
@@ -1,20 +1,27 @@
-func isSubsequence(_ s: String, _ t: String) -> Bool {
-    if s.count == 0 {
-        return true
+class MedianFinder {
+    private var nums = [Int]()
+    
+    func addNum(_ num: Int) {
+        var left = 0
+        var right = nums.count - 1
+        
+        while left <= right {
+            let mid = (left + right)/2
+            
+            if nums[mid] == num {
+                left = mid + 1
+                break
+            } else if nums[mid] < num {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        }
+        
+        nums.insert(num,at: left)
     }
     
-    let sMap = Array(s)
-    let tMap = Array(t)
-    var current = 0
-    
-    for c in tMap {
-        if c == sMap[current] {
-            current += 1
-        }
-        if current == s.count {
-            break
-        }
+    func findMedian() -> Double {
+        return nums.count%2 == 1 ? Double(nums[nums.count/2]) : Double(nums[nums.count/2-1] + nums[nums.count/2])/2
     }
-    
-    return current >= s.count
 }
