7036846b38ff57db2f87af48c54a8f6d3a401ad0
Fomagran <fomagran6@gmail.com>
2022-11-21 19:43:44 -0600

    [LeetCode] Length of Last Word Solution!

--- main.swift
+++ main.swift
@@ -1,23 +1,3 @@
-func subsetsWithDup(_ nums: [Int]) -> [[Int]] {
-    var answer = Set<[Int]>()
-    answer.insert([])
-    let sorted = nums.sorted()
-    
-    for i in 0..<nums.count {
-        dfs(i,[],sorted,&answer)
-    }
-    return  Array(answer)
-}
-
-func dfs(_ level: Int, _ prev: [Int], _ nums: [Int], _ answer: inout Set<[Int]> ) {
-  if level == nums.count {
-    return
-  }
-
-  let current = prev + [nums[level]]
-  answer.insert(current)
-
-  for l in level+1..<nums.count {
-    dfs(l,current,nums,&answer)
-  }
+func lengthOfLastWord(_ s: String) -> Int {
+    return s.components(separatedBy: " ").filter { $0.count != 0 }.last!.count
 }
