ff910c00578456468c6b99de6ab6b7c479660214
Fomagran <fomagran6@gmail.com>
2022-11-25 12:35:15 -0600

    [LeetCode] Combination Sum II TLE

--- main.swift
+++ main.swift
@@ -1,3 +1,37 @@
-func lengthOfLastWord(_ s: String) -> Int {
-    return s.components(separatedBy: " ").filter { $0.count != 0 }.last!.count
+func combinationSum2(_ candidates: [Int], _ target: Int) -> [[Int]] {
+    let sorted = candidates.sorted { $0 < $1 }
+    var answer = Set<[Int]>()
+    
+    func dfs(_ depth: Int, _ combination: [Int]) {
+        let sum = combination.reduce(0, { $0 + $1 })
+        
+        if depth == candidates.count {
+            return
+        }
+        
+        if sum > target {
+            return
+        }
+        
+        if sum == target {
+            answer.insert(combination)
+        }
+        
+        for i in depth+1..<candidates.count {
+            var new = combination
+            new.append(sorted[i])
+            dfs(i,new)
+        }
+    }
+    
+    for i in 0..<candidates.count {
+        dfs(i,[sorted[i]])
+    }
+    
+    return Array(answer)
 }
+
+
+
+
+print(combinationSum2([10,1,2,7,6,1,5], 8))
