a5d020dea83504d5797b9f340402b982dbaab730
Fomagran <fomagran6@gmail.com>
2023-10-17 13:27:50 +0900

    다리를 지나는 트럭 풀이

--- 알고리즘풀이.playground/Contents.swift
+++ 알고리즘풀이.playground/Contents.swift
@@ -1,25 +1,26 @@
-func solution(_ genres:[String], _ plays:[Int]) -> [Int] {
-    var result: [Int] = []
-    var genresPlayings: [String: Int] = [:]
-    var genresNumbers: [String: [Int: Int]] = [:]
+func solution(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
+    var result: Int = 0
+    var truckWeights: [Int] = truck_weights
+    var currentWeight: Int = 0
+    var bridgeTrucks: [Int: Int] = [:]
     
-    for (i,play) in plays.enumerated() {
-        let genre = genres[i]
-        genresPlayings[genre, default: 0] += play
-        genresNumbers[genre, default: [:]][i] = play
-    }
-    
-    let sortedByPlaying = genresPlayings.sorted { $0.value > $1.value}
-    
-    for genre in sortedByPlaying {
-        let sorted = genresNumbers[genre.key]!.sorted { $0.value == $1.value ? $0.key < $1.key : $0.value > $1.value }
-    
-        for i in 0..<min(sorted.count, 2) {
-            result.append(sorted[i].key)
+    while !truckWeights.isEmpty || !bridgeTrucks.isEmpty {
+        if bridgeTrucks[result] != nil {
+            currentWeight -= bridgeTrucks[result, default: 0]
+            bridgeTrucks[result] = nil
         }
+        
+        if !truckWeights.isEmpty  && currentWeight + truckWeights.first! <= weight {
+            let first = truckWeights.removeFirst()
+            currentWeight += first
+            bridgeTrucks[result + bridge_length] = first
+        }
+        
+        result += 1
     }
+    
     
     return result
 }
 
-solution(["classic", "pop", "classic", "classic", "pop"],[500, 600, 150, 800, 2500])
+solution(2, 10, [7,4,5,6])
